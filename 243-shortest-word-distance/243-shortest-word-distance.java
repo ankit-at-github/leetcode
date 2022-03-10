e63dfcf9013b2bfab76d7c8b60269ac0ff9a0312
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-03-10 09:58:48 -0500

    Time: 3 ms (43.42%), Space: 45 MB (16.88%) - LeetHub

--- 243-shortest-word-distance/243-shortest-word-distance.java
+++ 243-shortest-word-distance/243-shortest-word-distance.java
@@ -1,26 +1,24 @@
 class Solution {
     public int shortestDistance(String[] wordsDict, String word1, String word2) {
-        Map<String, Integer> map = new HashMap<>();
-        
+        int index1 = -1;
+        int index2 = -1;
         int min = Integer.MAX_VALUE;
         
         for(int i=0; i<wordsDict.length; i++)
         {
             if(wordsDict[i].equals(word1))
             {
-                if(map.containsKey(word2))
-                    min = Math.min(min, Math.abs(i-map.get(word2)));
-                map.put(word1,i);
+                index1 = i;
             }
-            else if(wordsDict[i].equals(word2))
+            if(wordsDict[i].equals(word2))
             {
-                if(map.containsKey(word1))
-                    min = Math.min(min, Math.abs(i-map.get(word1)));
-                map.put(word2,i);
+                index2 = i;
             }
-            
+            if(index1!=-1 && index2!=-1)
+            {
+                min = Math.min(min, Math.abs(index1 - index2));
+            }
         }
-        
         return min;
     }
 }
