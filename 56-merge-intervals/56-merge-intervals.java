8f5e67f3f3c0354911f1f9b2047ec92c5fd1d660
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-03-21 14:43:35 -0400

    Time: 21 ms (11.96%), Space: 55.4 MB (36.62%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,43 +1,45 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a,b) -> a[0]-b[0]);
+        
         List<List<Integer>> ans = new ArrayList<>();
-        
-        int[] start = new int[10001];
-        int[] end = new int[10001];
-        
-        for(int[] interval : intervals)
-        {
-            start[interval[0]]++;
-            end[interval[1]]--;
-        }
-        
-        int count = 0;
         List<Integer> current = new ArrayList<>();
-        for(int i=0; i<10001; i++)
+        
+        int prevStart = intervals[0][0];
+        int prevEnd = intervals[0][1];
+        
+        for(int i=1; i<intervals.length; i++)
         {
-            count+=start[i];
-            if(count!=0)
+            int currentStart = intervals[i][0];
+            int currentEnd = intervals[i][1];
+            
+            if(prevEnd >= currentStart)
             {
-                if(current.size() == 0) current.add(i);
-                count+=end[i];
-                if(count==0)
-                {
-                    current.add(i);
-                    ans.add(current);
-                    current = new ArrayList<>();
-                }
+                prevEnd = Math.max(prevEnd, currentEnd);
+            }
+            else
+            {
+                current.add(prevStart);
+                current.add(prevEnd);
+                ans.add(current);
+                current = new ArrayList();
+                prevStart = currentStart;
+                prevEnd = currentEnd;
             }
         }
         
-        int[][] result = new int[ans.size()][2];
+        /* final pair */
+        current.add(prevStart);
+        current.add(prevEnd);
+        ans.add(current);
         
+        int[][] output = new int[ans.size()][2];
         for(int i=0; i<ans.size(); i++)
         {
-            result[i][0] = ans.get(i).get(0);
-            result[i][1] = ans.get(i).get(1);
+            output[i][0] = ans.get(i).get(0);
+            output[i][1] = ans.get(i).get(1);
         }
         
-        return result;
-        
+        return output;
     }
 }
