3d11e04c03971259c92e2be396e4d184a8403cb3
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-03-23 11:42:16 -0400

    Time: 3 ms (61.75%), Space: 55.9 MB (22.98%) - LeetHub

--- 370-range-addition/370-range-addition.java
+++ 370-range-addition/370-range-addition.java
@@ -7,11 +7,16 @@ class Solution {
             int start = update[0];
             int end = update[1];
             int value = update[2];
-            for(int i=start; i<=end; i++)
-            {
-                arr[i]+=value;
-            }
+            
+            arr[start]+= value;
+            
+            if(end+1 < length)
+                arr[end+1]+= -value;
         }
+        
+        for(int i=1; i<length; i++)
+            arr[i]+=arr[i-1];
+        
         return arr;
     }
 }
