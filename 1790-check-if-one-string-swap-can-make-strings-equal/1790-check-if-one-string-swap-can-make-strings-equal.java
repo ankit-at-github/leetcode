c2d9906916014c4c022e2fb693e9eba618ad3928
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-03-30 15:34:27 -0400

    Time: 1 ms (81.36%), Space: 40.2 MB (94.77%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.java
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.java
@@ -1,21 +1,17 @@
 class Solution {
     public boolean areAlmostEqual(String s1, String s2) {
         if(s1.length()!=s2.length()) return false;
-        int[] freq = new int[26];
+        
+        List<Integer> l = new ArrayList<>();
         for(int i=0; i<s1.length(); i++)
         {
-            freq[s1.charAt(i)-'a']++;
-            freq[s2.charAt(i)-'a']--;
+            if(s1.charAt(i)!=s2.charAt(i)) l.add(i);
+            if(l.size() > 2) return false;
         }
         
-        for(int i=0; i<26; i++) if(freq[i]!=0) return false;
+        return (l.size()==0 || l.size()==2 && 
+                                 s1.charAt(l.get(0)) == s2.charAt(l.get(1)) &&
+                                 s1.charAt(l.get(1)) == s2.charAt(l.get(0)));
         
-        int swap = 0;
-        for(int i=0; i<s1.length(); i++)
-        {
-            if(s1.charAt(i)!=s2.charAt(i)) swap++;
-            if(swap>2) return false;
-        }
-        return true;
     }
 }
