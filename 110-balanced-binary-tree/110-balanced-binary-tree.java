b7fe63985400f40e88929fdfde02fb2333f73ddc
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-04-09 15:47:44 -0400

    Time: 1 ms (88.40%), Space: 44.6 MB (39.51%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.java
+++ 110-balanced-binary-tree/110-balanced-binary-tree.java
@@ -23,11 +23,12 @@ class Solution {
         if(root == null) return 0;
         
         int left = getHeight(root.left);
+        if(left == -1) return -1;
+        
         int right = getHeight(root.right);
+        if(right == -1) return -1;
         
         if(Math.abs(left-right) > 1) return -1;
-        
-        if(left == -1 || right == -1) return -1;
         
         return 1 + Math.max(left, right);
     }
