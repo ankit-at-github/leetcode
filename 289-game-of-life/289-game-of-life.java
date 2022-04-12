9bd02c03a9f0d136f80ea80144f07869bcfdd1fd
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-04-11 20:05:29 -0400

    Time: 0 ms (100.00%), Space: 40.5 MB (90.12%) - LeetHub

---
+++ 289-game-of-life/289-game-of-life.java
@@ -0,0 +1,55 @@
+class Solution {
+    public void gameOfLife(int[][] board) {
+
+        // Neighbors array to find 8 neighboring cells for a given cell
+        int[] neighbors = {0, 1, -1};
+
+        int rows = board.length;
+        int cols = board[0].length;
+
+        // Create a copy of the original board
+        int[][] copyBoard = new int[rows][cols];
+
+        // Create a copy of the original board
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                copyBoard[row][col] = board[row][col];
+            }
+        }
+
+        // Iterate through board cell by cell.
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+
+                // For each cell count the number of live neighbors.
+                int liveNeighbors = 0;
+
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+
+                        if (!(neighbors[i] == 0 && neighbors[j] == 0)) {
+                            int r = (row + neighbors[i]);
+                            int c = (col + neighbors[j]);
+
+                            // Check the validity of the neighboring cell.
+                            // and whether it was originally a live cell.
+                            // The evaluation is done against the copy, since that is never updated.
+                            if ((r < rows && r >= 0) && (c < cols && c >= 0) && (copyBoard[r][c] == 1)) {
+                                liveNeighbors += 1;
+                            }
+                        }
+                    }
+                }
+
+                // Rule 1 or Rule 3
+                if ((copyBoard[row][col] == 1) && (liveNeighbors < 2 || liveNeighbors > 3)) {
+                    board[row][col] = 0;
+                }
+                // Rule 4
+                if (copyBoard[row][col] == 0 && liveNeighbors == 3) {
+                    board[row][col] = 1;
+                }
+            }
+        }
+    }
+}
