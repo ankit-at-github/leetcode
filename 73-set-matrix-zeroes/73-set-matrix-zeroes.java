eb5b60fc37c3bfbcf6f50c5b4aae20aee71befb4
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-04-20 01:11:54 -0400

    Time: 2 ms (38.98%), Space: 54.1 MB (52.83%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,28 +1,28 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        List<List<Integer>> indexes = new ArrayList<>();
-        List<Integer> index = new ArrayList<>();
+        List<Integer> rows = new ArrayList<>();
+        List<Integer> cols = new ArrayList<>();
+        
         for(int i=0; i<matrix.length; i++)
         {
             for(int j=0; j<matrix[i].length; j++)
             {
                 if(matrix[i][j] == 0)
                 {
-                    index.add(i);
-                    index.add(j);
-                    indexes.add(new ArrayList(index));
-                    index = new ArrayList();
+                    rows.add(i);
+                    cols.add(j);
                 }
             }
         }
         
-        for(List<Integer> l : indexes)
+        for(int r : rows)
         {
-            int row = l.get(0);
-            int col = l.get(1);
-            
-            for(int i=0; i<matrix[0].length; i++) matrix[row][i] = 0;
-            for(int i=0; i<matrix.length; i++) matrix[i][col] = 0;
+            for(int j=0; j<matrix[r].length; j++) matrix[r][j] = 0;
         }
+        for(int c : cols)
+        {
+            for(int i=0; i<matrix.length; i++) matrix[i][c] = 0;
+        }
+        
     }
 }
