5cff1df0c9442ed766cf194dc4465146968025e4
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-05-11 22:57:21 -0400

    Time: 944 ms (5.00%), Space: 48 MB (54.92%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.java
+++ 47-permutations-ii/47-permutations-ii.java
@@ -1,29 +1,32 @@
 class Solution {
-    public void solve(int[] nums, int start, List<Integer> temp, boolean[] visited, List<List<Integer>> ans)
-    {
-        if(temp.size() == nums.length) ans.add(new ArrayList(temp));
-        
-        for(int i=0; i<nums.length; i++)
-        {
-            if(visited[i] || (i > 0 && nums[i] == nums[i-1] && !visited[i-1])) continue;
-            
-            temp.add(nums[i]);
-            visited[i] = true;
-            solve(nums, i, temp, visited, ans);
-            visited[i] = false;
-            temp.remove(temp.size()-1);
-        }
-    }
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> output = new ArrayList<>();
         
         Arrays.sort(nums);
         
         boolean[] visited = new boolean[nums.length];
         
-        solve(nums, 0, new ArrayList<>(), visited, ans);
+        solve(nums, visited, output, ans);
         
         return ans;
     }
-    
+    public void solve(int[] nums, boolean[] visited, List<Integer> output, List<List<Integer>> ans)
+    {
+        if(output.size() == nums.length)
+        {
+            if(!ans.contains(output))
+            ans.add(new ArrayList(output));
+            return;
+        }
+        for(int i=0; i<nums.length; i++)
+        {
+            if(visited[i]) continue;
+            visited[i] = true;
+            output.add(nums[i]);
+            solve(nums, visited, output, ans);
+            visited[i] = false;
+            output.remove(output.size()-1);
+        }
+    }
 }
