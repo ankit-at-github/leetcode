5acb3128e961c73c186e9af09ab74af9dbc98cd0
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-05-30 14:42:36 -0400

    Time: 1 ms (92.45%), Space: 54.2 MB (35.51%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,26 +1,44 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        List<Integer> row = new ArrayList<>();
-        List<Integer> col = new ArrayList<>();
+        boolean col = false;
         
-        for(int i=0; i<matrix.length; i++)
+        for(int j=0; j<matrix[0].length; j++) if(matrix[0][j] == 0) col = true;
+        
+        for(int i=1; i<matrix.length; i++)
         {
             for(int j=0; j<matrix[i].length; j++)
             {
+                
                 if(matrix[i][j] == 0)
                 {
-                    row.add(i);
-                    col.add(j);
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
         
-        for(int i=0; i<matrix.length; i++)
+        //checking first element of all rows
+        for(int i=1; i<matrix.length; i++)
         {
-            for(int j=0; j<matrix[i].length; j++)
+            if(matrix[i][0] == 0)
             {
-                if(row.contains(i) || col.contains(j)) matrix[i][j] = 0;
+                for(int j=0; j<matrix[i].length; j++) matrix[i][j] = 0;
             }
+        }
+        
+        //checking first element of all cols
+        for(int j=0; j<matrix[0].length; j++)
+        {
+            if(matrix[0][j] == 0)
+            {
+                for(int i=0; i<matrix.length; i++) matrix[i][j] = 0;
+            }
+        }
+        
+        //filling first row if matrix[0][j] = 0;
+        if(col)
+        {
+            for(int j=0; j<matrix[0].length; j++) matrix[0][j] = 0;
         }
     }
 }
