09ac3d687b97969895968bd0338331551b1f5f79
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-05-30 13:31:56 -0400

    Time: 31 ms (5.02%), Space: 53.9 MB (59.99%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,40 +1,26 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        boolean col = false;
+        List<Integer> row = new ArrayList<>();
+        List<Integer> col = new ArrayList<>();
+        
         for(int i=0; i<matrix.length; i++)
         {
-            if(matrix[i][0] == 0)
-            {
-                col = true;
-            }
-            for(int j=1; j<matrix[i].length; j++)
+            for(int j=0; j<matrix[i].length; j++)
             {
                 if(matrix[i][j] == 0)
                 {
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
+                    row.add(i);
+                    col.add(j);
                 }
             }
         }
         
-        for(int i=1; i<matrix.length; i++)
+        for(int i=0; i<matrix.length; i++)
         {
-            for(int j=1; j<matrix[i].length; j++)
+            for(int j=0; j<matrix[i].length; j++)
             {
-                if(matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
+                if(row.contains(i) || col.contains(j)) matrix[i][j] = 0;
             }
-        }
-        
-        //marking first row if it is zero.
-        if(matrix[0][0] == 0)
-        {
-            for(int i=0; i<matrix[0].length; i++) matrix[0][i] = 0;
-        }
-        
-        //marking first column if col is true which means first column should be marked 0.
-        if(col)
-        {
-            for(int i=0; i<matrix.length; i++) matrix[i][0] = 0;
         }
     }
 }
