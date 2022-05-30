b0b58682df3110ba1831ae615d3876283ac74d8d
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-05-30 16:02:38 -0400

    Time: 3 ms (22.25%), Space: 54.7 MB (6.01%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,14 +1,12 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        boolean col = false;
+        boolean firstCol = false;
         
-        for(int j=0; j<matrix[0].length; j++) if(matrix[0][j] == 0) col = true;
-        
-        for(int i=1; i<matrix.length; i++)
+        for(int i=0; i<matrix.length; i++)
         {
-            for(int j=0; j<matrix[i].length; j++)
+            if(matrix[i][0] == 0) firstCol = true;
+            for(int j=1; j<matrix[i].length; j++)
             {
-                
                 if(matrix[i][j] == 0)
                 {
                     matrix[i][0] = 0;
@@ -17,28 +15,25 @@ class Solution {
             }
         }
         
-        //checking first element of all rows
+        //checking first element of all rows and all cols from 1st row.
         for(int i=1; i<matrix.length; i++)
         {
-            if(matrix[i][0] == 0)
+            for(int j=1; j<matrix[0].length; j++)
             {
-                for(int j=0; j<matrix[i].length; j++) matrix[i][j] = 0;
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
             }
         }
         
-        //checking first element of all cols
-        for(int j=0; j<matrix[0].length; j++)
-        {
-            if(matrix[0][j] == 0)
-            {
-                for(int i=0; i<matrix.length; i++) matrix[i][j] = 0;
-            }
-        }
-        
-        //filling first row if matrix[0][j] = 0;
-        if(col)
+        //checking for firstCell
+        if(matrix[0][0] == 0)
         {
             for(int j=0; j<matrix[0].length; j++) matrix[0][j] = 0;
+        }
+        
+        //checking for first column
+        if(firstCol)
+        {
+            for(int i=0; i<matrix.length; i++) matrix[i][0] = 0;
         }
     }
 }
