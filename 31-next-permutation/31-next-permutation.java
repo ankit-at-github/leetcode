70fd78c8a538b4b11e0659ba889d380fbcec0f00
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-05-31 15:54:10 -0400

    Time: 1 ms (83.84%), Space: 43.5 MB (64.11%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -1,32 +1,41 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        //1.Find Pivot Index where longest increasing suffix breaks;
-        int pivotIndex = nums.length-2;
-        while(pivotIndex >=0 && nums[pivotIndex] >= nums[pivotIndex+1]) pivotIndex--;
-        
-        if(pivotIndex == -1) reverse(nums, 0, nums.length-1);
-        else
+        //1. Finding Pivot Index backward
+        int pivotIndex = -1;
+        for(int i=nums.length-1; i>=1; i--)
         {
-            //2. Find correct succesor of element at pivotIndex from right, more formally swap with element greater than nums[pivotIndex] as we next greater.
-            int swapIndex = nums.length-1;
-            while(swapIndex>=0 && nums[pivotIndex] >= nums[swapIndex]) swapIndex--;
-            
-            //3. Swap the elements
-            swap(nums, swapIndex, pivotIndex);
-            
-            //4. Reverse element from pivotIndex+1;
-            reverse(nums, pivotIndex+1, nums.length-1);
+            if(nums[i-1] < nums[i])
+            {
+                pivotIndex = i-1;
+                break;
+            }
         }
+        
+        if(pivotIndex >= 0)
+        {
+            //2. Finding Successor
+            int swapIndex = nums.length-1;
+            while(nums[swapIndex] <= nums[pivotIndex]) swapIndex--;
+            
+            //3. Swap the Elements
+            swap(nums, pivotIndex, swapIndex);
+            
+            //4. Reverse the elements from pivotIndex+1
+            reverse(nums, pivotIndex+1);
+        }
+        else
+            reverse(nums, 0);
     }
-    public void swap(int[] nums, int a, int b)
+    void swap(int[] nums, int a, int b)
     {
         int temp = nums[a];
         nums[a] = nums[b];
         nums[b] = temp;
     }
-    public void reverse(int[] nums, int start, int end)
+    void reverse(int[] nums, int start)
     {
-        while(start<end)
+        int end = nums.length-1;
+        while(start < end)
         {
             swap(nums, start++, end--);
         }
