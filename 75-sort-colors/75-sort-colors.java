2e201a658afbaec3324077dbde8c1c981e89589f
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-06-04 16:40:45 -0400

    Time: 0 ms (100.00%), Space: 42.8 MB (19.52%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,25 +1,23 @@
 class Solution {
     public void sortColors(int[] nums) {
-        //Solving this using 3 pointer approach. This approach can be used to solve any 3 distinct elements present in an array.
-        
-        int low = 0, mid = 0, high = nums.length-1;
-        
-        while(mid <= high)
+        int start = 0, mid = 0, end = nums.length-1;
+        while(mid <= end)
         {
             if(nums[mid] == 0)
             {
-                swap(nums, low, mid);
-                low++;
+                swap(nums, start, mid);
+                start++;
                 mid++;
             }
+            
             else if(nums[mid] == 1) mid++;
+            
             else
             {
-                swap(nums, mid, high);
-                high--;
+                swap(nums, mid, end);
+                end--;
             }
         }
-        
     }
     public void swap(int[] nums, int a, int b)
     {
