625fa3b126e63bca3ef53ccd8c7a09a803c9b4f4
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-06-05 14:50:01 -0400

    Time: 0 ms (100.00%), Space: 42.6 MB (60.13%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -1,41 +1,38 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        transpose(matrix);
-        reverse(matrix);
-    }
-    public void transpose(int[][] matrix)
-    {
-        int row = matrix.length;
-        int col = matrix[0].length;
-        
-        for(int i=0; i<row; i++)
-        {
-            for(int j=i; j<col; j++)
-            {
-                swap(matrix, i, j);
-            }
-        }
-    }
-    public void reverse(int[][] matrix)
-    {
-        for(int i=0; i<matrix.length; i++)
-        {
-            int start = 0;
-            int end = matrix[i].length-1;
-            while(start < end)
-            {
-                int temp = matrix[i][start];
-                matrix[i][start] = matrix[i][end];
-                matrix[i][end] = temp;
-                start++;
-                end--;
-            }
-        }
+        int n = matrix.length;
+        transpose(matrix, n);
+        reverse(matrix, n);
     }
     public void swap(int[][] matrix, int i, int j)
     {
         int temp = matrix[i][j];
         matrix[i][j] = matrix[j][i];
         matrix[j][i] = temp;
+    }
+    public void transpose(int[][] matrix, int n)
+    {
+        for(int i=0; i<n; i++)
+        {
+            for(int j=i+1; j<n; j++)
+            {
+                swap(matrix, i, j);
+            }
+        }
+    }
+    public void reverse(int[][] matrix, int n)
+    {
+        int start = 0, end = matrix[0].length-1;
+        while(start <= end)
+        {
+            for(int i=0; i<n; i++)
+            {
+                int temp = matrix[i][start];
+                matrix[i][start] = matrix[i][end];
+                matrix[i][end] = temp;
+            }
+            start++;
+            end--;
+        }
     }
 }
