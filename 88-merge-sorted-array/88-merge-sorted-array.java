4c31d25382154b6d611c4fd4d3d71072e60bd388
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-06-06 21:03:58 -0400

    Time: 0 ms (100.00%), Space: 42.5 MB (65.17%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -6,13 +6,12 @@ class Solution {
         {
             if(nums1[i] > nums2[j])
             {
-                nums1[index] = nums1[i--];
+                nums1[index--] = nums1[i--];
             }
             else
             {
-                nums1[index] = nums2[j--];
+                nums1[index--] = nums2[j--];
             }
-            index--;
         }
         while(i>=0) nums1[index--] = nums1[i--];
         while(j>=0) nums1[index--] = nums2[j--];
