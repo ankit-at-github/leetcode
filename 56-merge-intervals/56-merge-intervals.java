b58b07b731e390c63338332126e987c982fae384
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-06-08 00:13:33 -0400

    Time: 17 ms (21.64%), Space: 55.8 MB (7.90%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,41 +1,37 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(intervals, (a,b) -> a[0]-b[0]);
         
-        int[] start = new int[10001];
-        int[] end = new int[10001];
+        ArrayList<ArrayList<Integer>> mergedIntervals = new ArrayList<>();
         
-        for(int i=0; i<intervals.length; i++)
+        ArrayList<Integer> current = new ArrayList<>();
+        current.add(intervals[0][0]);
+        
+        int end = intervals[0][1];
+        for(int i=1; i<intervals.length; i++)
         {
-            start[intervals[i][0]]++;
-            end[intervals[i][1]]--;
-        }
-        
-        int count = 0;
-        List<Integer> current = new ArrayList<>();
-        for(int i=0; i<10001; i++)
-        {
-            count+=start[i];
-            if(count != 0)
+            if(intervals[i][0] > end)
             {
-                if(current.size()==0) current.add(i);
-                count+=end[i];
-                if(count == 0)
-                {
-                    current.add(i);
-                    ans.add(current);
-                    current = new ArrayList();
-                }
+                current.add(end);
+                mergedIntervals.add(current);
+                current = new ArrayList<>();
+                current.add(intervals[i][0]);
             }
+            end = Math.max(end, intervals[i][1]);
         }
-        
-        int[][] output = new int[ans.size()][2];
-        
-        for(int i=0; i<ans.size(); i++)
+        if(current.size() != 0)
         {
-            output[i][0] = ans.get(i).get(0);
-            output[i][1] = ans.get(i).get(1);
+            current.add(end);
+            mergedIntervals.add(current);
         }
-        return output;
+        int[][] ans = new int[mergedIntervals.size()][2];
+        int index = 0;
+        for(List<Integer> l : mergedIntervals)
+        {
+            ans[index][0] = l.get(0);
+            ans[index][1] = l.get(1);
+            index++;
+        }
+        return ans;
     }
 }
