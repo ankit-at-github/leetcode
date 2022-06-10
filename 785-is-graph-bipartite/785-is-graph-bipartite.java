ebdb3d088dbd036b5e6a93f725b510764d7abeaf
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-06-10 18:51:22 -0400

    Time: 3 ms (29.88%), Space: 53.7 MB (52.99%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -1,43 +1,46 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
-        int n = graph.length;
-        
-        int[] color = new int[n];
+        int[] color = new int[graph.length];
         Arrays.fill(color, -1);
         
-        for(int i=0; i<n; i++)
+        //Need to check for each node
+        for(int i=0; i<graph.length; i++)
         {
             if(color[i] == -1)
             {
                 if(!bfs(graph, i, color)) return false;
+                
             }
         }
         return true;
     }
-    public boolean bfs(int[][] graph, int index, int[] color)
+    public boolean bfs(int[][] graph, int node, int[] color)
     {
-        color[index] = 1;
+        color[node] = 0;
         
+        //Traversing graph
         Queue<Integer> q = new LinkedList<>();
-        q.add(index);
+        q.add(node);
         
         while(!q.isEmpty())
         {
-            int node = q.poll();
-            for(int connectedNode: graph[node])
+            int currentNode = q.poll();
+            //Getting the adjacent nodes to current node
+            for(int adjacentNode : graph[currentNode])
             {
-                if(color[connectedNode] != -1)
+                //checking if adjacent node is marked or not
+                if(color[adjacentNode] == 0 || color[adjacentNode] == 1)
                 {
-                    if(color[connectedNode] == color[node]) return false;
+                    if(color[adjacentNode] == color[currentNode]) return false;
                 }
                 else
                 {
-                    color[connectedNode] = 1-color[node];
-                    q.add(connectedNode);
+                    if(color[currentNode] == 0) color[adjacentNode] = 1;
+                    else color[adjacentNode] = 0;
+                    q.add(adjacentNode);
                 }
             }
         }
-        
         return true;
     }
 }
