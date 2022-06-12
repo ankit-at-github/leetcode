2b57a60a01412e43b9b2a5d38ccb07cd5b6fffbe
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-06-12 18:08:39 -0400

    Time: 0 ms (100.00%), Space: 38.7 MB (99.74%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,22 +1,19 @@
 class Solution {
-    public int solve(int m, int n, int[][] dp)
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for(int i=0; i<m; i++) dp[i][0] = 1;
+        for(int j=0; j<n; j++) dp[0][j] = 1;
+        return dpTabularBottomUpApproach(m, n, dp);
+    }
+    public int dpTabularBottomUpApproach(int m, int n, int[][] dp)
     {
         for(int i=1; i<m; i++)
         {
             for(int j=1; j<n; j++)
             {
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                dp[i][j] = dp[i][j-1] + dp[i-1][j];
             }
         }
         return dp[m-1][n-1];
-    }
-    public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
-        
-        for(int i=0; i<m; i++) dp[i][0] = 1;
-        for(int i=0; i<n; i++) dp[0][i] = 1;
-        
-        return solve(m, n, dp);
-        
     }
 }
