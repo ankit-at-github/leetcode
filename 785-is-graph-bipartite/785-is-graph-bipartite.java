e2bfc61c1c6cb6552a4e80367a0c6a2bcad35578
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-06-13 13:29:11 -0400

    Time: 1 ms (81.66%), Space: 54 MB (34.48%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -3,44 +3,31 @@ class Solution {
         int[] color = new int[graph.length];
         Arrays.fill(color, -1);
         
-        //Need to check for each node
         for(int i=0; i<graph.length; i++)
         {
             if(color[i] == -1)
             {
-                if(!bfs(graph, i, color)) return false;
-                
+                color[i] = 0;
+                if(!checkDFS(graph, i, color)) return false;
             }
         }
         return true;
     }
-    public boolean bfs(int[][] graph, int node, int[] color)
+    public boolean checkDFS(int[][] graph, int node, int[] color)
     {
-        color[node] = 0;
-        
-        //Traversing graph
-        Queue<Integer> q = new LinkedList<>();
-        q.add(node);
-        
-        while(!q.isEmpty())
-        {
-            int currentNode = q.poll();
-            //Getting the adjacent nodes to current node
-            for(int adjacentNode : graph[currentNode])
-            {
-                //checking if adjacent node is marked or not
-                if(color[adjacentNode] == 0 || color[adjacentNode] == 1)
-                {
-                    if(color[adjacentNode] == color[currentNode]) return false;
-                }
-                else
-                {
-                    if(color[currentNode] == 0) color[adjacentNode] = 1;
-                    else color[adjacentNode] = 0;
-                    q.add(adjacentNode);
-                }
-            }
-        }
+       for(int adjacentNode : graph[node])
+       {
+           if(color[adjacentNode] == -1)
+           {
+               if(color[node] == 0) color[adjacentNode] = 1;
+               else color[adjacentNode] = 0;
+               if(!checkDFS(graph, adjacentNode, color)) return false;
+           }
+           else
+           {
+               if(color[adjacentNode] == color[node]) return false;
+           }
+       }
         return true;
     }
 }
