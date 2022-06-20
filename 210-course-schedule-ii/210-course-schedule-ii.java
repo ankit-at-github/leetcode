566a8581d1567a7425e64b51579cba915a6fcd32
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-06-20 01:28:03 -0400

    Time: 8 ms (66.29%), Space: 48.9 MB (64.79%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.java
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -1,55 +1,46 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        //1.Creating graph
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         for(int i=0; i<numCourses; i++) adj.add(new ArrayList<Integer>());
         
-        for(int[] course : prerequisites)
+        int[] indegree = new int[numCourses];
+        
+        //creating graph and marking in-degree
+        for(int[] pre : prerequisites)
         {
-            int u = course[1];
-            int v = course[0];
+            int u = pre[1];
+            int v = pre[0];
             adj.get(u).add(v);
+            indegree[v]++;
         }
         
-        boolean[] visited = new boolean[numCourses];
-        boolean[] dfsVisited = new boolean[numCourses];
+        //adding nodes that has indegree as 0, to start with
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0; i<numCourses; i++) if(indegree[i] == 0) q.add(i);
         
-        Stack<Integer> order = new Stack<>();
-        int[] ans = {};
-        
-        for(int i=0; i<numCourses; i++)
+        ArrayList<Integer> order = new ArrayList<>();
+        while(!q.isEmpty())
         {
-            if(!visited[i])
+            for(int i=q.size(); i>0; i--)
             {
-                if(checkCycleDFS(adj, i, visited, dfsVisited, order)) return ans;
+                int node = q.poll();
+                order.add(node);
+                numCourses--;
+                for(int adjacentNodes : adj.get(node))
+                {
+                    indegree[adjacentNodes]--;
+                    if(indegree[adjacentNodes] == 0) q.add(adjacentNodes);
+                }
             }
         }
         
-        ans = new int[order.size()];
+        int[] result = {};
+        
+        if(numCourses!=0) return result;
+        result = new int[order.size()];
         int index = 0;
+        for(int x : order) result[index++] = x;
         
-        while(!order.isEmpty()) ans[index++] = order.pop();
-        
-        return ans;
-    }
-    public boolean checkCycleDFS(ArrayList<ArrayList<Integer>> adj, int node, boolean[] visited, boolean[] dfsVisited, Stack<Integer> order)
-    {
-        visited[node] = true;
-        dfsVisited[node] = true;
-        
-        for(int adjacentNode : adj.get(node))
-        {
-            if(!visited[adjacentNode])
-            {
-                if(checkCycleDFS(adj, adjacentNode, visited, dfsVisited, order)) return true;
-            }
-            else
-            {
-                if(visited[adjacentNode] && dfsVisited[adjacentNode]) return true;
-            }
-        }
-        dfsVisited[node] = false;
-        order.add(node);
-        return false;
+        return result;
     }
 }
