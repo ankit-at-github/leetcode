80f8f33fa6cfe4f532780f01c81ee1e880ec1ab2
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-06-21 14:45:10 -0400

    Time: 38 ms (44.09%), Space: 58.3 MB (75.15%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,23 +1,24 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
         
-        //At any point if nums[i] > 0 then sum of three elements cant be zero as we check next pair of elements from that index. Since, elements are alreayd sorted then all the elements after that index will be greater than 0.
-        for(int i=0; i<nums.length && nums[i]<=0; i++)
+        if(nums.length == 0) return res;
+        
+        Arrays.sort(nums);
+        
+        for(int i=0; i<nums.length; i++)
         {
-            //skip duplicate elemets
-            if(i > 0 && nums[i-1]==nums[i]) continue;
+            //skip duplicates
+            if(i > 0 && nums[i-1] == nums[i]) continue;
             
             twoSumPointer(nums, i, res);
-            
         }
         return res;
     }
     public void twoSumPointer(int[] nums, int index, List<List<Integer>> res)
     {
         int i=index+1, j=nums.length-1;
-        while(i<j)
+        while(i < j)
         {
             int sum = nums[index] + nums[i] + nums[j];
             if(sum < 0) i++;
@@ -25,10 +26,13 @@ class Solution {
             else
             {
                 res.add(Arrays.asList(nums[index], nums[i], nums[j]));
-                i++;j--;
-                while(i<j && nums[i]==nums[i-1] && nums[j]==nums[j+1])
+                i++;
+                j--;
+                //skip duplicates
+                while(i < j && nums[i-1]==nums[i] && nums[j] == nums[j+1])
                 {
-                    i++;j--;
+                    i++;
+                    j--;
                 }
             }
         }
