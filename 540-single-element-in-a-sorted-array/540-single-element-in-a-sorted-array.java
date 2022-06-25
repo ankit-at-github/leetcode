a9c5e4d9b24fb290146a4886f508932eda658402
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-06-25 18:32:24 -0400

    Time: 0 ms (100.00%), Space: 61.4 MB (5.95%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
@@ -5,23 +5,10 @@ class Solution {
         {
             int mid = low + (high-low)/2;
             
-            //Now check if you are one the right side of violated element or left side.
-            //Every pair starts from even index and the pair of index becomes (even-odd), however
-            //as soon as you encounter violated element this changes and pair of index becomes (odd-even)
-            //Using this fact.
-            if(mid%2 == 0)
-            {
-                //Following normal convention (even-odd) pair violated element is on right side
-                //else not following violated element is on left side.
-                
-                if(nums[mid] == nums[mid+1]) low = mid+2;
-                else high = mid;
-            }
-            else
-            {
-                if(nums[mid] == nums[mid-1]) low = mid+1;
-                else high = mid;
-            }
+            if(mid%2 == 1) mid--;
+            
+            if(nums[mid] == nums[mid+1]) low = mid+2;
+            else high = mid;
         }
         return nums[low];
     }
