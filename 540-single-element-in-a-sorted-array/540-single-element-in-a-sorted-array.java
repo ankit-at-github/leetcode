e8716d5d9a254fcbc63f72c20882418b7c9fa802
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-06-25 18:28:15 -0400

    Time: 0 ms (100.00%), Space: 61 MB (13.46%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
@@ -1,7 +1,28 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int ans = 0;
-        for(int x : nums) ans^=x;
-        return ans;
+        int low = 0, high = nums.length-1;
+        while(low < high)
+        {
+            int mid = low + (high-low)/2;
+            
+            //Now check if you are one the right side of violated element or left side.
+            //Every pair starts from even index and the pair of index becomes (even-odd), however
+            //as soon as you encounter violated element this changes and pair of index becomes (odd-even)
+            //Using this fact.
+            if(mid%2 == 0)
+            {
+                //Following normal convention (even-odd) pair violated element is on right side
+                //else not following violated element is on left side.
+                
+                if(nums[mid] == nums[mid+1]) low = mid+2;
+                else high = mid;
+            }
+            else
+            {
+                if(nums[mid] == nums[mid-1]) low = mid+1;
+                else high = mid;
+            }
+        }
+        return nums[low];
     }
 }
