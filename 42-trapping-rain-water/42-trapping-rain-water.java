8d9d0d718ff07bc433fbecca6e30d3b809ecd2dc
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-06-24 20:14:48 -0400

    Time: 2 ms (54.44%), Space: 49.4 MB (13.60%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,25 +1,20 @@
 class Solution {
     public int trap(int[] height) {
-        //Brute Force, water stored at each index;
-        int leftMax = height[0], rightMax = 0, sum = 0;
-        int[] trappedWater = new int[height.length];
+        int[] prefixMax = new int[height.length];
+        prefixMax[0] = height[0];
+        for(int i=1; i<height.length; i++) prefixMax[i] = Math.max(prefixMax[i-1], height[i]);
         
-        for(int i=1; i<height.length; i++)
+        int[] suffixMax = new int[height.length];
+        suffixMax[height.length-1] = height[height.length-1];
+        for(int i=height.length-2; i>=0; i--) suffixMax[i] = Math.max(suffixMax[i+1], height[i]);
+        
+        int sum = 0;
+        for(int i=0; i<height.length; i++)
         {
-            leftMax = Math.max(leftMax, height[i]);
-            
-            rightMax = Math.max(height[i], getRightMax(height, i));
-            
-            trappedWater[i] = Math.min(leftMax, rightMax) - height[i];
-            
-            sum = sum + (trappedWater[i] > 0?trappedWater[i]:0);
+            int value = Math.min(prefixMax[i], suffixMax[i]) - height[i];
+            sum = sum + (value>0?value:0);
         }
         return sum;
-    }
-    public int getRightMax(int[] height, int index)
-    {
-        int maxHeight = 0;
-        for(int j=index+1; j<height.length; j++) maxHeight = Math.max(maxHeight, height[j]);
-        return maxHeight;
+        
     }
 }
