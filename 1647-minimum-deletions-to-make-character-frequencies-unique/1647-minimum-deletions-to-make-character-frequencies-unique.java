7989d83359a17d32c8b4732f94080c9cc6bdf60e
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-06-28 13:01:41 -0400

    Time: 20 ms (48.70%), Space: 55 MB (29.52%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -3,21 +3,23 @@ class Solution {
         int[] freq = new int[26];
         for(int i=0; i<s.length(); i++) freq[s.charAt(i)-'a']++;
         
-        Set<Integer> set = new HashSet<>();
-        int deletion = 0;
-        for(int i=0; i<26; i++)
-        {
-            if(set.contains(freq[i]))
-            {
-                while(freq[i] > 0 && set.contains(freq[i]))
-                {
-                    freq[i]--;
-                    deletion++;
-                }
-            }
-            set.add(freq[i]);
-        }
+        Arrays.sort(freq);
         
-        return deletion;
+        //The expectation of next frequency would be less than the previous frequency seen.
+        //Re-adjust the frequency if current frequency is less than expected frequency.
+        int exp = freq[25]-1;
+        int deleteCount = 0;
+        for(int i=24; i>=0; i--)
+        {
+            //before this all elements are zero so break;
+            if(freq[i] == 0) break;
+            
+            if(freq[i] > exp) deleteCount+= freq[i] - exp;
+            else exp = freq[i];
+            
+            if(exp > 0) exp--; //Lowest expectation can be zero, not negative.
+            
+        }
+        return deleteCount;
     }
 }
