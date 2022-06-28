b07abe6d82307913a20f237d03725a0db18da3ea
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-06-27 21:01:51 -0400

    Time: 1 ms (89.56%), Space: 48.2 MB (30.15%) - LeetHub

--- 733-flood-fill/733-flood-fill.java
+++ 733-flood-fill/733-flood-fill.java
@@ -3,44 +3,25 @@ class Solution {
         int m = image.length;
         int n = image[0].length;
         
-        boolean[][] visited = new boolean[m][n];
-        
         int oldColor = image[sr][sc];
         
-        if(oldColor != color) dfs(image, sr, sc, color, oldColor, visited);
+        if(oldColor != color) dfs(image, sr, sc, color, oldColor);
         
         return image;
     }
-    public void dfs(int[][] image, int x, int y, int color, int oldColor, boolean[][] visited)
+    public void dfs(int[][] image, int x, int y, int color, int oldColor)
     {
-        image[x][y] = color;
-        visited[x][y] = true;
+        if(x < 0 || x >= image.length || y < 0 || y >= image[0].length || image[x][y] != oldColor) return;
         
-        //UP
-        if(validPath(image, x-1, y, visited, oldColor))
-        {
-            dfs(image, x-1, y, color, oldColor, visited);
-        }
-          //Down
-        if(validPath(image, x+1, y, visited, oldColor))
-        {
-            dfs(image, x+1, y, color, oldColor, visited);
-        }
-          //Left
-        if(validPath(image, x, y-1, visited, oldColor))
-        {
-            dfs(image, x, y-1, color, oldColor, visited);
-        }
-          //Right
-        if(validPath(image, x, y+1, visited, oldColor))
-        {
-            dfs(image, x, y+1, color, oldColor, visited);
-        }
-    }
-    public boolean validPath(int[][] image, int x, int y, boolean[][] visited, int oldColor)
-    {
-        if(x < 0 || x >= image.length || y < 0 || y >= image[0].length || 
-           image[x][y] != oldColor || visited[x][y] == true) return false;
-        return true;
+        image[x][y] = color;
+        
+        //Up
+        dfs(image, x-1, y, color, oldColor);
+        //Down
+        dfs(image, x+1, y, color, oldColor);
+        //Left
+        dfs(image, x, y-1, color, oldColor);
+        //Right
+        dfs(image, x, y+1, color, oldColor);
     }
 }
