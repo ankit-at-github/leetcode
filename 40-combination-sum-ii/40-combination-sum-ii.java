f2d23661ed829a3ec98b581ec8d3566b2502a92c
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-06-29 18:49:37 -0400

    Time: 6 ms (57.04%), Space: 44.4 MB (23.09%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.java
+++ 40-combination-sum-ii/40-combination-sum-ii.java
@@ -1,26 +1,27 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> output = new ArrayList<>();
         Arrays.sort(candidates);
-        
-        List<List<Integer>> output = new ArrayList<>();
-        
-        solve(candidates, 0, new ArrayList(), output, target);
-        
-        return output;
+        solve(candidates, 0, target, output, ans);
+        return ans;
     }
-    public void solve(int[] candidates, int index, List<Integer> temp, List<List<Integer>> output, int target)
+    public void solve(int[] candidates, int index, int target, List<Integer> output, List<List<Integer>> ans)
     {
-        if(target == 0) output.add(new ArrayList(temp));
-        
+        if(target == 0)
+        {
+            ans.add(new ArrayList(output));
+            return;
+        }
         for(int i=index; i<candidates.length; i++)
         {
-            if((i > index && candidates[i] == candidates[i-1])) continue;
-            if(target < candidates[i]) break;
+            if(target-candidates[i] < 0) break;
             
-            temp.add(candidates[i]);
-            solve(candidates, i+1, temp, output, target-candidates[i]);
-            temp.remove(temp.size()-1);
+            if(i!=index && candidates[i] == candidates[i-1]) continue;
+            
+            output.add(candidates[i]);
+            solve(candidates, i+1, target-candidates[i], output, ans);
+            output.remove(output.size()-1);
         }
     }
 }
