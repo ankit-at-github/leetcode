adede280ac26e48a35e220de1e7d2e82499345cc
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-06-30 16:45:52 -0400

    Time: 1 ms (97.91%), Space: 42.3 MB (94.01%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,23 +1,30 @@
 class Solution {
-    public void solve(int[] nums, List<Integer> output, List<List<Integer>> ans)
-    {
-        if(output.size() == nums.length) ans.add(new ArrayList(output));
-        
-        for(int i=0; i<nums.length; i++)
-        {
-            if(output.contains(nums[i])) continue;
-            
-            output.add(nums[i]);
-            solve(nums, output, ans);
-            output.remove(output.size()-1);
-        }
-    }
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> output = new ArrayList<>();
         
-        solve(nums, output, ans);
+        boolean[] visited = new boolean[nums.length];
+        
+        permutation(nums, 0, visited, output, ans);
         
         return ans;
+    }
+    public void permutation(int[] nums, int index, boolean[] visited, List<Integer> output, List<List<Integer>> ans)
+    {
+        if(output.size() == nums.length)
+        {
+            ans.add(new ArrayList(output));
+            return;
+        }
+        for(int i=0; i<nums.length; i++)
+        {
+            if(visited[i]) continue;
+            
+            visited[i] = true;
+            output.add(nums[i]);
+            permutation(nums, 0, visited, output, ans);
+            output.remove(output.size()-1);
+            visited[i] = false;
+        }
     }
 }
