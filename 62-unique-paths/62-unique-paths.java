470f0492803e8e788328637e28790990ca95cefe
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-06-29 20:10:07 -0400

    Time: 1 ms (50.39%), Space: 41.3 MB (22.59%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -3,10 +3,7 @@ class Solution {
         int[][] dp = new int[m][n];
         for(int i=0; i<m; i++) dp[i][0] = 1;
         for(int j=0; j<n; j++) dp[0][j] = 1;
-        return dpTabularBottomUpApproach(m, n, dp);
-    }
-    public int dpTabularBottomUpApproach(int m, int n, int[][] dp)
-    {
+        
         for(int i=1; i<m; i++)
         {
             for(int j=1; j<n; j++)
