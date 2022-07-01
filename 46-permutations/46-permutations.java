55a4b6dfe654453d102cd8993a49a6e49e6a5504
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-07-01 10:49:53 -0400

    Time: 3 ms (43.82%), Space: 44.4 MB (67.32%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -3,13 +3,11 @@ class Solution {
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> output = new ArrayList<>();
         
-        boolean[] visited = new boolean[nums.length];
-        
-        permutation(nums, 0, visited, output, ans);
+        permutation(nums, 0, output, ans);
         
         return ans;
     }
-    public void permutation(int[] nums, int index, boolean[] visited, List<Integer> output, List<List<Integer>> ans)
+    public void permutation(int[] nums, int index, List<Integer> output, List<List<Integer>> ans)
     {
         if(output.size() == nums.length)
         {
@@ -18,13 +16,11 @@ class Solution {
         }
         for(int i=0; i<nums.length; i++)
         {
-            if(visited[i]) continue;
+            if(output.contains(nums[i])) continue;
             
-            visited[i] = true;
             output.add(nums[i]);
-            permutation(nums, 0, visited, output, ans);
+            permutation(nums, 0, output, ans);
             output.remove(output.size()-1);
-            visited[i] = false;
         }
     }
 }
