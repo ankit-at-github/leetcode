e66651d0f27af8bcbecf1b27e0679bb4632724b4
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-07-01 22:45:52 -0400

    Time: 3 ms (89.28%), Space: 45.4 MB (50.49%) - LeetHub

--- 51-n-queens/51-n-queens.java
+++ 51-n-queens/51-n-queens.java
@@ -1,39 +1,72 @@
 class Solution {
-    
-    private Set<Integer> col = new HashSet<Integer>();
-    private Set<Integer> diag1 = new HashSet<Integer>();
-    private Set<Integer> diag2 = new HashSet<Integer>();
-    
-    public List<List<String>> solveNQueens(int n){
-        List<List<String>> res = new ArrayList<List<String>>();
-        dfs(res,new ArrayList<String>(), 0, n);
-        return res;
+    public List<List<String>> solveNQueens(int n) {
+        char[][] chessBoard = new char[n][n];
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<n; j++)
+                chessBoard[i][j] = '.';
+        }
+        
+        List<List<String>> ans = new ArrayList<>();
+        
+        solve(0, chessBoard, ans, n);
+        
+        return ans;
     }
-    private void dfs(List<List<String>> res, List<String> list, int row, int n){
-        if (row == n){
-            res.add(new ArrayList<String>(list));
+    public boolean isSafe(int row, int col, char[][] chessBoard, int n)
+    {
+        int rowCopy = row;
+        int colCopy = col;
+        
+        //left-up diagonal
+        while(rowCopy >=0 && colCopy >=0)
+        {
+            if(chessBoard[rowCopy][colCopy] == 'Q') return false;
+            rowCopy--;
+            colCopy--;
+        }
+        
+        //left
+        rowCopy = row;
+        colCopy = col;
+        while(colCopy >= 0)
+        {
+            if(chessBoard[rowCopy][colCopy] == 'Q') return false;
+            colCopy--;
+        }
+        
+        //left-down diagonal
+        rowCopy = row;
+        colCopy = col;
+        while(rowCopy < n && colCopy >=0)
+        {
+            if(chessBoard[rowCopy][colCopy] == 'Q') return false;
+            rowCopy++;
+            colCopy--;
+        }
+        return true;
+    }
+    public void solve(int col, char[][] chessBoard, List<List<String>> ans, int n)
+    {
+        if(col == n)
+        {
+            List<String> res = new ArrayList<>();
+            for(int i=0; i<n; i++)
+            {
+                String s = new String(chessBoard[i]);
+                res.add(s);
+            }
+            ans.add(res);
             return;
         }
-        for (int i = 0; i < n; i++)
+        for(int row=0; row<n; row++)
         {
-            if (col.contains(i) || diag1.contains(row + i) || diag2.contains(row - i)) continue;
-            
-            char[] charArray = new char[n];
-            Arrays.fill(charArray, '.');
-            charArray[i] = 'Q';
-            String rowString = new String(charArray);
-            
-            list.add(rowString);
-            col.add(i);
-            diag1.add(row + i);
-            diag2.add(row - i);
-            
-            dfs(res, list, row + 1, n);
-            
-            list.remove(list.size() - 1);
-            col.remove(i);
-            diag1.remove(row + i);
-            diag2.remove(row - i);
+            if(isSafe(row, col, chessBoard, n))
+            {
+                chessBoard[row][col] = 'Q';
+                solve(col+1, chessBoard, ans, n);
+                chessBoard[row][col] = '.';
+            }
         }
     }
 }
