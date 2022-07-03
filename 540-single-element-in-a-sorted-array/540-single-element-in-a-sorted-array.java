a34870f2b6dae4d063a2027304eee34e058c1cd6
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-07-03 19:41:13 -0400

    Time: 0 ms (100.00%), Space: 60.4 MB (56.67%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
@@ -3,13 +3,21 @@ class Solution {
         int low = 0, high = nums.length-1;
         while(low < high)
         {
+            
             int mid = low + (high-low)/2;
-            
-            if(mid%2 == 1) mid--;
-            
-            if(nums[mid] == nums[mid+1]) low = mid+2;
-            else high = mid;
+            //since mid is even so for correct pair next element should be same at odd position, and violater is on right side.
+            if(mid%2 == 0)
+            {
+                if(nums[mid] == nums[mid+1]) low = mid+2;
+                else high = mid;
+            }
+            else
+            {
+                if(nums[mid] == nums[mid+1]) high = mid;
+                else low = mid+1;
+            }
         }
+        
         return nums[low];
     }
 }
