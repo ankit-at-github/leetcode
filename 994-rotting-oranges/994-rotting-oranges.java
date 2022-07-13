3b7131915f930fa011cf36c7661e69491aa701a6
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-07-13 15:13:15 -0400

    Time: 2 ms (96.16%), Space: 42 MB (85.40%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.java
+++ 994-rotting-oranges/994-rotting-oranges.java
@@ -1,78 +1,72 @@
 class Solution {
-    public boolean checkPath(int[][] grid, int x, int y)
-    {
-        if((x>=0 && x<grid.length) && (y>=0 && y<grid[0].length) && grid[x][y] == 1) return true;
-        else return false;
-    }
     public int orangesRotting(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
         
-        Queue<Pair<Integer, Integer>> queue = new LinkedList<>();
         int freshOranges = 0;
-        
-        /*getting location of all rotten oranges in queue,
-        and count of all freshOranges*/
-        for(int i=0; i<grid.length; i++)
+        Queue<int[]> q = new LinkedList<>();
+        //Storing all rotten oranges place
+        for(int i=0; i<m; i++)
         {
-            for(int j=0; j<grid[0].length; j++)
+            for(int j=0; j<n; j++)
             {
+                if(grid[i][j] == 2) q.add(new int[]{i,j});
                 if(grid[i][j] == 1) freshOranges++;
-                else if(grid[i][j] == 2) queue.add(new Pair(i, j));
             }
         }
         
         if(freshOranges == 0) return 0;
         
         int time = -1;
-        while(!queue.isEmpty())
+        while(!q.isEmpty())
         {
-            int size = queue.size();
-            time++;
-            for(int i=0; i<size; i++)
+            for(int i=q.size(); i>0; i--)
             {
-                Pair<Integer, Integer> p = queue.poll();
-                int x = p.getKey();
-                int y = p.getValue();
+                int x = q.peek()[0];
+                int y = q.poll()[1];
                 
-                //left
-                int newX = x;
-                int newY = y-1;
-                if(checkPath(grid, newX, newY))
+                //Up
+                if(isSafe(grid, x-1, y, m, n))
                 {
-                    freshOranges--;
-                    grid[newX][newY] = 2;
-                    queue.add(new Pair(newX, newY));
+                    grid[x-1][y] = 2;
+                    q.add(new int[]{x-1, y});
+                }
+                //left
+                if(isSafe(grid, x, y-1, m, n))
+                {
+                    grid[x][y-1] = 2;
+                    q.add(new int[]{x, y-1});
                 }
                 //right
-                newX = x;
-                newY = y+1;
-                if(checkPath(grid, newX, newY))
+                if(isSafe(grid, x, y+1, m, n))
                 {
-                    freshOranges--;
-                    grid[newX][newY] = 2;
-                    queue.add(new Pair(newX, newY));
+                    grid[x][y+1] = 2;
+                    q.add(new int[]{x, y+1});
                 }
-                //top
-                newX = x-1;
-                newY = y;
-                if(checkPath(grid, newX, newY))
+                //down
+                if(isSafe(grid, x+1, y, m, n))
                 {
-                    freshOranges--;
-                    grid[newX][newY] = 2;
-                    queue.add(new Pair(newX, newY));
+                    grid[x+1][y] = 2;
+                    q.add(new int[]{x+1, y});
                 }
-                //bottom
-                newX = x+1;
-                newY = y;
-                if(checkPath(grid, newX, newY))
-                {
-                    freshOranges--;
-                    grid[newX][newY] = 2;
-                    queue.add(new Pair(newX, newY));
-                }
+                
+            }
+            time++;
+        }
+        
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(grid[i][j] == 1) return -1;
             }
         }
-        return freshOranges == 0 ? time : -1;
+        
+        return time;
+    }
+    public boolean isSafe(int[][] grid, int x, int y, int m, int n)
+    {
+        if(x < 0 || x >=m || y < 0 || y >= n || grid[x][y] == 0 || grid[x][y] == 2) return false;
+        return true;
     }
 }
