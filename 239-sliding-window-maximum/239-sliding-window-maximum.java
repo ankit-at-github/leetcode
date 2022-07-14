b0225c78b7de9e36529a2a1f286758bc632f983f
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-07-14 11:25:27 -0400

    Time: 57 ms (56.85%), Space: 143.6 MB (64.89%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.java
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -1,24 +1,21 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> b[0]-a[0]);
-        int left=0, right=0;
+        Deque<Integer> dq = new ArrayDeque<>();
         int[] ans = new int[nums.length-k+1];
+        int left = 0, right = 0;
         while(right < nums.length)
         {
-            if(right-left < k)
-            {
-                pq.add(new int[]{nums[right], right});
-                right++;
-            }
-            if(right-left == k)
-            {
-                ans[left++] = pq.peek()[0];
-            }
+            //remove index if it is outside of range K
+            while(!dq.isEmpty() && dq.peekFirst() < (right - k +1)) dq.pollFirst();
             
-            //If max element is out of window, remove all element from left till the index where max element was present.
-            while(!pq.isEmpty() && pq.peek()[1] < left) pq.poll();
+            while(!dq.isEmpty() && nums[right] > nums[dq.peekLast()]) dq.pollLast();
+            
+            dq.addLast(right);
+            
+            if(right-left+1 == k) ans[left++] = nums[dq.peekFirst()];
+            
+            right++;
         }
-        
         return ans;
     }
 }
