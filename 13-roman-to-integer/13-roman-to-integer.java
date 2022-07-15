3339d35b84fcd08c42aa1fd085f5d39ae94be0ce
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-07-15 16:22:24 -0400

    Time: 19 ms (13.96%), Space: 47 MB (23.81%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.java
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -1,77 +1,19 @@
 class Solution {
     public int romanToInt(String s) {
-        int value = 0;
-        int i = s.length()-1;
-        for(; i>=1; i--)
-        {
-            if(s.charAt(i) == 'I')
-                value+=1;
-            else if(s.charAt(i) == 'V')
-            {
-                if(s.charAt(i-1) == 'I')
-                {
-                    value+=4;
-                    i--;
-                }
-                else value+=5;
-            }
-            else if(s.charAt(i) == 'X')
-            {
-                if(s.charAt(i-1) == 'I')
-                {
-                    value+=9;
-                    i--;
-                }
-                else value+=10;
-            }
-            else if(s.charAt(i) == 'L')
-            {
-                if(s.charAt(i-1) == 'X')
-                {
-                    value+=40;
-                    i--;
-                }
-                else value+=50;
-            }
-            else if(s.charAt(i) == 'C')
-            {
-                if(s.charAt(i-1) == 'X')
-                {
-                    value+=90;
-                    i--;
-                }
-                else value+=100;
-            }
-            else if(s.charAt(i) == 'D')
-            {
-                if(s.charAt(i-1) == 'C')
-                {
-                    value+=400;
-                    i--;
-                }
-                else value+=500;
-            }
-            else if(s.charAt(i) == 'M')
-            {
-                if(s.charAt(i-1) == 'C')
-                {
-                    value+=900;
-                    i--;
-                }
-                else value+=1000;
-            }
-        }
-        if(i == 0)
-        {
-            if(s.charAt(0) == 'I') value+=1;
-            else if(s.charAt(0) == 'V') value+=5;
-            else if(s.charAt(0) == 'X') value+=10;
-            else if(s.charAt(0) == 'L') value+=50;
-            else if(s.charAt(0) == 'C') value+=100;
-            else if(s.charAt(0) == 'D') value+=500;
-            else value+=1000;
-        }
+        Map<Character, Integer> m = new HashMap<>();
+        m.put('I',1);m.put('V',5);m.put('X',10);m.put('L',50);m.put('C',100);m.put('D',500);m.put('M',1000);
         
-        return value;
+        int ans = 0, i=0;
+        for(; i<s.length()-1; i++)
+        {
+            if(m.get(s.charAt(i)) >= m.get(s.charAt(i+1))) ans+=m.get(s.charAt(i));
+            else
+            {
+                ans+=m.get(s.charAt(i+1))-m.get(s.charAt(i));
+                i++;
+            }
+        }
+        if(i == s.length()-1) ans+=m.get(s.charAt(i));
+        return ans;
     }
 }
