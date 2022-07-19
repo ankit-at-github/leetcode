2dbef60c131b7ce5070e4df7393650561309bff3
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-07-19 16:35:49 -0400

    Time: 23 ms (7.58%), Space: 52 MB (5.79%) - LeetHub

---
+++ 684-redundant-connection/684-redundant-connection.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0; i<=n; i++) adj.add(new ArrayList());
+        
+        int[] ans = new int[2];
+        
+        for(int[] edge : edges)
+        {
+            adj.get(edge[0]).add(edge[1]);
+            adj.get(edge[1]).add(edge[0]);
+            boolean[] visited = new boolean[n+1];
+            if(cycleDFS(edge[0], -1, visited, adj, ans))
+            {
+                ans[0] = edge[0];
+                ans[1] = edge[1];
+                return ans;
+            }
+        }
+        return new int[]{};
+    }
+    public boolean cycleDFS(int node, int parent, boolean[] visited, List<List<Integer>> adj, int[] ans)
+    {
+        visited[node] = true;
+        for(int adjacentNode : adj.get(node))
+        {
+            if(visited[adjacentNode] && adjacentNode!=parent)
+            {
+                ans[0] = adjacentNode;
+                ans[1] = node;
+                return true;
+            }
+            if(!visited[adjacentNode])
+            {
+                if(cycleDFS(adjacentNode, node, visited, adj, ans)) return true;
+            }
+        }
+        return false;
+    }
+}
