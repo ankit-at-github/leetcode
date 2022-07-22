69e087f6a2a1f7f12b848f7c59d063b6dafb758f
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-07-22 13:19:13 -0400

    Time: 4 ms (93.47%), Space: 47.6 MB (68.06%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -1,18 +1,9 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        //this question is nothing but detecting a cycle in directed graph.
-        //let's write a code for the same
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0; i<numCourses; i++) adj.add(new ArrayList());
         
-        //first create a graph with all edges given
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for(int i=0; i<numCourses; i++) adj.add(new ArrayList<>());
-        
-        for(int[] course : prerequisites)
-        {
-            int u = course[0];
-            int v = course[1];
-            adj.get(u).add(v);
-        }
+        for(int [] prerequisite : prerequisites) adj.get(prerequisite[0]).add(prerequisite[1]);
         
         boolean[] visited = new boolean[numCourses];
         boolean[] dfsVisited = new boolean[numCourses];
@@ -21,12 +12,12 @@ class Solution {
         {
             if(!visited[i])
             {
-                if(checkCycleDFS(adj, i, visited, dfsVisited)) return false;
+                if(cycleDFS(i, visited, dfsVisited, adj)) return false;
             }
         }
         return true;
     }
-    public boolean checkCycleDFS(ArrayList<ArrayList<Integer>> adj, int node, boolean[] visited, boolean[] dfsVisited)
+    public boolean cycleDFS(int node, boolean[] visited, boolean[] dfsVisited, List<List<Integer>> adj)
     {
         visited[node] = true;
         dfsVisited[node] = true;
@@ -34,12 +25,9 @@ class Solution {
         {
             if(!visited[adjacentNode])
             {
-                if(checkCycleDFS(adj, adjacentNode, visited, dfsVisited)) return true;
+                if(cycleDFS(adjacentNode, visited, dfsVisited, adj)) return true;
             }
-            else
-            {
-                if(visited[adjacentNode] && dfsVisited[adjacentNode]) return true;
-            }
+            if(visited[adjacentNode] && dfsVisited[adjacentNode]) return true;
         }
         dfsVisited[node] = false;
         return false;
