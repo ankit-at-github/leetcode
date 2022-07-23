1e8b117655d52de6c71094f5203b9604c8133f23
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-07-22 21:06:30 -0400

    Time: 8 ms (13.87%), Space: 54.8 MB (5.32%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -1,33 +1,47 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
-        int[] color = new int[graph.length];
-        Arrays.fill(color, -1);
+        int n = graph.length;
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0; i<n; i++) adj.add(new ArrayList());
         
-        for(int i=0; i<graph.length; i++)
+        for(int i=0; i<n; i++)
         {
-            if(color[i] == -1)
+            for(int x : graph[i])
+                adj.get(i).add(x);
+        }
+        
+        int[] colors = new int[n];
+        Arrays.fill(colors, -1);
+        
+        Queue<Integer> q = new LinkedList<>();
+        
+        for(int v=0; v<n; v++)
+        {
+            if(colors[v]==-1)
             {
-                color[i] = 0;
-                if(!checkDFS(graph, i, color)) return false;
+                q.add(v);
+                colors[v]=0;
+                while(!q.isEmpty())
+                {
+                    for(int i=q.size(); i>0; i--)
+                    {
+                        int node = q.poll();
+                        for(int adjacentNode : adj.get(node))
+                        {
+                            if(colors[adjacentNode] == -1)
+                            {
+                                colors[adjacentNode] = colors[node]==1?0:1;
+                                q.add(adjacentNode);
+                            }
+                            else
+                            {
+                                if(colors[adjacentNode] == colors[node]) return false;
+                            }
+                        }
+                    }
+                }
             }
         }
-        return true;
-    }
-    public boolean checkDFS(int[][] graph, int node, int[] color)
-    {
-       for(int adjacentNode : graph[node])
-       {
-           if(color[adjacentNode] == -1)
-           {
-               if(color[node] == 0) color[adjacentNode] = 1;
-               else color[adjacentNode] = 0;
-               if(!checkDFS(graph, adjacentNode, color)) return false;
-           }
-           else
-           {
-               if(color[adjacentNode] == color[node]) return false;
-           }
-       }
         return true;
     }
 }
