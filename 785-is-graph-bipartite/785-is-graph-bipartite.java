5960cefc0c44dad8df53923d2a97c9f3bf6135f5
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-07-22 22:51:47 -0400

    Time: 1 ms (88.90%), Space: 53 MB (81.32%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -5,30 +5,28 @@ class Solution {
         int[] colors = new int[n];
         Arrays.fill(colors, -1);
         
-        Queue<Integer> q = new LinkedList<>();
-        
-        for(int v=0; v<n; v++)
+        for(int i=0; i<n; i++)
         {
-            if(colors[v]==-1)
+            if(colors[i] == -1)
             {
-                q.add(v);
-                colors[v]=0;
-                while(!q.isEmpty())
-                {
-                        int node = q.poll();
-                        for(int adjacentNode : graph[node])
-                        {
-                            if(colors[adjacentNode] == -1)
-                            {
-                                colors[adjacentNode] = colors[node]==1?0:1;
-                                q.add(adjacentNode);
-                            }
-                            else
-                            {
-                                if(colors[adjacentNode] == colors[node]) return false;
-                            }
-                        }
-                }
+                colors[i] = 0;
+                if(!bipartiteDFS(i, graph, colors)) return false;
+            }
+        }
+        return true;
+    }
+    public boolean bipartiteDFS(int node, int[][] graph, int[] colors)
+    {
+        for(int adjacentNode : graph[node])
+        {
+            if(colors[adjacentNode] == -1)
+            {
+                colors[adjacentNode] = colors[node]==0?1:0;
+                if(!bipartiteDFS(adjacentNode, graph, colors)) return false;
+            }
+            else
+            {
+                if(colors[adjacentNode] == colors[node]) return false;
             }
         }
         return true;
