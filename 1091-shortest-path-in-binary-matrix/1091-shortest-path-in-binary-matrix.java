7493c1c78f7c6e0cd2f015602d6ab29c62db0841
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-07-25 13:33:32 -0400

    Time: 38 ms (27.71%), Space: 60.4 MB (36.56%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,41 +1,87 @@
 class Solution {
-    private int dir[][] = new int[][]{{0,1},{0,-1},{1,0},{-1,0},{1,-1},{-1,1},{-1,-1},{1,1}};
     public int shortestPathBinaryMatrix(int[][] grid) {
-
-        int m = grid.length;
-        int n = grid[0].length;
-
-        if(grid[0][0]==1 || grid[m-1][n-1]==1) {
-            return -1;
-        }
-
-        boolean[][] visited = new boolean[m][n];
-        visited[0][0] = true;
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{0,0});
-
-        int ans=0;
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            for(int i=0;i<size;i++) {
-                int[] pop = queue.remove();
-                if(pop[0]==m-1 && pop[1]==n-1) {
-                    return ans+1;
-                }
-                for (int k=0;k<8;k++) 
+        return bfsSPBM(grid, grid.length);
+    }
+    public int bfsSPBM(int[][] grid, int n)
+    {
+        if(grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;
+        
+        int mini = -1;
+        Queue<int[]> q = new LinkedList<int[]>();
+        q.add(new int[]{0, 0, 1});
+        
+        while(!q.isEmpty())
+        {
+            for(int i=q.size(); i>0; i--)
+            {
+                int x = q.peek()[0];
+                int y = q.peek()[1];
+                int cost = q.poll()[2];
+                
+                //grid[x][y] = 1;
+                
+                if(x == n-1 && y == n-1)
                 {
-                    int nextX = dir[k][0]+pop[0];
-                    int nextY = dir[k][1]+pop[1];
-
-                if(nextX>=0 && nextX<m && nextY>=0 && nextY<n && !visited[nextX][nextY] && grid[nextX][nextY]==0)
-                {
-                    queue.add(new int[]{nextX,nextY});
-                    visited[nextX][nextY]=true;
+                    if(mini!=-1) mini = Math.min(mini, cost);
+                    else mini = cost;
                 }
+                
+                //1
+                if(isValid(x-1, y-1, grid, n))
+                {
+                    grid[x-1][y-1] = 1;
+                    q.add(new int[]{x-1, y-1, cost+1});
+                }
+                //2
+                if(isValid(x-1, y, grid, n))
+                {
+                    grid[x-1][y] = 1;
+                    q.add(new int[]{x-1, y, cost+1});
+                }
+                //3
+                if(isValid(x-1, y+1, grid, n))
+                {
+                    grid[x-1][y+1] = 1;
+                    q.add(new int[]{x-1, y+1, cost+1});
+                }
+                //4
+                if(isValid(x, y-1, grid, n))
+                {
+                    grid[x][y-1] = 1;
+                    q.add(new int[]{x, y-1, cost+1});
+                }
+                //5
+                if(isValid(x, y+1, grid, n))
+                {
+                    grid[x][y+1] = 1;
+                    q.add(new int[]{x, y+1, cost+1});
+                }
+                //6
+                if(isValid(x+1, y-1, grid, n))
+                {
+                    grid[x+1][y-1] = 1;
+                    q.add(new int[]{x+1, y-1, cost+1});
+                }
+                //7
+                if(isValid(x+1, y, grid, n))
+                {
+                    grid[x+1][y] = 1;
+                    q.add(new int[]{x+1, y, cost+1});
+                }
+                //8
+                if(isValid(x+1, y+1, grid, n))
+                {
+                    grid[x+1][y+1] = 1;
+                    q.add(new int[]{x+1, y+1, cost+1});
                 }
             }
-            ans++;
         }
-        return -1;
+        
+        return mini;
+    }
+    public boolean isValid(int x, int y, int[][] grid, int n)
+    {
+        if(x < 0 || x >= n || y < 0 || y >= n || grid[x][y] == 1) return false;
+        return true;
     }
 }
