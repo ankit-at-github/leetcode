88939859daa641a06d2f1bc551242e434367f3ba
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-07-28 11:15:16 -0400

    Time: 0 ms (100.00%), Space: 40.5 MB (75.38%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -3,14 +3,7 @@ class Solution {
         int[] dp = new int[n+1];
         dp[0] = 1;
         dp[1] = 1;
-        return solve(n, dp);
-    }
-    public int solve(int n, int[] dp)
-    {
-        if(n<=1) return 1;
-        
-        if(dp[n]!=0) return dp[n];
-        
-        return (dp[n] = solve(n-1, dp) + solve(n-2, dp));
+        for(int i=2; i<=n; i++) dp[i] = dp[i-1]+dp[i-2];
+        return dp[n];
     }
 }
