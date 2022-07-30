12da50e0e04f3222bd1f09c87e5ed78ec3dd787b
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-07-30 19:28:49 -0400

    Time: 5 ms (89.27%), Space: 47.4 MB (39.18%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.java
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.java
@@ -1,53 +1,27 @@
 class Solution {
-    int minCost = 0;
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        //Creating graph
-        List<List<int[]>> adj = new ArrayList<>();
-        for(int i=0; i<n; i++) adj.add(new ArrayList<int[]>());
-        
-        for(int[] flight : flights)
-        {
-            adj.get(flight[0]).add(new int[]{flight[1], flight[2]});
-        }
-        
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1]-b[1]);
-        pq.add(new int[]{src, 0, 0});
-        
         int[] dist = new int[n];
-        int[] stops = new int[n];
-        for(int i=0; i<n; i++)
-        {
-            dist[i] = Integer.MAX_VALUE;
-            stops[i] = Integer.MAX_VALUE;
-        }
+        Arrays.fill(dist, Integer.MAX_VALUE);
         dist[src] = 0;
-        stops[src] = 0;
         
-        while(!pq.isEmpty())
+        for(int i=0; i<=k; i++)
         {
-            int node = pq.peek()[0];
-            int price = pq.peek()[1];
-            int stop = pq.poll()[2];
-            
-            if(node == dst) return price;
-            
-            if(stop == k+1) continue;
-            
-            for(int[] adjacentNode : adj.get(node))
+            int[] temp = Arrays.copyOf(dist, n);
+            for(int[] flight : flights)
             {
-                if(price + adjacentNode[1] < dist[adjacentNode[0]])
+                int u = flight[0];
+                int v = flight[1];
+                int price = flight[2];
+                
+                if(dist[u] == Integer.MAX_VALUE) continue;
+                
+                if(dist[u] + price < temp[v])
                 {
-                    dist[adjacentNode[0]] = price + adjacentNode[1];
-                    pq.add(new int[]{adjacentNode[0], dist[adjacentNode[0]], stop+1});
-                    stops[adjacentNode[0]] = stop+1;
-                }
-                else if(stop < stops[adjacentNode[0]])
-                {
-                    dist[adjacentNode[0]] = price + adjacentNode[1];
-                    pq.add(new int[]{adjacentNode[0], dist[adjacentNode[0]], stop+1});
+                    temp[v] = dist[u] + price;
                 }
             }
+            dist = temp;
         }
-        return -1;
+        return dist[dst]==Integer.MAX_VALUE?-1:dist[dst];
     }
 }
