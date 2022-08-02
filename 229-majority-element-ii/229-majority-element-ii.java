b272198e1f82418ccd41d64cde382055cfafac4f
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-08-02 18:16:10 -0400

    Time: 2 ms (93.67%), Space: 49.6 MB (59.60%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -1,22 +1,23 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int num1 = Integer.MIN_VALUE, num2 = Integer.MIN_VALUE, count1 = 0, count2 = 0;
         int n = nums.length;
-        for(int x : nums)
+        int majority1 = Integer.MIN_VALUE, majority2 = Integer.MIN_VALUE;
+        int count1 = 0, count2 = 0;
+        for(int i=0; i<nums.length; i++)
         {
-            if(x == num1) count1++;
-            else if(x == num2) count2++;
+            if(nums[i] == majority1) count1++;
+            else if(nums[i] == majority2) count2++;
             else if(count1 == 0)
             {
+                majority1 = nums[i];
                 count1 = 1;
-                num1 = x;
             }
             else if(count2 == 0)
             {
+                majority2 = nums[i];
                 count2 = 1;
-                num2 = x;
             }
-            else 
+            else
             {
                 count1--;
                 count2--;
@@ -26,12 +27,12 @@ class Solution {
         count2 = 0;
         for(int x : nums)
         {
-            if(x == num1) count1++;
-            if(x == num2) count2++;
+            if(x == majority1) count1++;
+            else if(x == majority2) count2++;
         }
-        ArrayList<Integer> ans = new ArrayList<>();
-        if(count1 > n/3) ans.add(num1);
-        if(count2 > n/3) ans.add(num2);
+        List<Integer> ans = new ArrayList<>();
+        if(count1 > n/3) ans.add(majority1);
+        if(count2 > n/3) ans.add(majority2);
         return ans;
     }
 }
