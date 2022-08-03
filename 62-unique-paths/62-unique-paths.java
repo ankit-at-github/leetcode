e5de6fa9d43c09d9b0677d2429cd57387bf41ba8
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-08-03 14:57:43 -0400

    Time: 1 ms (55.29%), Space: 41 MB (40.85%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,16 +1,20 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
-        for(int i=0; i<m; i++) dp[i][0] = 1;
-        for(int j=0; j<n; j++) dp[0][j] = 1;
+        return dfs(0, 0, m, n, dp);
+    }
+    public int dfs(int x, int y, int m, int n, int[][] dp)
+    {
+        //Not a valid path
+        if(x < 0 || y < 0|| x >= m || y >= n) return 0;
         
-        for(int i=1; i<m; i++)
-        {
-            for(int j=1; j<n; j++)
-            {
-                dp[i][j] = dp[i][j-1] + dp[i-1][j];
-            }
-        }
-        return dp[m-1][n-1];
+        //Destination
+        if(x == m-1 && y == n-1) return 1;
+        
+        //Already visited path
+        if(dp[x][y] > 0) return dp[x][y];
+        
+        //Storing number of path from each cell
+        return dp[x][y] = dfs(x, y+1, m, n, dp) + dfs(x+1, y, m, n, dp);
     }
 }
