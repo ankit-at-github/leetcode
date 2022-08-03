c6d061b4fe91bd7ffdf55c8459851cdad75fd574
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-08-03 10:44:05 -0400

    Time: 303 ms (6.94%), Space: 54.7 MB (35.87%) - LeetHub

---
+++ 729-my-calendar-i/729-my-calendar-i.java
@@ -0,0 +1,30 @@
+class MyCalendar {
+    List<List<Integer>> calendar;
+    public MyCalendar() {
+        calendar = new ArrayList<>();
+    }
+    
+    public boolean book(int start, int end) {
+        if(calendar.size() == 0)
+        {
+            calendar.add(Arrays.asList(start, end));
+            return true;
+        }
+        for(List<Integer> c : calendar)
+        {
+            int startTime = c.get(0);
+            int endTime = c.get(1);
+            //System.out.println(startTime +" "+ start);
+            //System.out.println(endTime +" "+ end);
+            if(end > startTime && start < endTime) return false;
+        }
+        calendar.add(Arrays.asList(start, end));
+        return true;
+    }
+}
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar obj = new MyCalendar();
+ * boolean param_1 = obj.book(start,end);
+ */
