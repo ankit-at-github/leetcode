9006fcb177f22c386da537cf93b4378795fd0284
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-08-06 17:25:19 -0400

    Time: 87 ms (80.00%), Space: 105.1 MB (20.00%) - LeetHub

--- 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.java
+++ 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.java
@@ -1,7 +1,6 @@
 class Solution {
     public long countBadPairs(int[] nums) {
         long n = (long)nums.length;
-        long totalPairs = n*(n-1)/2;
         Map<Integer, Integer> map = new HashMap<>();
         long goodPairs = 0;
         for(int i=0; i<n; i++)
@@ -10,6 +9,6 @@ class Solution {
             if(map.containsKey(diff)) goodPairs+=map.get(diff);
             map.put(diff, map.getOrDefault(diff, 0)+1);
         }
-        return totalPairs-goodPairs;
+        return 1L*(n*(n-1)/2)-goodPairs;
     }
 }
