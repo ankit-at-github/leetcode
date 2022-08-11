a0d46d714f068e78e4cee627df20ff0c6fbf2c6b
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-08-11 12:11:54 -0400

    Time: 26 ms (96.19%), Space: 71.2 MB (38.09%) - LeetHub

--- 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.java
+++ 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.java
@@ -2,49 +2,53 @@ class Solution {
     public int minimumCost(int n, int[][] connections) {
         if(connections.length < n-1) return -1;
         
-        List<List<int[]>> adj = new ArrayList<>();
-        for(int i=0; i<=n; i++) adj.add(new ArrayList<int[]>());
+        int[] parent = new int[n+1];
+        int[] rank = new int[n+1];
         
+        for(int i=0; i<=n; i++) parent[i] = i;
+        
+        Arrays.sort(connections, (a,b) -> a[2]-b[2]);
+        
+        int weight = 0;
+        boolean[] mst = new boolean[n+1];
         for(int[] connection : connections)
         {
             int u = connection[0];
             int v = connection[1];
             int w = connection[2];
             
-            adj.get(u).add(new int[]{v, w});
-            adj.get(v).add(new int[]{u, w});
-        }
-        
-        boolean[] mst = new boolean[n+1];
-        int[] weight = new int[n+1];
-        Arrays.fill(weight, Integer.MAX_VALUE);
-        
-        //Starting from 1.
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1]-b[1]);
-        pq.add(new int[]{1, 0});
-        
-        weight[1] = 0;
-        
-        while(!pq.isEmpty())
-        {
-            int currentNode = pq.poll()[0];
-            mst[currentNode] = true;
-            
-            for(int[] adjacent : adj.get(currentNode))
+            if(findParent(u, parent) != findParent(v, parent))
             {
-                int adjacentNode = adjacent[0];
-                int adjacentNodeWeight = adjacent[1];
-                
-                if(!mst[adjacentNode] && adjacentNodeWeight < weight[adjacentNode])
-                {
-                    weight[adjacentNode] = adjacentNodeWeight;
-                    pq.add(new int[]{adjacentNode, adjacentNodeWeight});
-                }
+                weight+=w;
+                mst[u] = true;
+                mst[v] = true;
             }
+            union(u, v, rank, parent);
         }
+        return weight;
+    }
+    public int findParent(int node, int[] parent)
+    {
+        if(node == parent[node]) return node;
+        return parent[node] = findParent(parent[node], parent);
+    }
+    public void union(int u, int v, int[] rank, int[] parent)
+    {
+        int rootU = findParent(u, parent);
+        int rootV = findParent(v, parent);
         
-        int w = 0;
-        for(int i=1; i<weight.length; i++) w+=weight[i];
-        return w;
+        if(rank[rootU] > rank[rootV])
+        {
+            parent[rootV] = rootU;
+        }
+        else if(rank[rootU] < rank[rootV])
+        {
+            parent[rootU] = rootV;
+        }
+        else
+        {
+            parent[rootU] = rootV;
+            rank[rootV]++;
+        }
     }
 }
