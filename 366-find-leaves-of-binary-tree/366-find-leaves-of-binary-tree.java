21313aaf71303eb2952fcaa0568204198866bebd
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-08-11 22:52:35 -0400

    Time: 2 ms (16.79%), Space: 42.5 MB (45.87%) - LeetHub

--- 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.java
+++ 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.java
@@ -17,26 +17,28 @@ class Solution {
     public List<List<Integer>> findLeaves(TreeNode root) {
         List<List<Integer>> ans = new ArrayList<>();
         Map<Integer, ArrayList<Integer>> map = new HashMap<>();
+        
         preorder(root, map);
-        for(List l : map.values()) ans.add(l);
+        
+        for(List<Integer> l : map.values()) ans.add(l);
         return ans;
     }
     public void preorder(TreeNode root, Map<Integer, ArrayList<Integer>> map)
     {
-        if(root==null) return;
+        if(root == null) return;
+        
         int level = getHeight(root);
-        if(map.containsKey(level)) map.get(level).add(root.val);
-        else
-        {
-            map.put(level, new ArrayList<>());
-            map.get(level).add(root.val);
-        }
+        
+        if(!map.containsKey(level)) map.put(level, new ArrayList());
+        
+        map.get(level).add(root.val);
+        
         preorder(root.left, map);
         preorder(root.right, map);
     }
     public int getHeight(TreeNode root)
     {
         if(root == null) return 0;
-        return 1 + Math.max(getHeight(root.left), getHeight(root.right));
+        return 1+Math.max(getHeight(root.left), getHeight(root.right));
     }
 }
