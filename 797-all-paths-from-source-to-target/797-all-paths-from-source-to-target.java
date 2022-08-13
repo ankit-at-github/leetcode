c0a791a9274e39486391892a41e274cb04b87a16
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-08-13 18:49:00 -0400

    Time: 13 ms (13.45%), Space: 55.1 MB (58.01%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
@@ -1,22 +1,27 @@
 class Solution {
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> current = new ArrayList<>();
-        current.add(0);
         
-        dfs(0, current, ans, graph);
+        if(graph.length == 0) return ans;
         
-        return ans;
-    }
-    public void dfs(int node, List<Integer> current, List<List<Integer>> ans, int[][] graph)
-    {
-        for(int adjacentNode : graph[node])
+        Queue<List<Integer>> q = new LinkedList<>();
+        q.add(Arrays.asList(0));
+        
+        while(!q.isEmpty())
         {
-            current.add(adjacentNode);
-            dfs(adjacentNode, current, ans, graph);
-            current.remove(current.size()-1);
+            List<Integer> current = new ArrayList<>(q.poll());
+            
+            int node = current.get(current.size()-1);
+            
+            if(node == graph.length-1) ans.add(new ArrayList(current));
+            
+            for(int adjacentNode : graph[node])
+            {
+                current.add(adjacentNode);
+                q.add(new ArrayList(current));
+                current.remove(current.size()-1);
+            }
         }
-        
-        if(node == graph.length-1) ans.add(new ArrayList(current));
+        return ans;
     }
 }
