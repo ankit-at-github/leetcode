a7849642d621c175d2c1d27235e06d1570655557
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-08-15 12:21:31 -0400

    Time: 242 ms (41.99%), Space: 346.4 MB (29.79%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
@@ -8,60 +8,52 @@ class Solution {
             adj.get(connection.get(0)).add(connection.get(1));
             adj.get(connection.get(1)).add(connection.get(0));
         }
-        
         boolean[] visited = new boolean[n];
-        int[] parent = new int[n];
         int[] discoveryTime = new int[n];
-        int[] lowestpossibleTime = new int[n];
+        int[] lowestPossibleTime = new int[n];
         
         List<List<Integer>> result = new ArrayList<>();
-        
         for(int i=0; i<n; i++)
         {
-            parent[i] = -1;
             discoveryTime[i] = -1;
-            lowestpossibleTime[i] = -1;
+            lowestPossibleTime[i] = -1;
         }
         
         for(int i=0; i<n; i++)
         {
             if(!visited[i])
             {
-                dfs(i, 0, discoveryTime, lowestpossibleTime, parent, visited, adj, result);
+                dfs(i, 0, discoveryTime, lowestPossibleTime, -1, visited, adj, result);
             }
         }
         return result;
     }
-    public void dfs(int node, int timer, int[] discoveryTime, int[] lowestpossibleTime, int[] parent, boolean[] visited, List<List<Integer>> adj, List<List<Integer>> result)
+    public void dfs(int node, int timer, int[] discoveryTime, int[] lowestPossibleTime, int parent, boolean[] visited, List<List<Integer>> adj, List<List<Integer>> result)
     {
         visited[node] = true;
         discoveryTime[node] = timer;
-        lowestpossibleTime[node] = timer;
+        lowestPossibleTime[node] = timer;
         
         for(int adjacentNode : adj.get(node))
         {
-            if(adjacentNode == parent[node]) continue;
+            if(adjacentNode == parent)  continue;
             
             if(!visited[adjacentNode])
             {
-                parent[adjacentNode] = node;
-                dfs(adjacentNode, timer+1, discoveryTime, lowestpossibleTime, parent, visited, adj, result);
+                dfs(adjacentNode, timer+1, discoveryTime, lowestPossibleTime, node, visited, adj, result);
+                //returning from dfs call of this node, checking if its lowestPossibleTime is updated.
+                //If yes, it's child had a back-edge
+                lowestPossibleTime[node] = Math.min(lowestPossibleTime[node], lowestPossibleTime[adjacentNode]);
                 
-                //returning from dfs
-                //update the lowestTime of parent node to make sure whether child node had a back-edge.
-                lowestpossibleTime[node] = Math.min(lowestpossibleTime[node], lowestpossibleTime[adjacentNode]);
                 //checking if this edge is a bridge
-                if(lowestpossibleTime[adjacentNode] > discoveryTime[node])
-                {
-                    result.add(Arrays.asList(adjacentNode,node));
-                }
+                if(lowestPossibleTime[adjacentNode] > discoveryTime[node])
+                    result.add(Arrays.asList(adjacentNode, node));
             }
             else
             {
-                //adjacentNode is visisted and is a back-edge so update lowestpossibleTime
-                lowestpossibleTime[node] = Math.min(lowestpossibleTime[node], discoveryTime[adjacentNode]);
+                //back-edge
+                lowestPossibleTime[node] = Math.min(lowestPossibleTime[node], discoveryTime[adjacentNode]);
             }
         }
-        
     }
 }
