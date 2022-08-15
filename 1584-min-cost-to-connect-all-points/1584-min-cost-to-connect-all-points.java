ef7b23fcd99a6230803e2337c90a5062a6d161e7
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-08-15 15:25:43 -0400

    Time: 339 ms (52.59%), Space: 50 MB (90.05%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,46 +1,41 @@
 class Solution {
     public int minCostConnectPoints(int[][] points) {
-        int n = points.length;
-        int mstCost = 0;
-        int edgesUsed = 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1]-b[1]);
+        pq.add(new int[]{0, 0});
         
-        // Track nodes which are visited.
-        boolean[] inMST = new boolean[n];
+        boolean[] mst = new boolean[points.length];
+        int[] cost = new int[points.length];
+        Arrays.fill(cost, Integer.MAX_VALUE);
         
-        int[] minDist = new int[n];
-        minDist[0] = 0;
+        cost[0] = 0;
         
-        for (int i = 1; i < n; ++i) {
-            minDist[i] = Integer.MAX_VALUE;
-        }
-        
-        while (edgesUsed < n) {
-            int currMinEdge = Integer.MAX_VALUE;
-            int currNode = -1;
+        int total = 0;
+        while(!pq.isEmpty())
+        {
+            int currentNode = pq.peek()[0];
+            int weight = pq.poll()[1];
             
-            // Pick least weight node which is not in MST.
-            for (int node = 0; node < n; ++node) {
-                if (!inMST[node] && currMinEdge > minDist[node]) {
-                    currMinEdge = minDist[node];
-                    currNode = node;
-                }
-            }
-            
-            mstCost += currMinEdge;
-            edgesUsed++;
-            inMST[currNode] = true;
-            
-            // Update adjacent nodes of current node.
-            for (int nextNode = 0; nextNode < n; ++nextNode) {
-                int weight = Math.abs(points[currNode][0] - points[nextNode][0]) + 
-                             Math.abs(points[currNode][1] - points[nextNode][1]);
-                
-                if (!inMST[nextNode] && minDist[nextNode] > weight) {
-                    minDist[nextNode] = weight;
+            mst[currentNode] = true;
+            for(int nextNode = 0; nextNode < points.length; nextNode++)
+            {
+                if(!mst[nextNode])
+                {
+                    int dist = distance(points[currentNode][0], points[nextNode][0], 
+                                        points[currentNode][1], points[nextNode][1]);
+                    if(cost[nextNode] > dist)
+                    {
+                        cost[nextNode] = dist;
+                        pq.add(new int[]{nextNode, dist});
+                    }
                 }
             }
         }
         
-        return mstCost;
+        for(int x : cost) total+=x;
+        return total;
+    }
+    public int distance(int x1, int x2, int y1, int y2)
+    {
+        return (Math.abs(x1-x2) + Math.abs(y1-y2));
     }
 }
