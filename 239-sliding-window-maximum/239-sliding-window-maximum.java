3e88bec8288a0994f192f771a2f329289d87be19
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-08-18 13:26:52 -0400

    Time: 36 ms (86.92%), Space: 61.6 MB (86.98%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.java
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -1,20 +1,20 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        Deque<Integer> dq = new ArrayDeque<>();
+        //Optimized approach using Deque
         int[] ans = new int[nums.length-k+1];
+        Deque<Integer> dq = new LinkedList<>();
         int left = 0, right = 0;
         while(right < nums.length)
         {
-            //remove index if it is outside of range K
-            while(!dq.isEmpty() && dq.peekFirst() < (right - k +1)) dq.pollFirst();
-            
             while(!dq.isEmpty() && nums[right] > nums[dq.peekLast()]) dq.pollLast();
             
             dq.addLast(right);
-            
-            if(right-left+1 == k) ans[left++] = nums[dq.peekFirst()];
-            
             right++;
+            
+            if(right - left == k) ans[left++] = nums[dq.peekFirst()];
+            
+            while(!dq.isEmpty() && left > dq.peekFirst()) dq.pollFirst();
+            
         }
         return ans;
     }
