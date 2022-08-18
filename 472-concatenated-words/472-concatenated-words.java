b9dfe038545509874628b09774ac53f359e7e177
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-08-18 17:08:45 -0400

    Time: 68 ms (89.91%), Space: 73.8 MB (45.34%) - LeetHub

--- 472-concatenated-words/472-concatenated-words.java
+++ 472-concatenated-words/472-concatenated-words.java
@@ -5,24 +5,31 @@ class Solution {
         //We need to create dict as searching is unique.
         //Set - Avoid Duplicates
         
-        Set<String> dict = new HashSet<>(Arrays.asList(words));
+        Set<String> dict = new HashSet<>();
+        
+        int mini = Integer.MAX_VALUE;
+        for(String word : words)
+        {
+            mini = Math.min(mini, word.length());
+            dict.add(word);
+        }
         
         for(String word : words)
         {
-            if(canForm(word, dict)) result.add(word);
+            if(canForm(word, dict, mini)) result.add(word);
         }
         return result;
     }
-    boolean canForm(String word, Set<String> dict)
+    boolean canForm(String word, Set<String> dict, int mini)
     {
-        for(int i=0; i<word.length(); i++)
+        for(int i=mini; i<=word.length()-mini; i++)
         {
             String left = word.substring(0, i);
             String right = word.substring(i);
             
             if(dict.contains(left))
             {
-                if(dict.contains(right) || canForm(right, dict)) return true;
+                if(dict.contains(right) || canForm(right, dict, mini)) return true;
             }
         }
         return false;
