b9b2f4477bcf802c84ac210f4edac635a9844d44
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-08-18 17:10:37 -0400

    Time: 58 ms (95.01%), Space: 70.1 MB (79.78%) - LeetHub

--- 472-concatenated-words/472-concatenated-words.java
+++ 472-concatenated-words/472-concatenated-words.java
@@ -2,6 +2,8 @@ class Solution {
     public List<String> findAllConcatenatedWordsInADict(String[] words) {
         List<String> result = new ArrayList<>();
         
+        Set<String> cache = new HashSet<>();
+        
         //We need to create dict as searching is unique.
         //Set - Avoid Duplicates
         
@@ -16,12 +18,14 @@ class Solution {
         
         for(String word : words)
         {
-            if(canForm(word, dict, mini)) result.add(word);
+            if(canForm(word, dict, mini, cache)) result.add(word);
         }
         return result;
     }
-    boolean canForm(String word, Set<String> dict, int mini)
+    boolean canForm(String word, Set<String> dict, int mini, Set<String> cache)
     {
+        if(cache.contains(word)) return true;
+        
         for(int i=mini; i<=word.length()-mini; i++)
         {
             String left = word.substring(0, i);
@@ -29,7 +33,11 @@ class Solution {
             
             if(dict.contains(left))
             {
-                if(dict.contains(right) || canForm(right, dict, mini)) return true;
+                if(dict.contains(right) || canForm(right, dict, mini, cache))
+                {
+                    cache.add(word);
+                    return true;
+                }
             }
         }
         return false;
