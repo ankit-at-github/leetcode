7e34e8ed4aec651c890b27989840d9337a0c2061
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-08-20 22:03:57 -0400

    Time: 10 ms (46.43%), Space: 46.7 MB (66.16%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.java
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.java
@@ -1,53 +1,34 @@
 class Solution {
-    int minCost = 0;
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        //Creating graph
-        List<List<int[]>> adj = new ArrayList<>();
-        for(int i=0; i<n; i++) adj.add(new ArrayList<int[]>());
+        if(src == dst) return 0;
         
-        for(int[] flight : flights)
-        {
-            adj.get(flight[0]).add(new int[]{flight[1], flight[2]});
-        }
+        int[] previous = new int[n];
+        int[] current = new int[n];
         
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1]-b[1]);
-        pq.add(new int[]{src, 0, 0});
-        
-        int[] dist = new int[n];
-        int[] stops = new int[n];
         for(int i=0; i<n; i++)
         {
-            dist[i] = Integer.MAX_VALUE;
-            stops[i] = Integer.MAX_VALUE;
+            previous[i] = Integer.MAX_VALUE;
+            current[i] = Integer.MAX_VALUE;
         }
-        dist[src] = 0;
-        stops[src] = 0;
         
-        while(!pq.isEmpty())
+        previous[src] = 0;
+        current[src] = 0;
+        
+        for(int i=0; i<=k ;i++)
         {
-            int node = pq.peek()[0];
-            int price = pq.peek()[1];
-            int stop = pq.poll()[2];
-            
-            if(node == dst) return price;
-            
-            if(stop == k+1) continue;
-            
-            for(int[] adjacentNode : adj.get(node))
+            for(int[] flight : flights)
             {
-                if(price + adjacentNode[1] < dist[adjacentNode[0]])
+                int previous_flight = flight[0];
+                int current_flight = flight[1];
+                int price = flight[2];
+                
+                if(previous[previous_flight] < Integer.MAX_VALUE)
                 {
-                    dist[adjacentNode[0]] = price + adjacentNode[1];
-                    pq.add(new int[]{adjacentNode[0], dist[adjacentNode[0]], stop+1});
-                    stops[adjacentNode[0]] = stop+1;
-                }
-                else if(stop < stops[adjacentNode[0]])
-                {
-                    dist[adjacentNode[0]] = price + adjacentNode[1];
-                    pq.add(new int[]{adjacentNode[0], dist[adjacentNode[0]], stop+1});
+                    current[current_flight] = Math.min(current[current_flight], price + previous[previous_flight]);
                 }
             }
+            previous = current.clone();
         }
-        return -1;
+        return current[dst] == Integer.MAX_VALUE?-1:current[dst];
     }
 }
