0dda076fab1db5b75212480926d80ef435a2358b
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-08-28 14:47:32 -0400

    Time: 13 ms (41.58%), Space: 47.4 MB (81.34%) - LeetHub

---
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[][] diagonalSort(int[][] mat) {
+        int r = mat.length;
+        int c = mat[0].length;
+        TreeMap<Integer, PriorityQueue<Integer>> map = new TreeMap<>();
+        for(int i=0; i<r; i++)
+        {
+            for(int j=0; j<c; j++)
+            {
+                if(!map.containsKey(i-j)) map.put((i-j), new PriorityQueue<>());
+                map.get(i-j).add(mat[i][j]);
+            }
+        }
+        for(int i=0; i<r; i++)
+        {
+            for(int j=0; j<c; j++)
+            {
+                mat[i][j] = map.get(i-j).poll();
+            }
+        }
+        return mat;
+    }
+}
