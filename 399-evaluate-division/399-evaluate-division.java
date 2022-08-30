afd7f2006cede3aa9d9df1bb8cdf665e0a09aad1
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-08-30 14:48:59 -0400

    Time: 2 ms (67.52%), Space: 42.9 MB (28.86%) - LeetHub

--- 399-evaluate-division/399-evaluate-division.java
+++ 399-evaluate-division/399-evaluate-division.java
@@ -1,67 +1,49 @@
 class Solution {
-    public double[] calcEquation(List<List<String>> equations, double[] values,
-            List<List<String>> queries) {
-
-        HashMap<String, HashMap<String, Double>> graph = new HashMap<>();
-
-        // Step 1). build the graph from the equations
-        for (int i = 0; i < equations.size(); i++) {
-            List<String> equation = equations.get(i);
-            String dividend = equation.get(0), divisor = equation.get(1);
-            double quotient = values[i];
-
-            if (!graph.containsKey(dividend))
-                graph.put(dividend, new HashMap<String, Double>());
-            if (!graph.containsKey(divisor))
-                graph.put(divisor, new HashMap<String, Double>());
-
-            graph.get(dividend).put(divisor, quotient);
-            graph.get(divisor).put(dividend, 1 / quotient);
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        Map<String, Map<String, Double>> adj = new HashMap<>();
+        
+        //creating graph
+        for(int i=0; i<equations.size(); i++)
+        {
+            String src = equations.get(i).get(0);
+            String dest = equations.get(i).get(1);
+            
+            //u->v
+            if(!adj.containsKey(src)) adj.put(src, new HashMap());
+            adj.get(src).put(dest, values[i]);
+            
+            //v->u
+            if(!adj.containsKey(dest)) adj.put(dest, new HashMap());
+            adj.get(dest).put(src, 1.0/values[i]);
         }
-
-        // Step 2). Evaluate each query via bactracking (DFS)
-        // by verifying if there exists a path from dividend to divisor
-        double[] results = new double[queries.size()];
-        for (int i = 0; i < queries.size(); i++) {
-            List<String> query = queries.get(i);
-            String dividend = query.get(0), divisor = query.get(1);
-
-            if (!graph.containsKey(dividend) || !graph.containsKey(divisor))
-                results[i] = -1.0;
-            else if (dividend == divisor)
-                results[i] = 1.0;
-            else {
-                HashSet<String> visited = new HashSet<>();
-                results[i] = backtrackEvaluate(graph, dividend, divisor, 1, visited);
-            }
+        
+        double[] ans = new double[queries.size()];
+        for(int i=0; i<queries.size(); i++)
+        {
+            ans[i] = dfs(queries.get(i).get(0), queries.get(i).get(1), new HashSet<>(), adj);
         }
-
-        return results;
+        
+        return ans;
     }
-
-    private double backtrackEvaluate(HashMap<String, HashMap<String, Double>> graph, String currNode, String targetNode, double accProduct, Set<String> visited) {
-
-        // mark the visit
-        visited.add(currNode);
-        double ret = -1.0;
-
-        Map<String, Double> neighbors = graph.get(currNode);
-        if (neighbors.containsKey(targetNode))
-            ret = accProduct * neighbors.get(targetNode);
-        else {
-            for (Map.Entry<String, Double> pair : neighbors.entrySet()) {
-                String nextNode = pair.getKey();
-                if (visited.contains(nextNode))
-                    continue;
-                ret = backtrackEvaluate(graph, nextNode, targetNode,
-                        accProduct * pair.getValue(), visited);
-                if (ret != -1.0)
-                    break;
+    public double dfs(String source, String destination, HashSet<String> visited, Map<String, Map<String, Double>> adj)
+    {   
+        //System.out.println(source);
+        //System.out.println(val);
+        if(!adj.containsKey(source)) return -1.0;
+        
+        if(adj.get(source).containsKey(destination)) return adj.get(source).get(destination);
+        
+        visited.add(source);
+        for(Map.Entry<String, Double> adjacent : adj.get(source).entrySet())
+        {
+            if(!visited.contains(adjacent.getKey()))
+            {
+                double weight = dfs(adjacent.getKey(), destination, visited, adj);
+                
+                if(weight != -1.0)
+                return weight*adjacent.getValue();
             }
         }
-
-        // unmark the visit, for the next backtracking
-        visited.remove(currNode);
-        return ret;
+        return -1.0;
     }
 }
