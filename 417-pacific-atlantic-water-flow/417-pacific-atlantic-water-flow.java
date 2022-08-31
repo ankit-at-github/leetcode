fac44f8e366d721448d362fadf6476288af41e4b
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-08-31 12:18:08 -0400

    Time: 7 ms (75.44%), Space: 55 MB (44.27%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
@@ -1,65 +1,44 @@
 class Solution {
-    boolean pacific = false;
-    boolean atlantic = false;
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
         int m = heights.length;
         int n = heights[0].length;
         List<List<Integer>> ans = new ArrayList<>();
+        
+        int[][] pacific = new int[m][n];
+        int[][] atlantic = new int[m][n];
+        
+        //Top and Last Row
+        for(int j=0; j<n; j++)
+        {
+            dfs(0, j, m, n, Integer.MIN_VALUE, pacific, heights);
+            dfs(m-1, j, m, n, Integer.MIN_VALUE, atlantic, heights);
+        }
+        //First and Last Column
+        for(int i=0; i<m; i++)
+        {
+            dfs(i, 0, m, n, Integer.MIN_VALUE, pacific, heights);
+            dfs(i, n-1, m, n, Integer.MIN_VALUE, atlantic, heights);
+        }
+        
         for(int i=0; i<m; i++)
         {
             for(int j=0; j<n; j++)
             {
-                boolean[][] visited = new boolean[m][n];
-                
-                dfs(i, j, m, n, heights, visited);
-                
-                if(pacific && atlantic) ans.add(Arrays.asList(i,j));
-                
-                pacific = false;
-                atlantic = false;
+                if(pacific[i][j] == 1 && atlantic[i][j] == 1)
+                    ans.add(Arrays.asList(i, j));
             }
         }
         return ans;
     }
-    public void dfs(int x, int y, int m, int n, int[][] heights, boolean[][] visited)
-    {   
-        visited[x][y] = true;
+    public void dfs(int x, int y, int m, int n, int prev, int[][] ocean, int[][] heights)
+    {
+        if(x < 0 || y < 0 || x >= m || y >= n || heights[x][y] < prev || ocean[x][y] == 1) return;
         
-        //North
-        if(valid(x-1, y, m, n, visited) && heights[x][y] >= heights[x-1][y])
-        {
-            dfs(x-1, y, m, n, heights, visited);
-        }
-        //East
-        if(valid(x, y+1, m, n, visited) && heights[x][y] >= heights[x][y+1])
-        {
-            dfs(x, y+1, m, n, heights, visited);
-        }
-        //South
-        if(valid(x+1, y, m, n, visited) && heights[x][y] >= heights[x+1][y])
-        {
-            dfs(x+1, y, m, n, heights, visited);
-        }
-        //West
-        if(valid(x, y-1, m, n, visited) && heights[x][y] >= heights[x][y-1])
-        {
-            dfs(x, y-1, m, n, heights, visited);
-        }
-    }
-    public boolean valid(int x, int y, int m, int n, boolean[][] visited)
-    {   
-        if(x < 0 || y < 0)
-        {
-            pacific = true;
-            return false;
-        }
+        ocean[x][y] = 1;
         
-        if(x >= m || y >= n)
-        {
-            atlantic = true;
-            return false;
-        }
-        if(visited[x][y]) return false;
-        return true;
+        dfs(x-1, y, m, n, heights[x][y], ocean, heights);
+        dfs(x+1, y, m, n, heights[x][y], ocean, heights);
+        dfs(x, y-1, m, n, heights[x][y], ocean, heights);
+        dfs(x, y+1, m, n, heights[x][y], ocean, heights);
     }
 }
