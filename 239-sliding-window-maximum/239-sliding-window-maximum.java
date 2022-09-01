ea300f827c49c64235acbee19dd4680b678ec29c
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-09-01 13:44:05 -0400

    Time: 37 ms (85.72%), Space: 61.5 MB (87.91%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.java
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -8,12 +8,11 @@ class Solution {
             while(!dq.isEmpty() && nums[right] > nums[dq.peekLast()]) dq.pollLast();
             
             dq.addLast(right);
+            right++;
             
-            if(right - left+1 == k) ans[left++] = nums[dq.peekFirst()];
+            if(right - left == k) ans[left++] = nums[dq.peekFirst()];
             
             while(!dq.isEmpty() && left > dq.peekFirst()) dq.pollFirst();
-            
-            right++;
         }
         return ans;
     }
