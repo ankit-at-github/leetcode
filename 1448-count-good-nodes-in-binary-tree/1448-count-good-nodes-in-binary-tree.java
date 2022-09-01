dbc9dfaa24ae243d277f95c269b318b781342670
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-08-31 22:05:07 -0400

    Time: 5 ms (16.45%), Space: 58 MB (84.76%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -15,34 +15,15 @@
  */
 class Solution {
     public int goodNodes(TreeNode root) {
-        int count = 0;
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(root, Integer.MIN_VALUE));
-        
-        while(!q.isEmpty())
-        {
-            for(int i=q.size(); i>0; i--)
-            {
-                Pair p = q.poll();
-                
-                int previous = p.prev;
-                
-                if(p.node.val >= previous) count++;
-                
-                if(p.node.left != null) q.add(new Pair(p.node.left, Math.max(previous, p.node.val)));
-                if(p.node.right != null) q.add(new Pair(p.node.right, Math.max(previous, p.node.val)));
-            }
-        }
-        
-        return count;
+        return count(root, Integer.MIN_VALUE);
     }
-}
-class Pair{
-    TreeNode node;
-    int prev;
-    Pair(TreeNode _node, int _prev)
+    public int count(TreeNode root, int maxi)
     {
-        node = _node;
-        prev = _prev;
+        if(root == null) return 0;
+        
+        int res = root.val>=maxi?1:0;
+        res+=count(root.left, Math.max(maxi, root.val));
+        res+=count(root.right, Math.max(maxi, root.val));
+        return res;
     }
 }
