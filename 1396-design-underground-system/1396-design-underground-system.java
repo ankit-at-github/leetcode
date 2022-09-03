4e6c555d0e4b8bd036093f3a236f5ed61fc26b65
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-09-03 17:23:05 -0400

    Time: 220 ms (29.89%), Space: 105.4 MB (19.00%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.java
+++ 1396-design-underground-system/1396-design-underground-system.java
@@ -1,27 +1,36 @@
 class UndergroundSystem {
-    Map<Integer, Pair<String, Integer>> checkInMap = new HashMap<>();  // Uid - {StationName, Time}
-    Map<String, Pair<Double, Integer>> routeMap = new HashMap<>(); // RouteName - {TotalTime, Count}
-
-    public UndergroundSystem() {}
-
+    Map<Integer, Pair<String, Integer>> in;
+    Map<String, Pair<Double, Integer>> routeMap;
+    public UndergroundSystem() {
+        in = new HashMap<>();
+        routeMap = new HashMap<>();
+    }
+    
     public void checkIn(int id, String stationName, int t) {
-        checkInMap.put(id, new Pair<>(stationName, t));
+        in.put(id, new Pair(stationName, t));
     }
-
+    
     public void checkOut(int id, String stationName, int t) {
-        Pair<String, Integer> checkIn = checkInMap.get(id);
-        checkInMap.remove(id); // Remove after using it which will not make HashTable big
-
-        String routeName = checkIn.getKey() + "_" + stationName;
-        int totalTime = t - checkIn.getValue();
-
-        Pair<Double, Integer> route = routeMap.getOrDefault(routeName, new Pair<>(0.0, 0));
-        routeMap.put(routeName, new Pair<>(route.getKey() + totalTime, route.getValue() + 1));
+        Pair<String, Integer> p = in.get(id);
+        
+        String routName = p.getKey() + "_" + stationName;
+        int timeTaken = t - p.getValue();
+        
+        Pair<Double, Integer> route = routeMap.getOrDefault(routName, new Pair(0.0, 0));
+        routeMap.put(routName, new Pair(route.getKey()+timeTaken, route.getValue()+1));
     }
-
+    
     public double getAverageTime(String startStation, String endStation) {
         String routeName = startStation + "_" + endStation;
         Pair<Double, Integer> trip = routeMap.get(routeName);
-        return trip.getKey() / trip.getValue();
+        return trip.getKey()/trip.getValue();
     }
 }
+
+/**
+ * Your UndergroundSystem object will be instantiated and called as such:
+ * UndergroundSystem obj = new UndergroundSystem();
+ * obj.checkIn(id,stationName,t);
+ * obj.checkOut(id,stationName,t);
+ * double param_3 = obj.getAverageTime(startStation,endStation);
+ */
