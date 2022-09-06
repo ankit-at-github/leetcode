77fe7969f5b465d26883d952d437e6de38a9764f
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-09-06 19:34:43 -0400

    Time: 29 ms (47.01%), Space: 46.6 MB (10.45%) - LeetHub

---
+++ 1244-design-a-leaderboard/1244-design-a-leaderboard.java
@@ -0,0 +1,34 @@
+class Leaderboard {
+    Map<Integer, Integer> board = new HashMap<>();
+    public Leaderboard() {
+        
+    }
+    
+    public void addScore(int playerId, int score) {
+        board.put(playerId, board.getOrDefault(playerId, 0)+score);
+    }
+    
+    public int top(int K) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b-a);
+        for(Map.Entry<Integer, Integer> entry : board.entrySet()) pq.add(entry.getValue());
+        
+        int sum = 0;
+        while(K-- > 0 && !pq.isEmpty())
+        {
+            sum+=pq.poll();
+        }
+        return sum;
+    }
+    
+    public void reset(int playerId) {
+        board.put(playerId, 0);
+    }
+}
+
+/**
+ * Your Leaderboard object will be instantiated and called as such:
+ * Leaderboard obj = new Leaderboard();
+ * obj.addScore(playerId,score);
+ * int param_2 = obj.top(K);
+ * obj.reset(playerId);
+ */
