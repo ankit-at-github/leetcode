fbff7a3d5f1c37517b2269171e414ae8273f4e22
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-09-07 17:48:56 -0400

    Time: 39 ms (31.70%), Space: 48.9 MB (67.06%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.java
@@ -1,16 +1,25 @@
 class Solution {
     public String frequencySort(String s) {
-        Map<Character, Integer> freq = new HashMap<>();
-        for(char c : s.toCharArray()) freq.put(c, freq.getOrDefault(c, 0)+1);
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> b[0] - a[0]);
-        for(Map.Entry<Character, Integer> entry : freq.entrySet()) pq.add(new int[]{entry.getValue(), entry.getKey()});
-        StringBuilder sb = new StringBuilder();
-        while(!pq.isEmpty())
+        Map<Character, Integer> tm = new HashMap<>();
+        
+        for(int i=0; i<s.length(); i++)
         {
-            int count = pq.peek()[0];
-            char c = (char)pq.poll()[1];
-            while(count -- > 0) sb.append(c);
+            if(!tm.containsKey(s.charAt(i))) tm.put(s.charAt(i), 1);
+            else tm.put(s.charAt(i), tm.get(s.charAt(i))+1);
         }
+        
+        Queue<Character> pq = new PriorityQueue<>((a,b) -> (tm.get(b)-tm.get(a)));
+        
+        for(char c : tm.keySet()) pq.add(c);
+        
+        StringBuilder sb = new StringBuilder();
+        while(pq.size() > 0)
+        {
+            char c = pq.poll();
+            for(int i=0; i<tm.get(c); i++) sb.append(c);
+        }
+        
         return sb.toString();
+        
     }
 }
