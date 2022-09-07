74ae393982ab3e8def308229e6fcc7891b76b0b2
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-09-07 16:01:45 -0400

    Time: 232 ms (28.71%), Space: 42.5 MB (35.99%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -1,35 +1,37 @@
 class Solution {
-    public boolean present(char[][] board, int x, int y, String word, int index, boolean[][] visited)
-    {
-        if(index == word.length()) return true;
-        
-        if(x<0 || x>=board.length || y<0 || y>=board[0].length || visited[x][y]) return false;
-        
-        if(board[x][y] != word.charAt(index)) return false;
-        
-        visited[x][y] = true;
-        
-        if(present(board, x, y+1, word, index+1, visited)||
-            present(board, x+1, y, word, index+1, visited)||
-            present(board, x, y-1, word, index+1, visited)||
-            present(board, x-1, y, word, index+1, visited)) return true;
-        
-        visited[x][y] = false;
-        
-        return false;
-    }
     public boolean exist(char[][] board, String word) {
         int m = board.length;
         int n = board[0].length;
         boolean[][] visited = new boolean[m][n];
-        for(int i=0; i<board.length; i++)
+        for(int i=0; i<m; i++)
         {
-            for(int j=0; j<board[i].length; j++)
+            for(int j=0; j<n; j++)
             {
-                if(board[i][j] == word.charAt(0) && present(board, i, j, word, 0, visited)) return true;
+                if(board[i][j] == word.charAt(0))
+                {
+                    visited = new boolean[m][n];
+                    if(dfs(m, n, i, j, word, 0, board, visited)) return true;
+                }
             }
         }
-        
         return false;
+    }
+    public boolean dfs(int m, int n, int x, int y, String word, int index, char[][] board, boolean[][] visited)
+    {
+        if(index == word.length()) return true;
+        
+        if(x < 0 || x >= m || y < 0 || y >= n || visited[x][y] || board[x][y] != word.charAt(index)) return false;
+        
+        visited[x][y] = true;
+        
+        boolean result = 
+            dfs(m, n, x, y+1, word, index+1, board, visited) || 
+            dfs(m, n, x+1, y, word, index+1, board, visited) || 
+            dfs(m, n, x, y-1, word, index+1, board, visited) || 
+            dfs(m, n, x-1, y, word, index+1, board, visited);
+        
+        visited[x][y] = false;
+        
+        return result;
     }
 }
