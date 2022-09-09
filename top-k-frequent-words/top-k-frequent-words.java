a9fc85a8553e624654549f29f58b95d20de3d535
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-09-09 17:52:08 -0400

    Time: 15 ms, Memory: 45.4 MB - LeetHub

---
+++ top-k-frequent-words/top-k-frequent-words.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        Map<String, Integer> map = new HashMap<>();
+        for(String word : words) map.put(word, map.getOrDefault(word, 0)+1);
+        
+        TreeMap<Integer, PriorityQueue<String>> tmap = new TreeMap<>(Collections.reverseOrder());
+        for(Map.Entry<String, Integer> entry : map.entrySet())
+        {
+            if(!tmap.containsKey(entry.getValue())) tmap.put(entry.getValue(), new PriorityQueue<>());
+            tmap.get(entry.getValue()).add(entry.getKey());
+        }
+        List<String> ans = new ArrayList<>();
+        for(PriorityQueue<String> pq : tmap.values())
+        {
+            while(!pq.isEmpty() && k > 0)
+            {
+                ans.add(pq.poll());
+                k--;
+            }
+        }
+        return ans;
+    }
+}
