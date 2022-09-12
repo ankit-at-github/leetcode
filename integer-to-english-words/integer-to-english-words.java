6958b02ea7bb11fb8b3818842e6e368ec9d5033b
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-09-12 16:29:04 -0400

    Time: 5 ms, Memory: 42.3 MB - LeetHub

---
+++ integer-to-english-words/integer-to-english-words.java
@@ -0,0 +1,35 @@
+class Solution {
+    String[] ones = {"","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten",
+                "Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
+    String[] tens = {"","Ten","Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"};
+    public String numberToWords(int num) {
+        if(num == 0) return "Zero";
+        return helper(num);
+    }
+    public String helper(int num)
+    {
+        StringBuilder sb = new StringBuilder();
+        if(num >= 1000000000)
+        {
+            sb.append(helper(num/1000000000)).append(" Billion ").append(helper(num%1000000000));
+        }
+        else if(num >= 1000000)
+        {
+            sb.append(helper(num/1000000)).append(" Million ").append(helper(num%1000000));
+        }
+        else if(num >= 1000)
+        {
+            sb.append(helper(num/1000)).append(" Thousand ").append(helper(num%1000));
+        }
+        else if(num >= 100)
+        {
+            sb.append(helper(num/100)).append(" Hundred ").append(helper(num%100));
+        }
+        else if(num >= 20)
+        {
+            sb.append(tens[num/10]+" ").append(helper(num%10));
+        }
+        else sb.append(ones[num]);
+        return sb.toString().trim();
+    }
+}
