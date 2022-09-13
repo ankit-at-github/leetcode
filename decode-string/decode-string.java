02399ffa906252aa4a460250d5f1b066082a62a7
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-09-12 20:52:51 -0400

    Time: 2 ms, Memory: 42.1 MB - LeetHub

---
+++ decode-string/decode-string.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String decodeString(String s) {
+        Stack<String> st = new Stack<>();
+        for(int i=s.length()-1; i>=0; i--)
+        {
+            if(s.charAt(i) == '[')
+            {
+                StringBuilder temp = new StringBuilder();
+                while(!st.peek().equals("]")) temp.append(st.pop());
+                st.pop();
+                st.push(temp.toString());
+            }
+            else if(Character.isDigit(s.charAt(i)))
+            {
+                StringBuilder num = new StringBuilder();
+                while(i >= 0 && Character.isDigit(s.charAt(i)))
+                {
+                    num.append(s.charAt(i));
+                    i--;
+                }
+                num = num.reverse();
+                int n = Integer.valueOf(num.toString());
+                String t = st.pop();
+                StringBuilder temp = new StringBuilder();
+                for(int j=0; j<n; j++) temp.append(t);
+                st.push(temp.toString());
+                i++;
+            }
+            else st.push(Character.toString(s.charAt(i)));
+        }
+        StringBuilder ans = new StringBuilder();
+        while(!st.isEmpty()) ans.append(st.pop());
+        return ans.toString();
+    }
+}
