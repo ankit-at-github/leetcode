c0abcccfd1ebbd5e6ba22829915bec88c5d0e80b
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-09-16 09:52:48 -0400

    Time: 1 ms (97.68%), Space: 41.9 MB (99.16%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,5 +1,6 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        //solve this question by DP, by take not take method.
         int totalProfit = 0;
         for(int i=1; i<prices.length; i++)
         {
