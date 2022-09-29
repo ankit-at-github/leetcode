d9e5b3f4b2e019eaca279673ae597e135a1d6d1b
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-09-29 11:33:03 -0400

    Time: 1 ms (92.12%), Space: 43.8 MB (39.89%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -1,27 +1,33 @@
 class Solution {
     public void nextPermutation(int[] nums) {
+        
+        //1. Finding Pivot Index from right
+        
         int pivotIndex = getPivot(nums);
-        //nums is maximum element so return sorted nums
+        
+        //numbers are in decreasing order no next permuation availabe
         if(pivotIndex == -1) reverse(nums, 0, nums.length-1);
         else
         {
+            //2. Find Swapping Index
             int swapIndex = nums.length-1;
-            
             while(swapIndex >= 0 && nums[swapIndex] <= nums[pivotIndex]) swapIndex--;
             
+            //3. Swap value at PivotIndex and SwapIndex
             swap(nums, swapIndex, pivotIndex);
+            //4. Reverse remaining
             reverse(nums, pivotIndex+1, nums.length-1);
         }
     }
     public int getPivot(int[] nums)
     {
-        int j = nums.length-1;
-        while(j > 0 && nums[j-1] >= nums[j]) j--;
-        return j-1;
+        int i = nums.length-1;
+        while(i>0 && nums[i-1] >= nums[i]) i--;
+        return i-1;
     }
     public void reverse(int[] nums, int start, int end)
     {
-        while(start <= end)
+        while(start < end)
         {
             swap(nums, start++, end--);
         }
