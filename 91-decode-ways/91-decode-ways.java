1b52a4b22488d588bb6c52c10d9103a761aa1b5a
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-10-01 19:21:53 -0400

    Time: 2 ms (70.24%), Space: 42.1 MB (67.55%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numDecodings(String s) {  
+        if (s.charAt(0) == '0') {
+            return 0;
+        }
+
+        int n = s.length();
+        int twoBack = 1;
+        int oneBack = 1;
+        for (int i = 1; i < n; i++) {
+            int current = 0;
+            if (s.charAt(i) != '0') {
+                current = oneBack;
+            }
+            int twoDigit = Integer.parseInt(s.substring(i - 1, i + 1));
+            if (twoDigit >= 10 && twoDigit <= 26) {
+                current += twoBack;
+            }
+           
+            twoBack = oneBack;
+            oneBack = current;
+        }
+        return oneBack;
+    }
+}
