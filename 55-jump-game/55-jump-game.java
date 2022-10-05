fc3ca4f78f02553051d48817e9c824a379a7659c
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-10-05 13:26:29 -0400

    Time: 454 ms (13.59%), Space: 42.9 MB (89.87%) - LeetHub

---
+++ 55-jump-game/55-jump-game.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        return jump(0, nums, new Boolean[nums.length+1]);
+    }
+    public boolean jump(int index, int[] nums, Boolean[] dp)
+    {
+        if(index == nums.length-1) return dp[index] = true;
+        
+        if(dp[index] != null) return dp[index];
+        
+        for(int i=1; i<=nums[index]; i++)
+        {
+            if(jump(index+i, nums, dp)) return dp[i] = true;
+        }
+        
+        return dp[index] = false;
+    }
+}
