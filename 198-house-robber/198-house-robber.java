c1d0423a953d1dfc6930ff705bb8c2f5e7d95a32
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-10-08 20:12:16 -0400

    Time: 1 ms (21.83%), Space: 41.7 MB (28.71%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,22 +1,19 @@
 class Solution {
     public int rob(int[] nums) {
+        //Botttom - UP
         int n = nums.length;
         int[] dp = new int[n];
-        Arrays.fill(dp, -1);
-        return maxProfit(n-1, nums, dp);
-    }
-    public int maxProfit(int index, int[] nums, int[] dp)
-    {
-        if(index == 0) return nums[index];
+        dp[0] = nums[0];
         
-        if(index < 0) return 0;
-        
-        if(dp[index] != -1) return dp[index];
-        
-        int pick = nums[index] + maxProfit(index-2, nums, dp);
-        
-        int notpick = maxProfit(index-1, nums, dp);
-        
-        return dp[index] = Math.max(pick, notpick);
+        for(int i=1; i<n; i++)
+        {
+            int pick = nums[i];
+            if(i > 1) pick+=dp[i-2];
+            
+            int notpick = dp[i-1];
+            
+            dp[i] = Math.max(pick, notpick);
+        }
+        return dp[n-1];
     }
 }
