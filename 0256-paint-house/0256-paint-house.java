c5108b5ec6a72864dfd0bae4aad7bc4e18decd3c
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-10-15 01:00:43 -0400

    Time: 11 ms (9.37%), Space: 43.3 MB (51.89%) - LeetHub

--- 0256-paint-house/0256-paint-house.java
+++ 0256-paint-house/0256-paint-house.java
@@ -1,29 +1,31 @@
 class Solution {
     public int minCost(int[][] costs) {
         int n = costs.length;
-        int[][] dp = new int[n][4];
+        int[] prev = new int[4];
         //Initialization
-        dp[0][0] = Math.min(costs[0][1], costs[0][2]);
-        dp[0][1] = Math.min(costs[0][0], costs[0][2]);
-        dp[0][2] = Math.min(costs[0][0], costs[0][1]);
-        dp[0][3] = Math.min(costs[0][0], Math.min(costs[0][1], costs[0][2]));
+        prev[0] = Math.min(costs[0][1], costs[0][2]);
+        prev[1] = Math.min(costs[0][0], costs[0][2]);
+        prev[2] = Math.min(costs[0][0], costs[0][1]);
+        prev[3] = Math.min(costs[0][0], Math.min(costs[0][1], costs[0][2]));
         
         for(int house=1; house<n; house++)
         {
+            int[] temp = new int[4];
             for(int last=0; last<4; last++)
             {
-                dp[house][last] = Integer.MAX_VALUE;
+                temp[last] = Integer.MAX_VALUE;
                 for(int color=0; color<3; color++)
                 {
                     if(color != last)
                     {
-                        int cost = costs[house][color] + dp[house-1][color];
-                        dp[house][last] = Math.min(dp[house][last], cost);
+                        int cost = costs[house][color] + prev[color];
+                        temp[last] = Math.min(temp[last], cost);
                     }
                 }
             }
+            prev = temp;
         }
         
-        return dp[n-1][3];
+        return prev[3];
     }
 }
