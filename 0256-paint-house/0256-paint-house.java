65bebcf35a5913356f72ababad4cabf902434043
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-10-15 00:25:00 -0400

    Time: 4 ms (22.77%), Space: 43.3 MB (51.89%) - LeetHub

--- 0256-paint-house/0256-paint-house.java
+++ 0256-paint-house/0256-paint-house.java
@@ -1,8 +1,30 @@
 class Solution {
     public int minCost(int[][] costs) {
         int n = costs.length;
-        int[][] memo = new int[n][4];
-        return min(n-1, 3, costs, memo);
+        int[][] dp = new int[n][4];
+        //Initialization
+        dp[0][0] = Math.min(costs[0][1], costs[0][2]);
+        dp[0][1] = Math.min(costs[0][0], costs[0][2]);
+        dp[0][2] = Math.min(costs[0][0], costs[0][1]);
+        dp[0][3] = Math.min(costs[0][0], Math.min(costs[0][1], costs[0][2]));
+        
+        for(int house=1; house<n; house++)
+        {
+            for(int last=0; last<4; last++)
+            {
+                dp[house][last] = Integer.MAX_VALUE;
+                for(int color=0; color<3; color++)
+                {
+                    if(color != last)
+                    {
+                        int cost = costs[house][color] + dp[house-1][color];
+                        dp[house][last] = Math.min(dp[house][last], cost);
+                    }
+                }
+            }
+        }
+        
+        return dp[n-1][3];
     }
     public int min(int n, int last, int[][] costs, int[][] memo)
     {
