dad38664d9c00df12d2ff7ed37cdb7f19b0a0071
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-10-15 14:30:58 -0400

    Time: 1 ms (67.64%), Space: 40.8 MB (62.00%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,37 +1,21 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] memo = new int[m][n];
+        int[][] dp = new int[m][n];
+        
         for(int i=0; i<m; i++)
         {
-            for(int j=0; j<n; j++) memo[i][j] = -1;
+            for(int j=0; j<n; j++)
+            {
+                if(i == 0 && j == 0) dp[i][j] = 1;
+                else
+                {
+                    int right = 0, down = 0;
+                    if(i > 0) down = dp[i-1][j];
+                    if(j > 0) right = dp[i][j-1];
+                    dp[i][j] = right+down;
+                }
+            }
         }
-        return totalPaths(0, 0, m, n, memo);
-    }
-    public int totalPaths(int x, int y, int m, int n, int[][] memo)
-    {
-        if(x == m-1 && y == n-1)
-        {
-            return 1;
-        }
-        
-        if(memo[x][y] != -1) return memo[x][y];
-        
-        int count = 0;
-        //down
-        if(isValid(x+1, y, m, n))
-        {
-            count += totalPaths(x+1, y, m, n, memo);
-        }
-        //right
-        if(isValid(x, y+1, m, n))
-        {
-            count+= totalPaths(x, y+1, m, n, memo);
-        }
-        return memo[x][y] = count;
-    }
-    public boolean isValid(int x, int y, int m, int n)
-    {
-        if(x < 0 || x >= m || y < 0 || y >= n) return false;
-        return true;
+        return dp[m-1][n-1];
     }
 }
