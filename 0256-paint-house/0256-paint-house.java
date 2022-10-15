9552e560b9e916ae17b629645bccd7e2cae2f75a
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-10-15 00:25:15 -0400

    Time: 2 ms (58.87%), Space: 41.5 MB (95.97%) - LeetHub

--- 0256-paint-house/0256-paint-house.java
+++ 0256-paint-house/0256-paint-house.java
@@ -26,29 +26,4 @@ class Solution {
         
         return dp[n-1][3];
     }
-    public int min(int n, int last, int[][] costs, int[][] memo)
-    {
-        if(n == 0)
-        {
-            int mini = Integer.MAX_VALUE;
-            for(int i=0; i<3; i++)
-            {
-                if(i!=last) mini = Math.min(mini, costs[0][i]);
-            }
-            return mini;
-        }
-        
-        if(memo[n][last] != 0) return memo[n][last];
-        
-        int mini = Integer.MAX_VALUE;
-        for(int i=0; i<3; i++)
-        {
-            if(i == last) continue;
-            
-            int cost = costs[n][i] + min(n-1, i, costs, memo);
-            mini = Math.min(mini, cost);
-        }
-        
-        return memo[n][last] = mini;
-    }
 }
