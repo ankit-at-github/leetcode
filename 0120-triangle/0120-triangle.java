7170c853d1efaf0b2f41304307c3ce0e010f5d1d
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-10-16 13:35:52 -0400

    Time: 4 ms (71.85%), Space: 42.2 MB (87.00%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,27 +1,21 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
-        int[][] memo = new int[n][n];
-        for(int i=0; i<n; i++)
+        int[][] dp = new int[n][n];
+        
+        //Storing last row values ---- base case
+        for(int j=0; j<n; j++) dp[n-1][j] = triangle.get(n-1).get(j);
+        
+        //Reverse DP
+        for(int i=n-2; i>=0; i--)
         {
-            for(int j=0; j<n; j++) memo[i][j] = -1;
+            for(int j=i; j>=0; j--)
+            {
+                int noStep = triangle.get(i).get(j) + dp[i+1][j];
+                int oneStep = triangle.get(i).get(j) + dp[i+1][j+1];
+                dp[i][j] = Math.min(noStep, oneStep);
+            }
         }
-        return minimumSum(0, 0, triangle, memo);
-    }
-    public int minimumSum(int row, int index, List<List<Integer>> triangle, int[][] memo)
-    {
-        if(row >= triangle.size()) return 0;
-        
-        if(memo[row][index] != -1) return memo[row][index];
-        
-        int min = Integer.MAX_VALUE;
-        
-        int noStep = triangle.get(row).get(index) + minimumSum(row+1, index, triangle, memo);
-        
-        int oneStep = triangle.get(row).get(index) + minimumSum(row+1, index+1, triangle, memo);
-        
-        min = Math.min(noStep, oneStep);
-        
-        return memo[row][index] = min;
+        return dp[0][0];
     }
 }
