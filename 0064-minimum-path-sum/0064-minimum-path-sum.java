b250eda8cd9b622d1f9a15d7c1554ad78d03b392
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-10-16 01:07:05 -0400

    Time: 6 ms (31.79%), Space: 46.3 MB (19.97%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -3,29 +3,25 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
         
-        int[][] memo = new int[m][n];
+        int[][] dp = new int[m][n];
         
         for(int i=0; i<m; i++)
         {
-            for(int j=0; j<n; j++) memo[i][j] = -1;
+            for(int j=0; j<n; j++)
+            {
+                if(i == 0 && j == 0) dp[i][j] = grid[i][j];
+                else
+                {
+                    int right = 100000000, down = 100000000;
+                    
+                    if(j > 0) right = grid[i][j] + dp[i][j-1];
+                    if(i > 0) down = grid[i][j] + dp[i-1][j];
+                    
+                    dp[i][j] = Math.min(right, down);
+                }
+            }
         }
         
-        return totalSum(0, 0, m, n, grid, memo);
-    }
-    public int totalSum(int x, int y, int m, int n, int[][] grid, int[][] memo)
-    {
-        if(x == m-1 && y == n-1) return grid[x][y];
-        
-        if(x < 0 || y < 0 || x >= m || y >= n) return 1000000000;
-        
-        if(memo[x][y] != -1) return memo[x][y];
-        
-        //down
-        int down = grid[x][y] + totalSum(x+1, y, m, n, grid, memo);
-        
-        //right
-        int right = grid[x][y] + totalSum(x, y+1, m, n, grid, memo);
-        
-        return memo[x][y] = Math.min(down, right);
+        return dp[m-1][n-1];
     }
 }
