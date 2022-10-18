af81102b4d83f1110bea1b8d7f66c96ad5479f18
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-10-18 15:05:53 -0400

    Time: 45 ms (6.30%), Space: 47.6 MB (41.58%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -3,41 +3,57 @@ class Solution {
         int m = matrix.length;
         int n = matrix[0].length;
         
-        int[][] memo = new int[m][n];
-        for(int i=0; i<m; i++)
+        int[][] dp = new int[m][n];
+        for(int j=0; j<n; j++) dp[0][j] = matrix[0][j];
+        
+        for(int i=1; i<m; i++)
         {
-            for(int j=0; j<n; j++) memo[i][j] = -1;
+            for(int j=0; j<n; j++)
+            {
+                int diagonal1 = Integer.MAX_VALUE;
+                int down = Integer.MAX_VALUE;
+                int diagonal2 = Integer.MAX_VALUE;
+                
+                if(j-1 >= 0) diagonal1 = matrix[i][j] + dp[i-1][j-1];
+                
+                down = matrix[i][j] + dp[i-1][j];
+                
+                if(j+1 < n) diagonal2 = matrix[i][j] + dp[i-1][j+1];
+                
+                dp[i][j] = Math.min(diagonal1, Math.min(down, diagonal2));
+            }
         }
         
         int mini = Integer.MAX_VALUE;
         
         for(int j=0; j<n; j++)
         {
-            mini = Math.min(mini, minSum(0, j, m, n, matrix, memo));
+            mini = Math.min(mini, dp[m-1][j]);
         }
+        
         return mini;
     }
-    public int minSum(int x, int y, int m, int n, int[][] matrix, int[][] memo)
-    {    
-        if(y < 0 || y >= n) return 10000000;
+//     public int minSum(int x, int y, int m, int n, int[][] matrix, int[][] memo)
+//     {    
+//         if(y < 0 || y >= n) return 10000000;
         
-        if(x == m-1) return matrix[x][y];
+//         if(x == m-1) return matrix[x][y];
         
-        if(memo[x][y] != -1) return memo[x][y];
+//         if(memo[x][y] != -1) return memo[x][y];
         
-        int min = Integer.MAX_VALUE;
+//         int min = Integer.MAX_VALUE;
         
-        //diagonal1
-        int diagonal1 = matrix[x][y] + minSum(x+1, y-1, m, n, matrix, memo);
+//         //diagonal1
+//         int diagonal1 = matrix[x][y] + minSum(x+1, y-1, m, n, matrix, memo);
             
-        //down
-        int down = matrix[x][y] + minSum(x+1, y, m, n, matrix, memo);
+//         //down
+//         int down = matrix[x][y] + minSum(x+1, y, m, n, matrix, memo);
             
-        //diagonal2
-        int diagonal2 = matrix[x][y] + minSum(x+1, y+1, m, n, matrix, memo);
+//         //diagonal2
+//         int diagonal2 = matrix[x][y] + minSum(x+1, y+1, m, n, matrix, memo);
             
-        min = Math.min(diagonal1, Math.min(down, diagonal2));
+//         min = Math.min(diagonal1, Math.min(down, diagonal2));
         
-        return memo[x][y] = min;
-    }
+//         return memo[x][y] = min;
+//     }
 }
