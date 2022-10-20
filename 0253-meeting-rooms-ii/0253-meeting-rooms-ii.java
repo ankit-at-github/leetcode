142db3950f1229285557ab3557c94bc10eb29266
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-10-20 14:41:43 -0400

    Time: 27 ms (9.99%), Space: 45.1 MB (58.70%) - LeetHub

---
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        Arrays.sort(intervals, (a,b) -> a[0]-b[0]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.add(intervals[0][1]);
+        
+        for(int i=1; i<intervals.length; i++)
+        {
+            if(pq.peek() <= intervals[i][0]) pq.poll();
+            pq.add(intervals[i][1]);
+        }
+        return pq.size();
+    }
+}
