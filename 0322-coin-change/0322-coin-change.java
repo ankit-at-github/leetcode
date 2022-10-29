35fe6651ce083f6c71c7e7467471a959e093ce61
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-10-29 14:35:19 -0400

    Time: 34 ms (51.01%), Space: 54.1 MB (19.82%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,33 +1,27 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        int[][] memo = new int[n][amount+1];
+        int[][] dp = new int[n][amount+1];
         
-        for(int i=0; i<n; i++)
+        for(int money=0; money<amount+1; money++)
         {
-            for(int j=0; j<amount+1; j++) memo[i][j] = -1;
+            if(money % coins[0] == 0)
+                dp[0][money] = money/coins[0];
+            else dp[0][money] = Integer.MAX_VALUE-1;
         }
         
-        int ans = counts(n-1, coins, amount, memo);
-        return ans >= Integer.MAX_VALUE-1 ? -1 : ans;
-    }
-    public int counts(int index, int[] coins, int amount, int[][] memo)
-    {
-        //base case
-        if(index == 0)
+        for(int index=1; index<n; index++)
         {
-            if(amount % coins[0] == 0) return amount/coins[0];
-            else return Integer.MAX_VALUE-1;
+            for(int money=0; money<amount+1; money++)
+            {
+                int notTake = dp[index-1][money];
+                
+                int take = Integer.MAX_VALUE;
+                if(coins[index] <= money) take = 1 + dp[index][money-coins[index]];
+                
+                dp[index][money] = Math.min(notTake, take);
+            }
         }
-        
-        if(memo[index][amount] != -1) return memo[index][amount];
-        
-        int notTake = counts(index-1, coins, amount, memo);
-        
-        int take = Integer.MAX_VALUE;
-        
-        if(coins[index] <= amount) take = 1 + counts(index, coins, amount-coins[index], memo);
-        
-        return memo[index][amount] = Math.min(notTake, take);
+        return dp[n-1][amount] >= Integer.MAX_VALUE-1 ? -1 : dp[n-1][amount];
     }
 }
