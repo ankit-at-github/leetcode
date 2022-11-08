5cd43f277583e8350fa29cf36c23d70a4ba90fd9
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-11-07 20:36:47 -0500

    Time: 816 ms (19.14%), Space: 135.4 MB (30.64%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -6,13 +6,15 @@ class Solution {
         int maxi = 0;
         for(int i=0; i<nums.length; i++)
         {
-            if(st.contains(nums[i]-1)) continue;
-            
-            int val = nums[i];
-            int count = 0;
-            while(st.contains(val++)) count++;
-            
-            maxi = Math.max(maxi, count);
+            if(!st.contains(nums[i]-1))
+            {
+                int val = nums[i];
+                while(st.contains(val))
+                {
+                    val+=1;
+                }
+                maxi = Math.max(maxi, val-nums[i]);
+            }
         }
         return maxi;
     }
