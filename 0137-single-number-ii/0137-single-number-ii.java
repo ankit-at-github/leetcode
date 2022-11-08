d56c5f504c48e1395fde37cd7b554b9c51180e47
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-11-08 18:26:13 -0500

    Time: 4 ms (82.82%), Space: 44.5 MB (35.07%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,17 +1,15 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int result = 0;
-        for(int i=0; i<32; i++)
+        //optimized approach using bitmasking
+        int result = 0 ;
+        for(int i=1; i<=32; i++)
         {
-            //to calculate number of one is each bit of all numbers
             int sum = 0;
             for(int num : nums)
             {
-                //getting ith bit of number and calculating sum. shift-operator shifted bit to get last bit use & with 1.
                 sum+=(num >> i) & 1;
             }
-            //storing 1 at ith location by masking 
-            result = result | (sum % 3 << i);
+            result = result | (sum%3 << i);
         }
         return result;
     }
