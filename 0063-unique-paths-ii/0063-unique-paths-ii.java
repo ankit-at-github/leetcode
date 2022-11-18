a510baa5d05a3f38261d7b153e122a6868e71f01
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-11-18 13:16:45 -0500

    Time: 2 ms (38.56%), Space: 42.3 MB (45.31%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -5,38 +5,28 @@ class Solution {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
         
-        int[][] memo = new int[m][n];
+        int[][] dp = new int[m][n];
+        
         for(int i=0; i<m; i++)
         {
-            for(int j=0; j<n; j++) memo[i][j] = -1;
-        }
-        return totalPaths(0, 0, m, n, memo, obstacleGrid);
-    }
-    public int totalPaths(int x, int y, int m, int n, int[][] memo, int[][] grid)
-    {
-        if(x == m-1 && y == n-1)
-        {
-            return 1;
+            for(int j=0; j<n; j++)
+            {
+                if(obstacleGrid[i][j] == 1) dp[i][j] = 0;
+                else
+                {
+                    if(i == 0 && j == 0) dp[i][j] = 1;
+                    else
+                    {
+                        int right = 0, down = 0;
+                        if(i > 0) down = dp[i-1][j];
+                        if(j > 0) right = dp[i][j-1];
+                        
+                        dp[i][j] = down + right;
+                    }
+                }
+            }
         }
         
-        if(memo[x][y] != -1) return memo[x][y];
-        
-        int count = 0;
-        //down
-        if(isValid(x+1, y, m, n, grid))
-        {
-            count += totalPaths(x+1, y, m, n, memo, grid);
-        }
-        //right
-        if(isValid(x, y+1, m, n, grid))
-        {
-            count+= totalPaths(x, y+1, m, n, memo, grid);
-        }
-        return memo[x][y] = count;
-    }
-    public boolean isValid(int x, int y, int m, int n, int[][] grid)
-    {
-        if(x < 0 || x >= m || y < 0 || y >= n || grid[x][y] == 1) return false;
-        return true;
+        return dp[m-1][n-1];
     }
 }
