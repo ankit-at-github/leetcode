af8fc8f4839398d43b9b338d9ae3d1b416d5b8a6
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-11-18 12:50:40 -0500

    Time: 0 ms (100.00%), Space: 40.8 MB (83.08%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,41 +1,40 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if(obstacleGrid[0][0] == 1) return 0;
+        
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
         
-        if(obstacleGrid[0][0] == 1) return 0;
-        
         int[][] memo = new int[m][n];
-        
         for(int i=0; i<m; i++)
         {
             for(int j=0; j<n; j++) memo[i][j] = -1;
         }
-        
-        return totalPaths(0, 0, m, n, obstacleGrid, memo);
+        return totalPaths(0, 0, m, n, memo, obstacleGrid);
     }
-    public int totalPaths(int x, int y, int m, int n, int[][] grid, int[][] memo)
-    {   
-        if(x == m-1 && y == n-1) return 1;
+    public int totalPaths(int x, int y, int m, int n, int[][] memo, int[][] grid)
+    {
+        if(x == m-1 && y == n-1)
+        {
+            return 1;
+        }
         
         if(memo[x][y] != -1) return memo[x][y];
         
         int count = 0;
-        
         //down
-        if(validPath(x+1, y, m, n, grid))
+        if(isValid(x+1, y, m, n, grid))
         {
-            count+=totalPaths(x+1, y, m, n, grid, memo);
+            count += totalPaths(x+1, y, m, n, memo, grid);
         }
         //right
-        if(validPath(x, y+1, m, n, grid))
+        if(isValid(x, y+1, m, n, grid))
         {
-            count+=totalPaths(x, y+1, m, n, grid, memo);
+            count+= totalPaths(x, y+1, m, n, memo, grid);
         }
-        
         return memo[x][y] = count;
     }
-    public boolean validPath(int x, int y, int m, int n, int[][] grid)
+    public boolean isValid(int x, int y, int m, int n, int[][] grid)
     {
         if(x < 0 || x >= m || y < 0 || y >= n || grid[x][y] == 1) return false;
         return true;
