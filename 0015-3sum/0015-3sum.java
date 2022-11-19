258be04b8e7218150505d38e29e4abd3d6a45b74
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-11-19 14:32:00 -0500

    Time: 30 ms (82.09%), Space: 58.7 MB (68.33%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,28 +1,30 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
+        
         List<List<Integer>> ans = new ArrayList<>();
-        for(int i=0; i<nums.length; i++)
+        for(int index=0; index<nums.length; index++)
         {
-            if(i > 0 && nums[i] == nums[i-1]) continue;
+            if(index > 0 && nums[index] == nums[index-1]) continue;
             
-            twoPointer(i, nums, ans);
+            twoPointer(index, nums, ans);
         }
         return ans;
     }
     public void twoPointer(int index, int[] nums, List<List<Integer>> ans)
     {
-        int left = index+1, right = nums.length-1;
-        while(left < right)
+        int start = index+1, end = nums.length-1;
+        while(start < end)
         {
-            if(nums[index] + nums[left] + nums[right] > 0) right--;
-            else if(nums[index] + nums[left] + nums[right] < 0) left++;
+            if(nums[index] + nums[start] + nums[end] > 0) end--;
+            else if(nums[index] + nums[start] + nums[end] < 0) start++;
             else
             {
-                ans.add(Arrays.asList(nums[index], nums[left], nums[right]));
-                left++;right--;
-                while(left < right && nums[left] == nums[left-1]) left++;
-                //while(right > left && nums[right] == nums[right+1]) right--;
+                ans.add(Arrays.asList(nums[index], nums[start], nums[end]));
+                start++;
+                end--;
+                
+                while(start < end && nums[start] == nums[start-1]) start++;
             }
         }
     }
