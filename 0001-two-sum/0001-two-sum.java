a9c0593505e3faabda52d45d7891ebf89087fa90
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-11-19 13:59:28 -0500

    Time: 8 ms (66.38%), Space: 45.3 MB (46.99%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,10 +1,5 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        //elements are not sorted so we can not use classic two-pointer approach
-        
-        //brute force approach would be to use two for loops which gives o(n^2) time complexity.
-        
-        //optimized approach
         Map<Integer, Integer> map = new HashMap<>();
         for(int i=0; i<nums.length; i++)
         {
