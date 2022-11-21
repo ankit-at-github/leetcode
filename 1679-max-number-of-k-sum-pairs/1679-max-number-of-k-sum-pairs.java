e9f1ca59e5c3ecf458318ec02965d46f3b9fcc4c
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-11-20 21:15:36 -0500

    Time: 36 ms (74.89%), Space: 57.2 MB (84.01%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
@@ -1,18 +1,18 @@
 class Solution {
     public int maxOperations(int[] nums, int k) {
+        //maximum number of unique pairs whose sum is equal to k
         Map<Integer,Integer> map = new HashMap<>();
-        int op=0;
+        int count =0 ;
         for(int i=0; i<nums.length; i++)
         {
-            int find = k-nums[i];
-            if(map.getOrDefault(find,0) > 0)
+            if(map.containsKey(k-nums[i]))
             {
-                op++;
-                map.put(find, map.get(find)-1);
+                count++;
+                if(map.get(k-nums[i]) == 1) map.remove(k-nums[i]);
+                else map.put(k-nums[i], map.get(k-nums[i])-1);
             }
-            else map.put(nums[i], map.getOrDefault(nums[i],0)+1);
+            else map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
         }
-        return op;
-        
+        return count;
     }
 }
