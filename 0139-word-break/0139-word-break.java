3102b98f978622b2efaa1b03587ab7d0ef4d2439
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2022-11-22 11:22:17 -0500

    Time: 25 ms (15.62%), Space: 48.7 MB (8.20%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -2,23 +2,22 @@ class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         Set<String> st = new HashSet<>(wordDict);
         
-        return find(0, s, st, new Boolean[s.length()]);
-    }
-    public boolean find(int index, String s, Set<String> st, Boolean[] memo)
-    {
-        if(index >= s.length()) return true;
+        boolean[] dp = new boolean[s.length()+1];
+        dp[0] = true;
         
-        if(memo[index] != null) return memo[index];
-        
-        StringBuilder sb = new StringBuilder();
-        for(int i=index; i<s.length(); i++)
+        for(int i=1; i<=s.length(); i++)
         {
-            sb.append(s.charAt(i));
-            if(st.contains(sb.toString()))
+            for(int j=0; j<i; j++)
             {
-                if(find(i+1, s, st, memo)) return memo[i] = true;
+                String sb = s.substring(j, i);
+                if(st.contains(sb) && dp[j])
+                {
+                    dp[i] = true;
+                    break;
+                }
             }
         }
-        return memo[index] = false;
+        
+        return dp[s.length()];
     }
 }
