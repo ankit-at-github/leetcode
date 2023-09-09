d1f08be7681ac0189d54d8e4c3276fe6089775e6
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2023-09-09 14:21:54 -0400

    Time: 2 ms (85.76%), Space: 41 MB (84.14%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -2,65 +2,58 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        
+        int freshOranges = 0;
         Queue<int[]> q = new LinkedList<>();
-        int fresh_oranges = 0;
         for(int i=0; i<m; i++)
         {
             for(int j=0; j<n; j++)
             {
-                if(grid[i][j] == 1) fresh_oranges++;
+                if(grid[i][j] == 1) freshOranges++;
                 if(grid[i][j] == 2) q.add(new int[]{i, j});
             }
         }
-        
-        if(fresh_oranges == 0) return 0;
-        
-        int time = -1;
+        if(freshOranges == 0) return 0;
+        int minutes = -1;
         while(!q.isEmpty())
         {
             for(int i=q.size(); i>0; i--)
             {
-                int[] coordinate = q.poll();
-                int x = coordinate[0];
-                int y = coordinate[1];
+                int x = q.peek()[0];
+                int y = q.poll()[1];
                 
-                //up
-                if(validCell(x-1, y, grid))
+                if(valid(x+1, y, m, n, grid))
                 {
-                    fresh_oranges--;
-                    grid[x-1][y] = 2;
-                    q.add(new int[]{x-1, y});
-                }
-                //down
-                if(validCell(x+1, y, grid))
-                {
-                    fresh_oranges--;
                     grid[x+1][y] = 2;
+                    freshOranges--;
                     q.add(new int[]{x+1, y});
                 }
-                //left
-                if(validCell(x, y-1, grid))
+                if(valid(x, y+1, m, n, grid))
                 {
-                    fresh_oranges--;
-                    grid[x][y-1] = 2;
-                    q.add(new int[]{x, y-1});
-                }
-                //right
-                if(validCell(x, y+1, grid))
-                {
-                    fresh_oranges--;
                     grid[x][y+1] = 2;
+                    freshOranges--;
                     q.add(new int[]{x, y+1});
                 }
+                if(valid(x-1, y, m, n, grid))
+                {
+                    grid[x-1][y] = 2;
+                    freshOranges--;
+                    q.add(new int[]{x-1, y});
+                }
+                if(valid(x, y-1, m, n, grid))
+                {
+                    grid[x][y-1] = 2;
+                    freshOranges--;
+                    q.add(new int[]{x, y-1});
+                }
             }
-            time++;
+            minutes++;
         }
-        return fresh_oranges == 0?time:-1;
+        if(freshOranges!=0) return -1;
+        return minutes;
     }
-    public boolean validCell(int x, int y, int[][] grid)
+    public boolean valid(int x, int y, int m, int n, int[][] grid)
     {
-        if(x < 0 || x >= grid.length || y < 0 || y>=grid[0].length || grid[x][y]!=1) return false;
+        if(x<0 || y<0 || x>=m || y>=n || grid[x][y]!=1) return false;
         return true;
     }
 }
