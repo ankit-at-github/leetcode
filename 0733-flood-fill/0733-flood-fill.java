a3101b5417fc7ba43a2eb5ee4dc2413578cd6704
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2023-09-09 11:10:29 -0400

    Time: 0 ms (100.00%), Space: 44.7 MB (8.81%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,49 +1,20 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int original = image[sr][sc];
-        if(original == color) return image;
-        
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{sr, sc});
-        image[sr][sc] = color;
-        
-        while(!q.isEmpty())
-        {
-            int[] coordinate = q.poll();
-            int x = coordinate[0];
-            int y = coordinate[1];
-            
-            if(validPath(x-1, y, image, original))
-            {
-                image[x-1][y] = color;
-                q.add(new int[]{x-1, y});
-            }
-            
-            if(validPath(x, y-1, image, original))
-            {
-                image[x][y-1] = color;
-                q.add(new int[]{x, y-1});
-            }
-            
-            if(validPath(x, y+1, image, original))
-            {
-                image[x][y+1] = color;
-                q.add(new int[]{x, y+1});
-            }
-            
-            if(validPath(x+1, y, image, original))
-            {
-                image[x+1][y] = color;
-                q.add(new int[]{x+1, y});
-            }
-            
-        }
+        if(image[sr][sc] == color) return image;
+        int m = image.length;
+        int n = image[0].length;
+        int pixel = image[sr][sc];
+        dfs(image, sr, sc, m, n, color, pixel);
         return image;
     }
-    public boolean validPath(int x, int y, int[][] image, int original)
+    public void dfs(int[][] image, int x, int y, int m, int n, int color, int pixel)
     {
-        if(x < 0 || x >= image.length || y < 0 || y >= image[0].length || image[x][y] != original) return false;
+        if(x < 0 || y < 0 || x >= m || y >= n || image[x][y] != pixel) return;
         
-        return true;
+        image[x][y] = color;
+        dfs(image, x+1, y, m, n, color, pixel);
+        dfs(image, x, y+1, m, n, color, pixel);
+        dfs(image, x, y-1, m, n, color, pixel);
+        dfs(image, x-1, y, m, n, color, pixel);
     }
 }
