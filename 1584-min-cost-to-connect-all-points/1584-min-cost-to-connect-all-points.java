4a937263259b92436453cbaf8de096d25ccb0765
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2023-09-14 21:34:07 -0400

    Time: 161 ms (54.92%), Space: 43.1 MB (94.71%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,37 +1,38 @@
 class Solution {
     public int minCostConnectPoints(int[][] points) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1]-b[1]);
-        pq.add(new int[]{0, 0});
-        
-        boolean[] mst = new boolean[points.length];
-        
-        int[] cost = new int[points.length];
+        int p = points.length;
+        int[] cost = new int[p];
         Arrays.fill(cost, Integer.MAX_VALUE);
-        
+        //Starting Node cost will be zero.
         cost[0] = 0;
-        
+        //To track visited node
+        boolean[] mst = new boolean[p];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1]-b[1]);
+        pq.add(new int[]{0,0});
         while(!pq.isEmpty())
         {
             int currentNode = pq.peek()[0];
             int weight = pq.poll()[1];
-            
             mst[currentNode] = true;
-            for(int nextNode = 0; nextNode < points.length; nextNode++)
+            for(int nextNode=0; nextNode<p; nextNode++)
             {
-                int dist = distance(points[currentNode][0], points[nextNode][0], points[currentNode][1], points[nextNode][1]);
-                if(cost[nextNode] > dist && !mst[nextNode])
+                if(!mst[nextNode])
                 {
-                    cost[nextNode] = dist;
-                    pq.add(new int[]{nextNode, dist});
+                                    int distance = mDistance(points[currentNode][0], points[nextNode][0], points[currentNode][1], points[nextNode][1]);
+                if(cost[nextNode] > distance)
+                {
+                    cost[nextNode] = distance;
+                    pq.add(new int[]{nextNode, distance});
+                }
                 }
             }
         }
-        int total = 0;
-        for(int x : cost) total+=x;
-        return total;
+        int minCost = 0;
+        for(int x : cost) minCost+=x;
+        return minCost;
     }
-    public int distance(int x1, int x2, int y1, int y2)
+    public int mDistance(int x1, int x2, int y1, int y2)
     {
-        return (Math.abs(x1-x2) + Math.abs(y1-y2));
+        return Math.abs(x1-x2) + Math.abs(y1-y2);
     }
 }
