ce2630d0368aaad03945675be80c3f4e707723cc
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2023-10-05 23:08:57 -0400

    Time: 0 ms (100.00%), Space: 39.2 MB (39.77%) - LeetHub

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -1,24 +1,21 @@
 class Solution {
     public int integerBreak(int n) {
         if(n <= 3) return n-1;
-        
         int[] memo = new int[n+1];
-        
         return helper(n, memo);
     }
     public int helper(int n, int[] memo)
-    {   
-        if(n <= 3) return n;
+    {
+        if(n == 0) return 1;
         
         if(memo[n] != 0) return memo[n];
         
-        int ans = n;
+        int max_product = n;
         
-        for(int i=2; i<n; i++)
-        {
-            ans = Math.max(ans, i * helper(n-i, memo));
-        }
-        memo[n] = ans;
-        return ans;
+        for(int i=1; i<n; i++)
+            max_product = Math.max(max_product, i * helper(n-i, memo));
+        
+        memo[n] = max_product;
+        return memo[n];
     }
 }
