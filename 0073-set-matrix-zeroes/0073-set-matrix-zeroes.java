a9e7df8bdb7d4ab4b3b4ce8e932b907680fe9b2a
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2024-08-26 22:35:56 -0400

    Time: 1 ms (79.27%), Space: 45.3 MB (93.99%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,34 +1,30 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        //Not touching corners of matrix
         int m = matrix.length;
         int n = matrix[0].length;
-        
-        List<Integer> rows = new ArrayList<>();
-        List<Integer> cols = new ArrayList<>();
-        
-        for(int i=0; i<m; i++)
-        {
-            for(int j=0; j<n; j++)
-            {
-                if(matrix[i][j] == 0)
-                {
-                    rows.add(i);
-                    cols.add(j);
+        boolean firstCol = false;
+        for(int i=0; i<m; i++){
+            if(matrix[i][0] == 0) firstCol = true;
+            for(int j=1; j<n; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
-        
-        //Row
-        for(int r :rows)
-        {
-            for(int j=0; j<n; j++) matrix[r][j] = 0;
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
+            }
+        }
+        //Marking 1st Row
+        if(matrix[0][0] == 0){
+            for(int j=0; j<n; j++) matrix[0][j] = 0;
+        }
+        //Marking 1st Col
+        if(firstCol){
+            for(int i=0; i<m; i++) matrix[i][0] = 0;
         }
         
-        //Cols
-        for(int c :cols)
-        {
-            for(int i=0; i<m; i++) matrix[i][c] = 0;
-        }
     }
 }
