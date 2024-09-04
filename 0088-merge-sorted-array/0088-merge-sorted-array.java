dc9acce43e6848bc481a94465ac28509fb2da860
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2024-09-04 19:21:19 -0400

    Time: 0 ms (100.00%), Space: 41.9 MB (92.07%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,31 +1,28 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int left = m-1, right = n-1;
         int index = m+n-1;
-        while(m > 0 && n > 0)
-        {
-            int a = nums1[m-1];
-            int b = nums2[n-1];
-            
-            if(a > b)
-            {
-                nums1[index--] = a;
-                m--;
+        while(left >= 0 && right >= 0){
+            if(nums1[left] >= nums2[right]){
+                nums1[index] = nums1[left];
+                index--;
+                left--;
             }
-            else
-            {
-                nums1[index--] = b;
-                n--;
+            else{
+                nums1[index] = nums2[right];
+                index--;
+                right--;
             }
         }
-        while(m > 0)
-        {
-            nums1[index--] = nums1[m-1];
-            m--;
+        while(left>=0){
+            nums1[index] = nums1[left];
+            index--;
+            left--;
         }
-        while(n > 0)
-        {
-            nums1[index--] = nums2[n-1];
-            n--;
+        while(right>=0){
+            nums1[index] = nums2[right];
+            index--;
+            right--;
         }
     }
 }
