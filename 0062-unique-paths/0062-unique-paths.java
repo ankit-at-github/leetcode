6042b46970898523bbf7324c2c44e2d48a25f054
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2024-09-16 22:01:20 -0400

    Time: 0 ms (100.00%), Space: 40.6 MB (18.50%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,22 +1,27 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
+        int[][] memo = new int[m][n];
+        for(int i=0; i<m; i++) Arrays.fill(memo[i], -1);
+        return solve(0, 0, m, n, memo);
+    }
+    public int solve(int x, int y, int m, int n, int[][] memo){
+        // 1. Base Case
         
-        for(int i=0; i<m; i++)
-        {
-            for(int j=0; j<n; j++)
-            {
-                if(i == 0 && j == 0) dp[i][j] = 1;
-                else
-                {
-                    int right = 0, down = 0;
-                    if(i > 0) down = dp[i-1][j];
-                    if(j > 0) right = dp[i][j-1];
-                    
-                    dp[i][j] = down+right;
-                }
-            }
-        }   
-        return dp[m-1][n-1];
+        //not my path
+        if(x < 0 || y < 0 || x >= m || y >= n) return 0;
+        
+        if(x == m-1 && y == n-1){
+            return 1;
+        }
+        
+        if(memo[x][y] != -1) return memo[x][y];
+        
+        // 2. Exploring ways
+        //right
+        int right = solve(x, y+1, m, n, memo);
+        //down
+        int down = solve(x+1, y, m, n, memo);
+        
+        return memo[x][y] = right + down;
     }
 }
