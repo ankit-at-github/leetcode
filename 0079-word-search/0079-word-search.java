fff6e45652d23db18206a61b4016fc7920c8d6b1
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2024-10-06 11:35:33 -0400

    Time: 157 ms (39.88%), Space: 41.1 MB (97.73%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -5,25 +5,25 @@ class Solution {
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(board[i][j] == word.charAt(0)){
-                    if(wordSearch(board, i, j, m, n, 0, word)) return true;
+                    if(wordSearch(board, i, j, m, n, 0, word, new boolean[m][n])) return true;
                 }
             }
         }
         return false;
     }
-    public boolean wordSearch(char[][] board, int x, int y, int m, int n, int index, String word){
-        if(x < 0 || y < 0 || x >=m || y>= n || board[x][y] != word.charAt(index)) return false;
+    public boolean wordSearch(char[][] board, int x, int y, int m, int n, int index, String word, boolean[][] visited){
+        if(index >= word.length()) return true;
         
-        if(index == word.length()-1) return true;
+        if(x < 0 || y < 0 || x >=m || y>= n || visited[x][y] || board[x][y] != word.charAt(index)) return false;
         
-        board[x][y] = '#';
+        visited[x][y] = true;
         
-        boolean right = wordSearch(board, x, y+1, m, n, index+1, word);
-        boolean down = wordSearch(board, x+1, y, m, n, index+1, word);
-        boolean left = wordSearch(board, x, y-1, m, n, index+1, word);
-        boolean up = wordSearch(board, x-1, y, m, n, index+1, word);
+        boolean right = wordSearch(board, x, y+1, m, n, index+1, word, visited);
+        boolean down = wordSearch(board, x+1, y, m, n, index+1, word, visited);
+        boolean left = wordSearch(board, x, y-1, m, n, index+1, word, visited);
+        boolean up = wordSearch(board, x-1, y, m, n, index+1, word, visited);
         
-        board[x][y] = word.charAt(index);
+        visited[x][y] = false;
         
         return (right||down||left||up);
     }
