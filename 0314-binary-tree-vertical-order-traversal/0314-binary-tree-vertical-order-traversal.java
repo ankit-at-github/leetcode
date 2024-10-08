ac81aac114918a170c49c97d4b41bbfefedca182
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2024-10-07 20:55:50 -0400

    Time: 3 ms (58.51%), Space: 42.2 MB (99.14%) - LeetHub

---
+++ 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Pair{
+    int axis;
+    TreeNode node;
+    Pair(int _axis, TreeNode _node){
+        axis = _axis;
+        node = _node;
+    }
+}
+class Solution {
+    public List<List<Integer>> verticalOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if(root == null) return ans;
+        TreeMap<Integer, List<Integer>> tmap = new TreeMap<>();
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(0, root));
+        while(!q.isEmpty()){
+            for(int i=q.size(); i>0; i--){
+                Pair p = q.poll();
+                int axis = p.axis;
+                TreeNode node = p.node;
+                
+                if(!tmap.containsKey(axis)) tmap.put(axis, new ArrayList<>());
+                tmap.get(axis).add(node.val);
+                
+                if(node.left!=null) q.add(new Pair(axis-1, node.left));
+                if(node.right!=null) q.add(new Pair(axis+1, node.right));
+            }
+        }
+        for(List<Integer> l : tmap.values()){
+            ans.add(l);
+        }
+        return ans;
+    }
+}
