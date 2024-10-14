ccab86f50480875c421bb0d22c07f2d3f2ed2409
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2024-10-14 19:01:26 -0400

    Time: 23 ms (78.76%), Space: 53.9 MB (35.06%) - LeetHub

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -0,0 +1,31 @@
+class Pair{
+    int distance;
+    int index;
+    Pair(int _distance, int _index){
+        distance = _distance;
+        index = _index;
+    }
+}
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> a.distance - b.distance);
+        for(int i=0; i<points.length; i++){
+            int distance = getEdistance(points[i][0], points[i][1]);
+            pq.add(new Pair(distance, i));
+        }
+        int[][] ans = new int[k][2];
+        int index = 0;
+        while(k > 0){
+            Pair p = pq.poll();
+            ans[index][0] = points[p.index][0];
+            ans[index][1] = points[p.index][1];
+            index++;
+            k--;
+        }
+        return ans;
+    }
+    public int getEdistance(int x1, int y1){
+        int val = (x1-0)*(x1-0) + (y1-0)*(y1-0);
+        return val;
+    }
+}
