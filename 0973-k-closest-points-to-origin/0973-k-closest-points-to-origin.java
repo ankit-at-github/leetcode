ef42e9714b9e3210a2735de144c9b932683c7cd6
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2024-10-14 19:18:00 -0400

    Time: 32 ms (47.43%), Space: 54.7 MB (28.86%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,31 +1,19 @@
-class Pair{
-    int distance;
-    int index;
-    Pair(int _distance, int _index){
-        distance = _distance;
-        index = _index;
-    }
-}
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> a.distance - b.distance);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> b[0] - a[0]);
         for(int i=0; i<points.length; i++){
-            int distance = getEdistance(points[i][0], points[i][1]);
-            pq.add(new Pair(distance, i));
+            int distance = getEdistance(points[i]);
+            pq.add(new int[]{distance, i});
+            if(pq.size() > k) pq.poll();
         }
         int[][] ans = new int[k][2];
-        int index = 0;
-        while(k > 0){
-            Pair p = pq.poll();
-            ans[index][0] = points[p.index][0];
-            ans[index][1] = points[p.index][1];
-            index++;
-            k--;
+        for(int i=0; i<k; i++){
+            int index = pq.poll()[1];
+            ans[i] = points[index];
         }
         return ans;
     }
-    public int getEdistance(int x1, int y1){
-        int val = x1*x1 + y1*y1;
-        return val;
+    public int getEdistance(int[] point){
+        return point[0]*point[0] + point[1]*point[1];
     }
 }
