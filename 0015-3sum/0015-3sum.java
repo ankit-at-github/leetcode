184b48add1d6f0b0c3f677063636affbff80a1b9
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2024-10-14 09:11:31 -0400

    Time: 23 ms (99.04%), Space: 52.5 MB (18.77%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,30 +1,29 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
-        
-        List<List<Integer>> ans = new ArrayList<>();
-        for(int index=0; index<nums.length; index++)
-        {
-            if(index > 0 && nums[index] == nums[index-1]) continue;
-            
-            twoPointer(index, nums, ans);
+        for(int i=0; i<=nums.length-3; i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            twoSum(i+1, nums, -nums[i], result);
         }
-        return ans;
+        return result;
     }
-    public void twoPointer(int index, int[] nums, List<List<Integer>> ans)
-    {
-        int start = index+1, end = nums.length-1;
-        while(start < end)
-        {
-            if(nums[index] + nums[start] + nums[end] > 0) end--;
-            else if(nums[index] + nums[start] + nums[end] < 0) start++;
-            else
-            {
-                ans.add(Arrays.asList(nums[index], nums[start], nums[end]));
+    public void twoSum(int start, int[] nums, int target, List<List<Integer>> result){
+        int end = nums.length-1;
+        while(start < end){
+            if(nums[start] + nums[end] > target) end--;
+            else if(nums[start] + nums[end] < target) start++;
+            else{
+                List<Integer> current = new ArrayList<>();
+                current.add(-target);
+                current.add(nums[start]);
+                current.add(nums[end]);
+                result.add(current);
+                
+                while(start < end && nums[start] == nums[start+1]){ start++;}
+                
                 start++;
                 end--;
-                
-                while(start < end && nums[start] == nums[start-1]) start++;
             }
         }
     }
