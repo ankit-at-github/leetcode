be11a142830faf600a2c2d68d3df633be85e9742
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2024-10-15 04:32:58 -0400

    Time: 23 ms (92.53%), Space: 46.5 MB (35.20%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -4,13 +4,18 @@ class Solution {
         //Get Subarray of all size O(N^2)
         //check if sum is equal to K, increment count. O(N)
         //TC: O(N^2)
+        Map<Integer, Integer> prefixSum = new HashMap<>();
         int count = 0;
+        int sum = 0;
         for(int start=0; start<nums.length; start++){
-            int sum = 0;
-            for(int end=start; end<nums.length; end++){
-                sum+=nums[end];
-                if(sum == k) count++;
+            sum+=nums[start];
+            
+            if(sum == k) count++;
+            
+            if(prefixSum.containsKey(sum-k)){
+                count+=prefixSum.get(sum-k);
             }
+            prefixSum.put(sum, prefixSum.getOrDefault(sum, 0)+1);
         }
         return count;
     }
