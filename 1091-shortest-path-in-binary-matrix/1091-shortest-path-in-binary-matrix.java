9f0ee9e98654b3120ff029c93d5939edc77584a0
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2024-10-16 08:54:34 -0400

    Time: 37 ms (11.20%), Space: 45.3 MB (65.92%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,86 +1,40 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
-        return bfsSPBM(grid, grid.length);
-    }
-    public int bfsSPBM(int[][] grid, int n)
-    {
-        if(grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;
+        int m = grid.length;
+        int n = grid[0].length;
         
-        Queue<int[]> q = new LinkedList<int[]>();
-        q.add(new int[]{0, 0});
+        if(grid[0][0] == 1 || grid[m-1][n-1] == 1) return -1;
         
-        int ans = 0;
-        while(!q.isEmpty())
-        {
-            for(int i=q.size(); i>0; i--)
-            {
-                int x = q.peek()[0];
-                int y = q.poll()[1];
+        boolean[][] visited = new boolean[m][n];
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0, 0, 0});
+        int mini = Integer.MAX_VALUE;
+        while(!q.isEmpty()){
+            for(int i=q.size(); i>0; i--){
+                int[] arr = q.poll();
+                int x = arr[0];
+                int y = arr[1];
+                int len = arr[2];
                 
-                //grid[x][y] = 1;
+                if(x < 0 || y < 0 || x >= m || y >= n || visited[x][y] || grid[x][y] == 1) continue;
                 
-                if(x == n-1 && y == n-1)
-                {
-                    return ans+1;
+                visited[x][y] = true;
+                
+                if(x == m-1 && y == n-1){
+                    mini = Math.min(mini, len);
                 }
                 
-                //1
-                if(isValid(x-1, y-1, grid, n))
-                {
-                    grid[x-1][y-1] = 1;
-                    q.add(new int[]{x-1, y-1});
-                }
-                //2
-                if(isValid(x-1, y, grid, n))
-                {
-                    grid[x-1][y] = 1;
-                    q.add(new int[]{x-1, y});
-                }
-                //3
-                if(isValid(x-1, y+1, grid, n))
-                {
-                    grid[x-1][y+1] = 1;
-                    q.add(new int[]{x-1, y+1});
-                }
-                //4
-                if(isValid(x, y-1, grid, n))
-                {
-                    grid[x][y-1] = 1;
-                    q.add(new int[]{x, y-1});
-                }
-                //5
-                if(isValid(x, y+1, grid, n))
-                {
-                    grid[x][y+1] = 1;
-                    q.add(new int[]{x, y+1});
-                }
-                //6
-                if(isValid(x+1, y-1, grid, n))
-                {
-                    grid[x+1][y-1] = 1;
-                    q.add(new int[]{x+1, y-1});
-                }
-                //7
-                if(isValid(x+1, y, grid, n))
-                {
-                    grid[x+1][y] = 1;
-                    q.add(new int[]{x+1, y});
-                }
-                //8
-                if(isValid(x+1, y+1, grid, n))
-                {
-                    grid[x+1][y+1] = 1;
-                    q.add(new int[]{x+1, y+1});
-                }
+                //8 directional
+                q.add(new int[] {x, y+1, len+1});
+                q.add(new int[] {x+1, y+1, len+1});
+                q.add(new int[] {x+1, y, len+1});
+                q.add(new int[] {x+1, y-1, len+1});
+                q.add(new int[] {x, y-1, len+1});
+                q.add(new int[] {x-1, y-1, len+1});
+                q.add(new int[] {x-1, y, len+1});
+                q.add(new int[] {x-1, y+1, len+1});
             }
-            ans++;
         }
-        
-        return -1;
-    }
-    public boolean isValid(int x, int y, int[][] grid, int n)
-    {
-        if(x < 0 || x >= n || y < 0 || y >= n || grid[x][y] == 1) return false;
-        return true;
+        return mini==Integer.MAX_VALUE ? -1 : mini+1 ;
     }
 }
