a18504be22d9e34dbcbeb0a61d15fc280ac21146
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2024-10-16 08:57:22 -0400

    Time: 29 ms (22.78%), Space: 45.3 MB (76.72%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -6,33 +6,34 @@ class Solution {
         if(grid[0][0] == 1 || grid[m-1][n-1] == 1) return -1;
         
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{0, 0, 0});
+        q.add(new int[]{0, 0});
         int mini = Integer.MAX_VALUE;
+        int level = 0;
         while(!q.isEmpty()){
             for(int i=q.size(); i>0; i--){
                 int[] arr = q.poll();
                 int x = arr[0];
                 int y = arr[1];
-                int len = arr[2];
                 
                 if(x < 0 || y < 0 || x >= m || y >= n || grid[x][y] == 1) continue;
                 
                 grid[x][y] = 1;
                 
                 if(x == m-1 && y == n-1){
-                    mini = Math.min(mini, len);
+                    mini = Math.min(mini, level);
                 }
                 
                 //8 directional
-                q.add(new int[] {x, y+1, len+1});
-                q.add(new int[] {x+1, y+1, len+1});
-                q.add(new int[] {x+1, y, len+1});
-                q.add(new int[] {x+1, y-1, len+1});
-                q.add(new int[] {x, y-1, len+1});
-                q.add(new int[] {x-1, y-1, len+1});
-                q.add(new int[] {x-1, y, len+1});
-                q.add(new int[] {x-1, y+1, len+1});
+                q.add(new int[] {x, y+1});
+                q.add(new int[] {x+1, y+1});
+                q.add(new int[] {x+1, y});
+                q.add(new int[] {x+1, y-1});
+                q.add(new int[] {x, y-1});
+                q.add(new int[] {x-1, y-1});
+                q.add(new int[] {x-1, y});
+                q.add(new int[] {x-1, y+1});
             }
+            level++;
         }
         return mini==Integer.MAX_VALUE ? -1 : mini+1 ;
     }
