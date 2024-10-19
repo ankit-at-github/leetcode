8b16eef39a0a2654f18f74f1af64c2e5b4c0bb92
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2024-10-19 10:16:48 -0400

    Time: 27 ms (76.05%), Space: 72.7 MB (31.03%) - LeetHub

--- 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.java
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.java
@@ -3,16 +3,18 @@ class Solution {
         List<List<Integer>> res = new ArrayList<>();
         int total = 0;
         for(int row = 0; row < nums.size(); row++){
+            total+=nums.get(row).size();
             for(int col = 0; col < nums.get(row).size(); col++){
                 if(res.size() < row+col+1) res.add(row+col, new ArrayList());
                 res.get(row+col).add(0, nums.get(row).get(col));
-                total++;
             }
         }
         int[] ans = new int[total];
         int index = 0;
-        for(List<Integer> l : res){
-            for(int n : l) ans[index++] = n;
+        for(int i=0; i<res.size(); i++){
+            for(int j=0; j<res.get(i).size(); j++){
+                ans[index++] = res.get(i).get(j);
+            }
         }
         return ans;
     }
