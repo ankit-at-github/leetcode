cd20099d5c5299492bc8976d7f8e09966663dbba
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2024-10-27 22:32:42 -0400

    Time: 29 ms (53.66%), Space: 44.6 MB (46.75%) - LeetHub

--- 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.java
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.java
@@ -12,7 +12,7 @@ class Solution {
     public Node lowestCommonAncestor(Node p, Node q) {
         Node p1 = p;
         Node p2 = q;
-        while(p1!=p2){
+        while(p1 != p2){
             p1 = p1 == null ? q : p1.parent;
             p2 = p2 == null ? p : p2.parent;
         }
