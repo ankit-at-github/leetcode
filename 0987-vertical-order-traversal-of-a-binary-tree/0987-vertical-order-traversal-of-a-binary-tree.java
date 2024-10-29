1a7598a7755baa049b98051fb5f45da802cdce3b
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2024-10-29 00:25:06 -0400

    Time: 4 ms (44.52%), Space: 42.7 MB (16.23%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
@@ -13,24 +13,11 @@
  *     }
  * }
  */
-class Tuple{
-    TreeNode node;
-    int axis;
-    int level;
-    Tuple(TreeNode node, int axis, int level)
-    {
-        this.node = node;
-        this.axis = axis;
-        this.level = level;
-    }
-}
 class Solution {
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        List<List<Integer>> ans = new ArrayList<>();
-        if(root == null) return ans;
         TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> tmap = new TreeMap<>();
         Queue<Tuple> q = new LinkedList<>();
-        q.add(new Tuple(root, 0, 0));
+        q.add(new Tuple(root, 0, 1));
         while(!q.isEmpty()){
             for(int i=q.size(); i>0; i--){
                 Tuple t = q.poll();
@@ -43,20 +30,29 @@ class Solution {
                 
                 tmap.get(axis).get(level).add(current.val);
                 
-                if(current.left!=null) q.add(new Tuple(current.left, axis-1, level+1));
-                if(current.right!=null) q.add(new Tuple(current.right, axis+1, level+1));
+                if(current.left != null) q.add(new Tuple(current.left, axis-1, level+1));
+                if(current.right != null) q.add(new Tuple(current.right, axis+1, level+1));
             }
         }
-        
+        List<List<Integer>> ans = new ArrayList<>();
         for(TreeMap<Integer, PriorityQueue<Integer>> map : tmap.values()){
             List<Integer> current = new ArrayList<>();
             for(PriorityQueue<Integer> pq : map.values()){
-                while(!pq.isEmpty()){
-                    current.add(pq.poll());
-                }
+                while(!pq.isEmpty()) current.add(pq.poll());
             }
             ans.add(current);
         }
         return ans;
+    }
+}
+class Tuple{
+    TreeNode node;
+    int axis;
+    int level;
+    
+    Tuple(TreeNode _node, int _axis, int _level){
+        node = _node;
+        axis = _axis;
+        level = _level;
     }
 }
