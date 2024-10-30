a6ac83ed028be57001b2619ab5bdc107175e3c43
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2024-10-29 23:47:47 -0400

    Time: 3 ms (79.94%), Space: 61.8 MB (5.36%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.java
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.java
@@ -1,20 +1,20 @@
 class Solution {
     public int[] findBuildings(int[] heights) {
         int n = heights.length;
-        if(n == 1) return new int[]{0};
         List<Integer> ans = new ArrayList<>();
-        int previousHeight = heights[n-1];
         ans.add(n-1);
-        for(int i=heights.length-2; i>=0; i--){
-            
-            if(heights[i] > previousHeight){
-                previousHeight = heights[i];
-                ans.add(i);
+        int maxi = heights[n-1];
+        for(int right=n-2; right>=0; right--){
+            if(heights[right] > maxi){
+                maxi = Math.max(maxi, heights[right]);
+                ans.add(right);
             }
         }
         int[] result = new int[ans.size()];
         int index = 0;
-        for(int i=ans.size()-1; i>=0; i--) result[index++] = ans.get(i);
+        for(int i=ans.size()-1; i>=0; i--){
+            result[index++] = ans.get(i);
+        }
         return result;
     }
 }
