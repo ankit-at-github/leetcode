ba875c83de2265e8c13adeb4e67cd33f2ae3e09c
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2024-10-30 21:16:27 -0400

    Time: 15 ms (91.84%), Space: 55.2 MB (23.48%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,19 +1,48 @@
 class Solution {
+    private int[][] points;
+    
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> b[0] - a[0]);
-        for(int i=0; i<points.length; i++){
-            int distance = getEdistance(points[i]);
-            pq.add(new int[]{distance, i});
-            if(pq.size() > k) pq.poll();
+	
+        this.points = points;
+        int low = 0;
+        int high = 0;
+		
+	 // looking for the range of low and high
+        for(int i = 0; i < points.length; i ++){
+            high = Math.max(high, distanceCal(points[i]));
+            low = Math.min(low, distanceCal(points[i]));
         }
-        int[][] ans = new int[k][2];
-        for(int i=0; i<k; i++){
-            int index = pq.poll()[1];
-            ans[i] = points[index];
+		
+        while(low <= high){
+            int pivot = low + (high - low)/2;
+            if(distance(pivot, k) > k){
+                high = pivot - 1;
+            }else
+                low = pivot + 1 ;
         }
-        return ans;
+        
+        int[][] res = new int[k][2];
+        int j = 0;
+        for(int i = 0; i < points.length; i ++){
+            if(distanceCal(points[i]) < low){
+                res[j ++] = points[i];
+            }
+        }
+        return res;
     }
-    public int getEdistance(int[] point){
-        return point[0]*point[0] + point[1]*point[1];
+	
+    // finding the number of points whether their distance is below threshold
+    public int distance(int threshold, int k){
+        int counter = 0;
+        for(int[] point : points){
+            if(distanceCal(point) <= threshold){
+                counter ++;
+            }
+        }
+        return counter;
+    }
+    
+    public int distanceCal(int[] point){
+        return point[0] * point[0] + point[1] * point[1];
     }
 }
