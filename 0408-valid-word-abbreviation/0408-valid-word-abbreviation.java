0cd365b228510f42d2a8413d4db1ff9a211f4c01
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2024-10-31 16:19:56 -0400

    Time: 0 ms (100.00%), Space: 41.7 MB (65.02%) - LeetHub

--- 0408-valid-word-abbreviation/0408-valid-word-abbreviation.java
+++ 0408-valid-word-abbreviation/0408-valid-word-abbreviation.java
@@ -1,22 +1,25 @@
 class Solution {
     public boolean validWordAbbreviation(String word, String abbr) {
-        int p1 = 0, p2 = 0;
+        int p1=0, p2=0;
         int num = 0;
         while(p2 < abbr.length()){
             char c = abbr.charAt(p2);
             if(Character.isDigit(c)){
-                num = num*10 + c-'0';
+                num = num * 10 + c-'0';
                 if(num == 0) return false;
-            }else{
+            }
+            else{
                 p1+=num;
-                
-                if(p1 >= word.length() || word.charAt(p1) != abbr.charAt(p2)) return false;
-                
                 num = 0;
+                
+                if(p1 >= word.length() || c != word.charAt(p1)) return false;
+                
                 p1++;
-            }  
+                
+            }
             p2++;
         }
-        return p1+num==word.length();
+        
+        return p1+num == word.length();
     }
 }
