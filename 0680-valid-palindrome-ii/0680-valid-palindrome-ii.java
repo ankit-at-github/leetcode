b6634fe8cdf05c8b849e80364881f93ec9e2ea5f
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2024-10-31 17:54:19 -0400

    Time: 4 ms (99.68%), Space: 45.3 MB (44.92%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -3,16 +3,16 @@ class Solution {
         int left = 0, right = s.length()-1;
         while(left < right){
             if(s.charAt(left) != s.charAt(right)){
-                return helper(s, left, right-1) || helper(s, left+1, right);
+                return (helper(s, left+1, right) || helper(s, left, right-1));
             }
             left++;
             right--;
         }
         return true;
     }
-    public boolean helper(String word, int left, int right){
+    public boolean helper(String s, int left, int right){
         while(left < right){
-            if(word.charAt(left) != word.charAt(right)) return false;
+            if(s.charAt(left) != s.charAt(right)) return false;
             left++;
             right--;
         }
