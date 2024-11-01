0ac3b21459a71519a2ba67c600f6072511c0a2f7
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2024-11-01 00:25:17 -0400

    Time: 0 ms (100.00%), Space: 41.3 MB (23.80%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -14,36 +14,21 @@
  * }
  */
 class Solution {
+    int total = 0;
     public int sumNumbers(TreeNode root) {
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(root, 0));
-        int total = 0;
-        while(!q.isEmpty()){
-            for(int i=q.size(); i>0; i--){
-                Pair p = q.poll();
-                TreeNode node = p.node;
-                int prevVal = p.prevVal;
-                
-                int currentVal = prevVal * 10 + node.val;
-                
-                if(node.left == null && node.right == null){
-                    total+=currentVal;
-                }
-                
-                
-                if(node.left != null) q.add(new Pair(node.left, currentVal));
-                if(node.right != null) q.add(new Pair(node.right, currentVal));
-                
-            }
-        }
+        getSum(root, 0);
         return total;
     }
-}
-class Pair{
-    TreeNode node;
-    int prevVal;
-    Pair(TreeNode _node, int _prevVal){
-        node = _node;
-        prevVal = _prevVal;
+    public void getSum(TreeNode root, int currentVal){
+        if(root == null) return;
+        
+        if(root.left == null && root.right == null){
+            currentVal = currentVal*10 + root.val;
+            total+=currentVal;
+        }
+        
+        getSum(root.left, currentVal*10 + root.val);
+        getSum(root.right, currentVal*10 + root.val);
+        
     }
 }
