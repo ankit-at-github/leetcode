559bb6f45150574169a1431aa72dfb218c9746c9
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2024-11-01 08:03:42 -0400

    Time: 12 ms (54.47%), Space: 45.3 MB (30.75%) - LeetHub

--- 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.java
+++ 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.java
@@ -7,13 +7,12 @@ class Solution {
             String[] components = log.split(":");
             if(components[1].equals("start")){
                 if(!st.isEmpty()){
-                    result[st.peek()]+= Integer.parseInt(components[2]) - prevTime;
+                    result[st.peek()]+=Integer.parseInt(components[2]) - prevTime;
                 }
                 st.push(Integer.parseInt(components[0]));
                 prevTime = Integer.parseInt(components[2]);
-            }
-            else{
-                result[st.pop()]+= Integer.parseInt(components[2]) - prevTime + 1;
+            }else{
+                result[st.pop()]+=Integer.parseInt(components[2]) - prevTime + 1;
                 prevTime = Integer.parseInt(components[2]) + 1;
             }
         }
