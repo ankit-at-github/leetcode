ad6d24453df70ff09705a390c4492abb19fd86e2
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2024-11-03 13:45:05 -0500

    Time: 0 ms (100.00%), Space: 43 MB (43.85%) - LeetHub

--- 0270-closest-binary-search-tree-value/0270-closest-binary-search-tree-value.java
+++ 0270-closest-binary-search-tree-value/0270-closest-binary-search-tree-value.java
@@ -15,29 +15,18 @@
  */
 class Solution {
     public int closestValue(TreeNode root, double target) {
-        List<Integer> sortedList = new ArrayList<>();
-        inorder(root, sortedList);
-        int low = 0, high = sortedList.size()-1;
-        while(low <= high){
-            int mid = low + (high - low) / 2;
+        int result = root.val;
+        while(root != null){
             
-            if(sortedList.get(mid) == target) return sortedList.get(mid);
+            if(Math.abs(root.val - target) < Math.abs(result - target)){
+                result = root.val;
+            }
+            else if(Math.abs(root.val - target) == Math.abs(result - target)){
+                result = Math.min(result, root.val);
+            }
             
-            if(sortedList.get(mid) < target) low = mid+1;
-            else high = mid-1;
+            root = target < root.val ? root.left : root.right;
         }
-        
-        if(low >= sortedList.size()) return sortedList.get(sortedList.size()-1);
-        else if(low == 0) return sortedList.get(0);
-        else{
-            return Math.abs(target - sortedList.get(low)) < Math.abs(target - sortedList.get(low - 1)) ? sortedList.get(low) : sortedList.get(low-1);
-        }
-    }
-    public void inorder(TreeNode root, List<Integer> sortedList){
-        if(root == null) return;
-        
-        inorder(root.left, sortedList);
-        sortedList.add(root.val);
-        inorder(root.right, sortedList);
+        return result;
     }
 }
