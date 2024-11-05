8f6dea301a7fb34af827a3207585bb2b1240ef13
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2024-11-05 14:44:15 -0500

    Time: 22 ms (12.05%), Space: 49 MB (9.99%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,31 +1,22 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> map = new HashMap<>();
-        
-        for(String s : strs)
-        {
-            int[] freq = new int[26];
-            for(char c : s.toCharArray()) freq[c-'a']++;
-            
-            StringBuilder sb = new StringBuilder();
-            
-            for(int i=0; i<26; i++)
-            {
-                sb.append('#');
-                sb.append(freq[i]);
-            }
-            
-            String key = sb.toString();
-            
-            if(!map.containsKey(key)) map.put(key, new ArrayList<String>());
-            
-            map.get(key).add(s);
-        }
-        
         List<List<String>> ans = new ArrayList<>();
-        
-        for(List<String> l : map.values()) ans.add(l);
-        
+        Map<String, List<String>> anagram = new HashMap<>();
+        for(String str : strs){
+            int[] freq = new int[26];
+            for(char c : str.toCharArray()){
+                freq[c-'a']++;
+            }
+            StringBuilder key = new StringBuilder();
+            for(int i=0; i<26; i++){
+                key.append(freq[i]).append("#");
+            }
+            if(!anagram.containsKey(key.toString())) anagram.put(key.toString(), new ArrayList());
+            anagram.get(key.toString()).add(str);
+        }
+        for(List<String> grouped : anagram.values()){
+            ans.add(grouped);
+        }
         return ans;
     }
 }
