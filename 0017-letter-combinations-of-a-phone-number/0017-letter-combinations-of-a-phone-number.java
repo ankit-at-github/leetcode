d7fb963806f68618321519f0a8150f668e8fd629
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2024-11-05 23:49:21 -0500

    Time: 0 ms (100.00%), Space: 42.4 MB (18.74%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,35 +1,37 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-        Map<Integer, String> keypad = new HashMap<>();
-        keypad.put(2, "abc");
-        keypad.put(3, "def");
-        keypad.put(4, "ghi");
-        keypad.put(5, "jkl");
-        keypad.put(6, "mno");
-        keypad.put(7, "pqrs");
-        keypad.put(8, "tuv");
-        keypad.put(9, "wxyz");
+        Map<Character, String> keypad = new HashMap<>();
+        keypad.put('2', "abc");
+        keypad.put('3', "def");
+        keypad.put('4', "ghi");
+        keypad.put('5', "jkl");
+        keypad.put('6', "mno");
+        keypad.put('7', "pqrs");
+        keypad.put('8', "tuv");
+        keypad.put('9', "wxyz");
         
         List<String> ans = new ArrayList<>();
         
         if(digits.length() == 0) return ans;
         
-        solve(0, digits, new StringBuilder(), keypad, ans);
+        solve(digits, 0, new StringBuilder(), ans, keypad);
+        
         return ans;
     }
-    public void solve(int index, String digits, StringBuilder sb, Map<Integer, String> keypad, List<String> ans){
-        
-        if(sb.length() == digits.length()){
-            ans.add(sb.toString());
+    public void solve(String digits, int index, StringBuilder sb, List<String> ans, Map<Character, String> keypad){
+        if(index >= digits.length()){
+            ans.add(new String(sb.toString()));
             return;
         }
         
-        String keyPadValue = keypad.get(digits.charAt(index)-'0');
+        String letters = keypad.get(digits.charAt(index));
         
-        for(int i=0; i<keyPadValue.length(); i++){
-            sb.append(keyPadValue.charAt(i));
-            solve(index+1, digits, sb, keypad, ans);
-            sb.deleteCharAt(sb.length()-1);
+        //System.out.println(letters);
+        
+        for(int i=0; i<letters.length(); i++){
+            sb.append(letters.charAt(i));
+            solve(digits, index+1, sb, ans, keypad);
+            sb.deleteCharAt((sb.length()-1));
         }
     }
 }
