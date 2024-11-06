4521f1e2cfa3dc322508748b98ffa4fff3bf05cb
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2024-11-06 18:54:28 -0500

    Time: 5 ms (59.64%), Space: 45.7 MB (64.01%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,22 +1,39 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        TreeMap<Integer, List<Integer>> tmap = new TreeMap<>();
-        for(int num : arr){
-            int diff = Math.abs(num - x);
-            if(!tmap.containsKey(diff)) tmap.put(diff, new ArrayList());
-            tmap.get(diff).add(num);
-        }
-        List<Integer> ans = new ArrayList<>();
-        for(List<Integer> l : tmap.values()){
-            for(int n : l){
-                ans.add(n);
-                if(ans.size() == k){
-                    Collections.sort(ans);
-                    return ans;
-                }
+        int index = getIndex(arr, x);
+        
+        int left = index-1, right = left+1;
+        
+        while(right - left - 1 < k){
+            //element found at 0th index
+            if(left == -1){
+                right++;
+                continue;
+            }
+            //element found at last index or somewhere in mid
+            if(right == arr.length || Math.abs(arr[left] - x) <= Math.abs(arr[right] - x)){
+                left--;
+            }
+            else{
+                right++;
             }
         }
-        Collections.sort(ans);
+        
+        List<Integer> ans = new ArrayList<>();
+        
+        for(int i = left+1; i < right; i++){
+            ans.add(arr[i]);
+        }
         return ans;
+    }
+    public int getIndex(int[] arr, int x){
+        int low = 0, high = arr.length;
+        while(low < high){
+            int mid = low + (high - low) / 2;
+            
+            if(arr[mid] < x) low = mid+1;
+            else high = mid;
+        }
+        return low;
     }
 }
