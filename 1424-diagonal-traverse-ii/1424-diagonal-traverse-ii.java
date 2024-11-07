6035afea0943e012ffdb36dc42a08972fcdc30b8
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2024-11-07 00:04:33 -0500

    Time: 21 ms (92.88%), Space: 64.9 MB (73.21%) - LeetHub

--- 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.java
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.java
@@ -1,29 +1,29 @@
 class Solution {
     public int[] findDiagonalOrder(List<List<Integer>> nums) {
+        List<Integer> ans = new ArrayList<>();
+        
         Queue<int[]> q = new LinkedList<>();
         q.add(new int[]{0,0});
-        List<Integer> ans = new ArrayList<>();
         while(!q.isEmpty()){
-            int[] index = q.poll();
-            int x = index[0];
-            int y = index[1];
-            ans.add(nums.get(x).get(y));
-            
-            //checking if next row exist in current column
-            if(y == 0 && x + 1 < nums.size()){
-                q.add(new int[]{x+1, y});
+            for(int i=q.size(); i>0; i--){
+                int[] loc = q.poll();
+                int x = loc[0];
+                int y = loc[1];
+                
+                ans.add(nums.get(x).get(y));
+                
+                if(y == 0 && x + 1 < nums.size()){
+                    q.add(new int[]{x+1, y});
+                }
+                
+                if(y + 1 < nums.get(x).size()){
+                    q.add(new int[]{x, y+1});
+                }
             }
-            //checking if next element exist in current row (Right Side)
-            if(y+1 < nums.get(x).size()){
-                q.add(new int[]{x, y+1});
-            }
-            
         }
         int[] result = new int[ans.size()];
-        int i = 0;
-        for(int num : ans){
-            result[i++] = num;
-        }
+        int index = 0;
+        for(int x : ans) result[index++] = x;
         return result;
     }
 }
