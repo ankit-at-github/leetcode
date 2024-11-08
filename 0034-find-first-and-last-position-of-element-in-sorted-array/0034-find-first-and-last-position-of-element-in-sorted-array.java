d106014516b929939c256544a314e4125f92a7f5
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2024-11-07 23:39:22 -0500

    Time: 0 ms (100.00%), Space: 45.9 MB (31.82%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,22 +1,34 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int leftMost = binarySearch(nums, target, true);
-        int rightMost = binarySearch(nums, target, false);
-        return new int[]{leftMost, rightMost};
+        //if found at mid, continue searching left or right
+        return new int[]{leftMost(nums, target), rightMost(nums, target)};
     }
-    public int binarySearch(int[] nums, int target, boolean leftBiased){
-        int left = 0, right = nums.length-1;
-        int index = -1;
-        while(left <= right){
-            int mid = left + (right - left) / 2;
-            if(target > nums[mid]) left = mid+1;
-            else if(target < nums[mid]) right = mid-1;
-            else{
-                index = mid;
-                if(leftBiased) right = mid-1;
-                else left = mid+1;
-            }
+    public int leftMost(int[] nums, int target){
+        int start = 0, end = nums.length-1;
+        int result = -1;
+        while(start <= end){
+            int mid = start + (end-start) / 2;
+            
+            if(nums[mid] == target){
+                result = mid;
+                end = mid-1;
+            }else if(nums[mid] < target) start = mid+1;
+            else end = mid-1; 
         }
-        return index;
+        return result;
+    }
+    public int rightMost(int[] nums, int target){
+        int start = 0, end = nums.length-1;
+        int result = -1;
+        while(start <= end){
+            int mid = start + (end-start) / 2;
+            
+            if(nums[mid] == target){
+                result = mid;
+                start = mid+1;
+            }else if(nums[mid] < target) start = mid+1;
+            else end = mid-1; 
+        }
+        return result;
     }
 }
