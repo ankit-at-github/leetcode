5515f4919410960194772a600676614abbaf1dc8
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2024-11-09 14:14:41 -0500

    Time: 2 ms (79.46%), Space: 43.1 MB (61.24%) - LeetHub

--- 0249-group-shifted-strings/0249-group-shifted-strings.java
+++ 0249-group-shifted-strings/0249-group-shifted-strings.java
@@ -2,20 +2,19 @@ class Solution {
     public List<List<String>> groupStrings(String[] strings) {
         Map<String, List<String>> map = new HashMap<>();
         for(String s : strings){
-            //distance between each letter of string and first character is same
-            int diff = s.charAt(0)-'a';
-            char[] t = s.toCharArray();
-            for(int i=0; i<t.length; i++){
-                char d = (char) (t[i] - diff);
+            int diff = s.charAt(0) - 'a';
+            char[] charArray = s.toCharArray();
+            for(int i=0; i<s.length(); i++){
+                char d = (char)(charArray[i] - diff);
                 if(d < 'a'){
                     d+=26;
                 }
-                t[i] = d;
+                charArray[i] = d;
             }
-            String key = new String(t);
+            String key = new String(charArray);
             if(!map.containsKey(key)) map.put(key, new ArrayList());
             map.get(key).add(s);
         }
-        return new ArrayList<>(map.values());
+        return new ArrayList(map.values());
     }
 }
