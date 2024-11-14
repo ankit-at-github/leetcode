1919949de230e2a6fce29b35d50d5a9f069275ed
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2024-11-13 20:57:12 -0500

    Time: 1 ms (100.00%), Space: 41.8 MB (96.14%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -4,56 +4,50 @@ class Solution {
         int n = grid[0].length;
         int freshOranges = 0;
         Queue<int[]> q = new LinkedList<>();
-        for(int i=0; i<m; i++)
-        {
-            for(int j=0; j<n; j++)
-            {
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
                 if(grid[i][j] == 1) freshOranges++;
-                if(grid[i][j] == 2) q.add(new int[]{i, j});
+                else if(grid[i][j] == 2) q.add(new int[]{i, j});
             }
         }
+        
+        //No Fresh Oranges at time 0;
         if(freshOranges == 0) return 0;
-        int minutes = -1;
-        while(!q.isEmpty())
-        {
-            for(int i=q.size(); i>0; i--)
-            {
-                int x = q.peek()[0];
-                int y = q.poll()[1];
+        
+        int time = -1;
+        while(!q.isEmpty()){
+            for(int i=q.size(); i>0; i--){
+                int[] axis = q.poll();
+                int x = axis[0];
+                int y = axis[1];
                 
-                if(valid(x+1, y, m, n, grid))
-                {
-                    grid[x+1][y] = 2;
-                    freshOranges--;
-                    q.add(new int[]{x+1, y});
-                }
-                if(valid(x, y+1, m, n, grid))
-                {
-                    grid[x][y+1] = 2;
+                if(validPath(x, y+1, grid)){
+                    grid[x][y+1] = 0;
                     freshOranges--;
                     q.add(new int[]{x, y+1});
                 }
-                if(valid(x-1, y, m, n, grid))
-                {
-                    grid[x-1][y] = 2;
+                if(validPath(x+1, y, grid)){
+                    grid[x+1][y] = 0;
                     freshOranges--;
-                    q.add(new int[]{x-1, y});
+                    q.add(new int[]{x+1, y});
                 }
-                if(valid(x, y-1, m, n, grid))
-                {
-                    grid[x][y-1] = 2;
+                if(validPath(x, y-1, grid)){
+                    grid[x][y-1] = 0;
                     freshOranges--;
                     q.add(new int[]{x, y-1});
                 }
+                if(validPath(x-1, y, grid)){
+                    grid[x-1][y] = 0;
+                    freshOranges--;
+                    q.add(new int[]{x-1, y});
+                }
             }
-            minutes++;
+            time++;
         }
-        if(freshOranges!=0) return -1;
-        return minutes;
+        return freshOranges == 0 ? time : -1;
     }
-    public boolean valid(int x, int y, int m, int n, int[][] grid)
-    {
-        if(x<0 || y<0 || x>=m || y>=n || grid[x][y]!=1) return false;
+    public boolean validPath(int x, int y, int[][] grid){
+        if(x < 0 || y < 0 || x >= grid.length || y >= grid[0].length || grid[x][y] != 1) return false;
         return true;
     }
 }
