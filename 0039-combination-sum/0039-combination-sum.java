c3f13eaf38dac7e95efac3472f5c5b6cb4d0c1f7
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2024-11-17 13:39:23 -0500

    Time: 2 ms (84.56%), Space: 44.3 MB (83.19%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,29 +1,21 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> output = new ArrayList<>();
-        
-        Arrays.sort(candidates);
-        
-        solve(0, candidates, target, output, ans);
-        
+        solve(0, candidates, target, new ArrayList(), ans);
         return ans;
     }
-    public void solve(int index, int[] candidates, int target, List<Integer> output, List<List<Integer>> ans)
-    {
-        if(target == 0)
-        {
-            ans.add(new ArrayList(output));
+    public void solve(int index, int[] candidates, int target, List<Integer> intermediateResult, List<List<Integer>> ans){
+        if(target < 0) return;
+        
+        if(target == 0){
+            ans.add(new ArrayList(intermediateResult));
             return;
         }
-        
-        for(int i=index; i<candidates.length; i++)
-        {
-            if(target-candidates[i] < 0) break;
-            
-            output.add(candidates[i]);
-            solve(i, candidates, target-candidates[i], output, ans);
-            output.remove(output.size()-1);
+        //How to skip duplicates? Start from same index
+        for(int i=index; i<candidates.length; i++){
+            intermediateResult.add(candidates[i]);
+            solve(i, candidates, target - candidates[i], intermediateResult, ans);
+            intermediateResult.remove(intermediateResult.size()-1);
         }
         
     }
