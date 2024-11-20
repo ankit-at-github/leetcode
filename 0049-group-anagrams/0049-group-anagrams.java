6cccb8bcd5b1d299dc9f7112611e6da1aaee4d61
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2024-11-19 21:11:44 -0500

    Time: 20 ms (14.86%), Space: 48.7 MB (12.15%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,21 +1,21 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> ans = new ArrayList<>();
-        Map<String, List<String>> anagram = new HashMap<>();
+        Map<String, List<String>> anagrams = new HashMap<>();
         for(String str : strs){
             int[] freq = new int[26];
             for(char c : str.toCharArray()){
                 freq[c-'a']++;
             }
-            StringBuilder key = new StringBuilder();
-            for(int i=0; i<26; i++){
-                key.append(freq[i]).append("#");
-            }
-            if(!anagram.containsKey(key.toString())) anagram.put(key.toString(), new ArrayList());
-            anagram.get(key.toString()).add(str);
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i<26; i++) sb.append(freq[i]).append("#");
+            String key = sb.toString();
+            if(!anagrams.containsKey(key)) anagrams.put(key, new ArrayList());
+            anagrams.get(key).add(str);
+            
         }
-        for(List<String> grouped : anagram.values()){
-            ans.add(grouped);
+        List<List<String>> ans = new ArrayList<>();
+        for(List<String> anagram : anagrams.values()){
+            ans.add(anagram);
         }
         return ans;
     }
