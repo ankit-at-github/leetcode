c488e2ce1b129e8d1468f746fe51f58532bc307c
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2024-11-22 22:45:39 -0500

    Time: 0 ms (100.00%), Space: 40.8 MB (7.59%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,27 +1,20 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] memo = new int[m][n];
-        for(int i=0; i<m; i++) Arrays.fill(memo[i], -1);
-        return solve(0, 0, m, n, memo);
+        for(int i=0; i<m; i++) Arrays.fill(memo[i], - 1);
+        return dfs(memo, m-1, n-1, m, n);
     }
-    public int solve(int x, int y, int m, int n, int[][] memo){
-        // 1. Base Case
+    public int dfs(int[][] memo, int x, int y, int m, int n){
         
-        //not my path
         if(x < 0 || y < 0 || x >= m || y >= n) return 0;
         
-        if(x == m-1 && y == n-1){
-            return 1;
-        }
-        
         if(memo[x][y] != -1) return memo[x][y];
+            
+        if(x == 0 && y == 0) return 1;
         
-        // 2. Exploring ways
-        //right
-        int right = solve(x, y+1, m, n, memo);
-        //down
-        int down = solve(x+1, y, m, n, memo);
+        int up = dfs(memo, x-1, y, m, n);
+        int left = dfs(memo, x, y-1, m, n);
         
-        return memo[x][y] = right + down;
+        return memo[x][y] = up + left;
     }
 }
