2df781f83d38cb6d8e98d306bde39dc913a224c9
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2024-11-23 12:24:47 -0500

    Time: 0 ms (100.00%), Space: 45.7 MB (33.09%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -4,6 +4,7 @@ class Solution {
         int n = matrix[0].length;
         boolean firstCol = false;
         for(int i=0; i<m; i++){
+            //first col has zero, only checking once
             if(matrix[i][0] == 0) firstCol = true;
             for(int j=1; j<n; j++){
                 if(matrix[i][j] == 0){
@@ -12,19 +13,20 @@ class Solution {
                 }
             }
         }
+        
         for(int i=1; i<m; i++){
             for(int j=1; j<n; j++){
                 if(matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
             }
         }
-        //Marking 1st Row
+        
+        //firstRow
         if(matrix[0][0] == 0){
-            for(int j=0; j<n; j++) matrix[0][j] = 0;
+            for(int j=1; j<n; j++) matrix[0][j] = 0;
         }
-        //Marking 1st Col
+        //firstCol
         if(firstCol){
             for(int i=0; i<m; i++) matrix[i][0] = 0;
         }
-        
     }
 }
