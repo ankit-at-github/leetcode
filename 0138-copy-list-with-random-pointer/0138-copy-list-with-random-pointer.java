a5c75a68503677f84dd3831ff469b64671ae1f47
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2024-12-15 12:09:03 -0500

    Time: 0 ms (100.00%), Space: 45.1 MB (5.63%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -16,20 +16,34 @@ class Node {
 class Solution {
     public Node copyRandomList(Node head) {
         if(head == null) return null;
-        Map<Node, Node> map = new HashMap<>();
-        Node temp = head;
-        while(temp != null){
-            map.put(temp, new Node(temp.val));
-            temp = temp.next;
+        //interleaving of old node and new node [A->A'->B->B'->null]
+        //1. Copy and interleaving of old nodes with new nodes
+        Node oldNode = head;
+        while(oldNode != null){
+            Node copyNode = new Node(oldNode.val);
+            copyNode.next = oldNode.next;
+            oldNode.next = copyNode;
+            oldNode = copyNode.next;
         }
-        Node head2 = map.get(head);
-        temp = head2;
-        while(head != null){
-            head2.next = map.get(head.next);
-            head2.random = map.get(head.random);
-            head = head.next;
+        //2. Link Random Pointer of new node created
+        oldNode = head;
+        while(oldNode != null){
+            if(oldNode.random != null){
+                oldNode.next.random = oldNode.random.next;
+            }
+            oldNode = oldNode.next.next;
+        }
+        //3. Restore Original List
+        Node head2 = head.next;
+        Node dummy = head2;
+        oldNode = head;
+        while(head2.next != null){
+            oldNode.next = head2.next;
+            oldNode = oldNode.next;
+            head2.next = head2.next.next;
             head2 = head2.next;
         }
-        return temp;
+        oldNode.next = null;
+        return dummy;
     }
 }
