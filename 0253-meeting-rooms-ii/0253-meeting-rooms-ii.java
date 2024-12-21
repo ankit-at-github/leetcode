75bf3187ff50cd9cc2565d194f22ec0e0bf4ee14
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2024-12-21 09:53:14 -0500

    Time: 3 ms (99.97%), Space: 46.5 MB (10.25%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -13,17 +13,14 @@ class Solution {
         Arrays.sort(end);
         int startPointer = 0, endPointer = 0;
         int usedRooms = 0;
-        int maxRooms = 0;
         while(startPointer < n){
-            if(start[startPointer] < end[endPointer]){
-                usedRooms++;
-                startPointer++;
-            }else{
+            if(start[startPointer] >= end[endPointer]){
                 usedRooms--;
                 endPointer++;
             }
-            maxRooms = Math.max(maxRooms, usedRooms);
+            usedRooms++;
+            startPointer++;
         }
-        return maxRooms;
+        return usedRooms;
     }
 }
