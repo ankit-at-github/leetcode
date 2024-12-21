9975827f2c0eae9f1347babfc5ed12bd0f5b187b
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2024-12-21 08:53:34 -0500

    Time: 6 ms (79.09%), Space: 46.2 MB (25.04%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.java
+++ 0252-meeting-rooms/0252-meeting-rooms.java
@@ -1,8 +1,15 @@
 class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
-        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
-        for(int i=1; i<intervals.length; i++){
-            if(intervals[i][0] < intervals[i-1][1]) return false;
+        int n = intervals.length;
+        
+        if(n < 2) return true;
+        
+        Arrays.sort(intervals, (a,b) -> a[0]-b[0]);
+        
+        int end = intervals[0][1];
+        for(int i=1; i<n; i++){
+            if(end > intervals[i][0]) return false;
+            end = intervals[i][1];
         }
         return true;
     }
