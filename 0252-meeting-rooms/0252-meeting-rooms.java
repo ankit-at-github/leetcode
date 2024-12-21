956d932ece7e4cbf741bfee578c1c4278d310c94
ANKIT KUMAR <32020073+ankit-at-github@users.noreply.github.com>
2024-12-21 08:54:41 -0500

    Time: 5 ms (96.84%), Space: 46.1 MB (35.83%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.java
+++ 0252-meeting-rooms/0252-meeting-rooms.java
@@ -6,10 +6,8 @@ class Solution {
         
         Arrays.sort(intervals, (a,b) -> a[0]-b[0]);
         
-        int end = intervals[0][1];
         for(int i=1; i<n; i++){
-            if(end > intervals[i][0]) return false;
-            end = intervals[i][1];
+            if(intervals[i-1][1] > intervals[i][0]) return false;
         }
         return true;
     }
